73e8bdca6ae03c86a36f5bc3c32da268c264f503
James Montemagno <james.montemagno@gmail.com>
2016-12-03 14:43:43 -0800

    Initial Commit

---
+++ FormsCommunityToolkit.Converters/HasDataConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Globalization;
+using System.Collections;
+
+namespace FormsCommunityToolkit.Converters
+{
+    public class HasDataConverter : IValueConverter
+    {
+        /// <summary>
+        /// Init this instance.
+        /// </summary>
+        public static void Init()
+        {
+            var time = DateTime.UtcNow;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //if null then not visible
+            if (value == null)
+                return false;
+
+            //if empty string then not visible
+            if (value is string)
+                return !string.IsNullOrWhiteSpace((string)value);
+
+            //if blank list not visible
+            if (value is IList)
+                return ((IList)value).Count > 0;
+
+            return true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ FormsCommunityToolkit.Converters/HexToColorConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Globalization;
+
+namespace FormsCommunityToolkit.Converters
+{
+    /// <summary>
+    /// Hex to color converter.
+    /// </summary>
+    public class HexToColorConverter : IValueConverter
+    {
+        /// <summary>
+        /// Init this instance.
+        /// </summary>
+        public static void Init()
+        {
+            var time = DateTime.UtcNow;
+        }
+        static readonly Color defaultColor = Color.FromHex("#3498db");
+
+        /// <param name="value">To be added.</param>
+        /// <param name="targetType">To be added.</param>
+        /// <param name="parameter">To be added.</param>
+        /// <param name="culture">To be added.</param>
+        /// <summary>
+        /// Convert the specified value, targetType, parameter and culture.
+        /// </summary>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var color = value as string;
+            try
+            {
+                return string.IsNullOrEmpty(color) ? defaultColor : Color.FromHex(color);
+            }
+            catch
+            {
+                return defaultColor;
+            }
+        }
+
+        /// <param name="value">To be added.</param>
+        /// <param name="targetType">To be added.</param>
+        /// <param name="parameter">To be added.</param>
+        /// <param name="culture">To be added.</param>
+        /// <summary>
+        /// Converts the back.
+        /// </summary>
+        /// <returns>The back.</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => null;
+
+    }
+}
+
