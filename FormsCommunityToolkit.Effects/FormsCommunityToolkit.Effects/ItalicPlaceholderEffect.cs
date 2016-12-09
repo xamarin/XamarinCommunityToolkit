792a47376eb1dcf33abda59c6e99e961536bcf06
Nick Mathews <nick.mathews@perficient.com>
2016-12-09 07:08:16 -0500

    Updated Sample app
    Updated the Sample app to include a demonstration of the Italic Placeholder Effect. I forgot the RoutingEffect so had to make that.

---
+++ FormsCommunityToolkit.Effects/FormsCommunityToolkit.Effects/ItalicPlaceholderEffect.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace FormsCommunityToolkit.Effects
+{
+	public class ItalicPlaceholderEffect : RoutingEffect
+	{
+		public ItalicPlaceholderEffect() : base("Organon.Effects.ItalicPlaceholderEffect")
+		{
+		}
+	}
+}
