70f497c2404b2f887277808208138c8d462e908b
Shawn Kendrot <skendrot@hotmail.com>
2016-12-13 17:28:58 -0700

    Add support for changing the true color for a toggle switch

---
+++ FormsCommunityToolkit.Effects/FormsCommunityToolkit.Effects.UWP/VisualTreeExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace FormsCommunityToolkit.Effects.UWP
+{
+    static class VisualTreeExtensions
+    {
+        public static T GetChildOfType<T>(this DependencyObject obj) where T: DependencyObject
+        {
+            if (obj == null) return null;
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                var child = VisualTreeHelper.GetChild(obj, i);
+
+                var result = (child as T) ?? GetChildOfType<T>(child);
+                if (result != null) return result;
+            }
+            return null;
+        }
+
+        public static FrameworkElement GetChildByName(this FrameworkElement obj, string name)
+        {
+            if (obj == null) return null;
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                var child = VisualTreeHelper.GetChild(obj, i) as FrameworkElement;
+                if (child == null) continue;
+
+                if(child.Name == name)
+                {
+                    return child;
+                }
+                var result = GetChildByName(child, name);
+                if (result != null) return result;
+            }
+            return null;
+        }
+    }
+}
