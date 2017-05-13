78ee769bef593c088fee6aa5cee4e2d17edbbaca
Depechie <glenn.versweyveld@gmail.com>
2017-05-13 11:53:10 +0200

    First stab at adding samples project

---
+++ Samples/Samples/Samples/Views/EffectsEntryClearPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FormsCommunityToolkit.Samples.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EffectsEntryClearPage : ContentPage
+    {
+        public EffectsEntryClearPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Samples/Samples/Samples/Views/EffectsEntryPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FormsCommunityToolkit.Samples.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EffectsEntryPage : ContentPage
+    {
+        public EffectsEntryPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCapitalizeKeyboardButtonClicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new EffectsEntryCapitalizeKeyboardPage());
+        }
+
+        private async void OnClearButtonClicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new EffectsEntryClearPage());
+        }
+    }
+}
