07823c6a28906d0e57d7053471108d046f507090
Depechie <glenn.versweyveld@gmail.com>
2017-09-19 08:31:57 +0200

    Remove file level xaml compilation attribute

--- Samples/Samples/Samples/Views/EffectsLabelPage.xaml.cs
+++ Samples/Samples/Samples/Views/EffectsLabelPage.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace FormsCommunityToolkit.Samples.Views
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class EffectsLabelPage : ContentPage
     {
         public EffectsLabelPage()
