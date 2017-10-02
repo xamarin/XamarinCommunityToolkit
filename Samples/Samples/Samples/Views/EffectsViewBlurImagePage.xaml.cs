d1b43a772843df6e51b89f424199d01e7506c0a8
Depechie <glenn.versweyveld@gmail.com>
2017-10-02 14:27:13 +0200

    Image View blur effect sample #HacktoberFest

---
+++ Samples/Samples/Samples/Views/EffectsViewBlurImagePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace FormsCommunityToolkit.Samples.Views
+{
+    public partial class EffectsViewBlurImagePage : ContentPage
+    {
+        public EffectsViewBlurImagePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
