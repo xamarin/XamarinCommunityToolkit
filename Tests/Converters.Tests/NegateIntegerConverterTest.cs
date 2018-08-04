cd7939d9429d0d39f8433f35c25204cc229acdee
adham ali <adhamali_4500@outlook.com>
2018-08-04 13:43:17 +0200

    NegateIntegerConverter and NegateIntegerConverterTest. Negates an integer value and returns 0 if invalid value

---
+++ Tests/Converters.Tests/NegateIntegerConverterTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xamarin.Toolkit.Converters;
+
+namespace Converters.Tests
+{
+    [TestClass]
+    public class NegateIntegerConverterTest
+    {
+        NegateIntegerConverter converter;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            converter = new NegateIntegerConverter();
+        }
+
+        [TestMethod]
+        public void Convert_ConvertBackReturns_0_IfInvalidValue()
+        {
+            var invalidValue = "InvalidValue";
+            var result = converter.Convert(invalidValue, null, null, null);
+
+            Assert.AreEqual(result, 0);
+        }
+
+        [TestMethod]
+        public void Convert_ConvertBackNegatesValue()
+        {
+            var validValue = 5;
+            var result = converter.Convert(validValue, null, null, null);
+
+            Assert.AreEqual(result, -5);
+        }
+    }
+}
---
+++ Toolkit/Converters/NegateIntegerConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Xamarin.Toolkit.Converters
+{
+    /// <summary>
+    /// Negates an integer.
+    /// </summary>
+    public class NegateIntegerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Invert(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Invert(value);
+        }
+
+        int Invert(object value)
+        {
+            var integer = 0;
+
+            try
+            {
+                integer = (int)value;
+            }
+            catch
+            {
+                return 0;
+            }
+
+            return (int)value * -1;
+        }
+    }
+}
