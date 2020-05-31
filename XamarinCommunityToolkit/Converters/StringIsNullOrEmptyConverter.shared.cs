528f28a1d74be80a8ddd53f822d27448c2129c5b
Steven Thewissen <github@thewissen.io>
2020-05-31 22:03:47 +0200

    Added some basic converters

---
+++ XamarinCommunityToolkit/Converters/StringIsNullOrEmptyConverter.shared.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace XamarinCommunityToolkit.Converters
+{
+    public class StringIsNullOrEmptyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => string.IsNullOrEmpty(value?.ToString());
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
