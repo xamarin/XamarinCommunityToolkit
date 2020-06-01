8738c07dd02deae42a987b791b1ddf65755cf543
Steven Thewissen <github@thewissen.io>
2020-06-01 19:15:18 +0200

    Converting to string explicitly instead of ToString

--- XamarinCommunityToolkit/Converters/StringIsNullOrEmptyConverter.shared.cs
+++ XamarinCommunityToolkit/Converters/StringIsNullOrEmptyConverter.shared.cs
@@ -6,7 +6,7 @@ namespace XamarinCommunityToolkit.Converters
 {
     public class StringIsNullOrEmptyConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => string.IsNullOrEmpty(value?.ToString());
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => string.IsNullOrEmpty((string)value);
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
