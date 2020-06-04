74809debf6033f690d6e13e039211e6e6c381537
Steven Thewissen <github@thewissen.io>
2020-06-04 09:36:26 +0200

    Basic project setup & added some basic converters (#120)
    * Fixes #119
    
    * Added some basic converters
    
    * Converting to string explicitly instead of ToString
    
    * Changes to csproj
    
    * Renamed converters
    
    * Small PR changes
    
    * PR updates

---
+++ XamarinCommunityToolkit/Converters/InvertedBooleanConverter.shared.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace XamarinCommunityToolkit.Converters
+{
+    /// <summary>
+    /// Converts true to false and false to true.
+    /// </summary>
+    public class InvertedBooleanConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts a boolean to its inverse value.
+        /// </summary>
+        /// <param name="value">The value to convert.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">Additional parameter for the converter to handle. Not implemented.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>An inverted boolean from the one coming in.</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => !(bool)value;
+
+        /// <summary>
+        /// Converts a boolean to its inverse value.
+        /// </summary>
+        /// <param name="value">The value to convert.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">Additional parameter for the converter to handle. Not implemented.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>An inverted boolean from the one coming in.</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => !(bool)value;
+    }
+}
---
+++ XamarinCommunityToolkit/Converters/IsNotNullOrEmptyConverter.shared.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace XamarinCommunityToolkit.Converters
+{
+    /// <summary>
+    /// Converts the incoming value to a boolean indicating whether or not the value is not null and not empty.
+    /// </summary>
+    public class IsNotNullOrEmptyConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts the incoming value to a boolean indicating whether or not the value is not null and not empty.
+        /// </summary>
+        /// <param name="value">The value to convert.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">Additional parameter for the converter to handle. Not implemented.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>A boolean indicating if the incoming value is not null and not empty.</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+            value is string str ? !string.IsNullOrWhiteSpace(str) : value != null;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            throw new NotImplementedException();
+    }
+}
