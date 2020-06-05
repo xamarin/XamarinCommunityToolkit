c8d38e222167e85825a267c70adc124e31dc812a
Sunny Mukherjee <sendingtosunny@gmail.com>
2020-06-04 21:32:45 -0400

    Behaviors
    - BaseBehavior
    - EffectBehavior
    - EventToCommandBehavior
    - NumericValidationBehavior
    - AttachedNumericValidationBehavior

---
+++ XamarinCommunityToolkit/Behaviors/NumericValidationBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XamarinCommunityToolkit.Behaviors
+{
+    public class NumericValidationBehavior : BaseBehavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+
+        protected override void OnDetachingFrom(Entry entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+        }
+
+        void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            double result;
+            bool isValid = double.TryParse(args.NewTextValue, out result);
+            ((Entry)sender).TextColor = isValid ? Color.Default : Color.Red;
+        }
+    }
+}
