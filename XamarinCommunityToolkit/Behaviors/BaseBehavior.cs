8736800da12d0a29f950b2c7201e87e185efc90f
Sunny Mukherjee <sendingtosunny@gmail.com>
2020-06-04 21:32:45 -0400

    Behaviors
    - BaseBehavior
    - EffectBehavior
    - EventToCommandBehavior
    - NumericValidationBehavior
    - AttachedNumericValidationBehavior

---
+++ XamarinCommunityToolkit/Behaviors/BaseBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+namespace XamarinCommunityToolkit.Behaviors
+{
+    [Preserve(AllMembers = true)]
+    public class BaseBehavior<T> : Behavior<T> where T : BindableObject
+    {
+        public T AssociatedObject { get; private set; }
+
+        protected override void OnAttachedTo(T bindable)
+        {
+            base.OnAttachedTo(bindable);
+            AssociatedObject = bindable;
+
+            if (bindable.BindingContext != null)
+            {
+                BindingContext = bindable.BindingContext;
+            }
+
+            bindable.BindingContextChanged += OnBindingContextChanged;
+        }
+
+        protected override void OnDetachingFrom(T bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.BindingContextChanged -= OnBindingContextChanged;
+            AssociatedObject = null;
+        }
+
+        void OnBindingContextChanged(object sender, EventArgs e)
+        {
+            OnBindingContextChanged();
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            BindingContext = AssociatedObject.BindingContext;
+        }
+    }
+}
