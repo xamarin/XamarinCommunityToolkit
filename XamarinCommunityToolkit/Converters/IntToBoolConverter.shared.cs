236073df84f429c791f3be69d376f228d52fda97
Andrei Misiukevich <andrei.misiukevich@gmail.com>
2020-06-10 00:03:41 +0300

    covered IntToBoolConverter by comments

--- XamarinCommunityToolkit/Converters/IntToBoolConverter.shared.cs
+++ XamarinCommunityToolkit/Converters/IntToBoolConverter.shared.cs
@@ -4,13 +4,32 @@ using Xamarin.Forms;
 
 namespace XamarinCommunityToolkit.Converters
 {
+    /// <summary>
+    /// Converts an integer to corresponding boolean and vice versa.
+    /// </summary>
     public class IntToBoolConverter : IValueConverter
     {
+        /// <summary>
+        /// Converts an integer to corresponding boolean.
+        /// </summary>
+        /// <param name="value">The value to convert.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">Additional parameter for the converter to handle. Not implemented.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>False if the value is zero, otherwice True.</returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
             => value is int result
             ? result != 0
             : throw new ArgumentException("Value is not a valid integer", nameof(value));
 
+        /// <summary>
+        /// Converts back boolean to corresponding integer.
+        /// </summary>
+        /// <param name="value">The value to convert.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">Additional parameter for the converter to handle. Not implemented.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>0 if the value is False, otherwice 1.</returns>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is bool result)
