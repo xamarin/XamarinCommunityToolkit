f5592021a3ca18f3266dfd79dc8e4d82f26b66f8
Steven Thewissen <github@thewissen.io>
2020-06-09 14:03:50 +0200

    Null checks changed

--- XamarinCommunityToolkit/Converters/EqualConverter.shared.cs
+++ XamarinCommunityToolkit/Converters/EqualConverter.shared.cs
@@ -18,7 +18,7 @@ namespace XamarinCommunityToolkit.Converters
         /// <param name="culture">The culture to use in the converter.</param>
         /// <returns>An inverted boolean from the one coming in.</returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-            => value != null && value.Equals(parameter);
+            => (value != null && value.Equals(parameter)) || (value == null && parameter == null);
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
             => throw new NotImplementedException();
