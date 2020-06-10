619b4645f5c5d19917a3960b29a96aaba0fca166
Sunny Mukherjee <sendingtosunny@gmail.com>
2020-06-09 21:48:28 -0400

    CI Build Fixes & New Behaviors
    - Fixed CI build errors by removing multiple Include elements in project file, which caused the same file to be included multiple times during a build
    - Fixed the automatic adding of obj folder to the solution by adding Exclude attribute to the single Compile element
    - Added RegularExpressionValidationBehavior and MaxLengthValidationBehavior for Entry controls
    - Added static RegularExpressions to contain most common regular expressions that can be used in any app, especially behaviors and validators

---
+++ XamarinCommunityToolkit/Behaviors/MaxLengthValidationBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XamarinCommunityToolkit.Behaviors
+{
+    public class MaxLengthValidationBehavior : BaseBehavior<Entry>
+    {
+        public static readonly BindableProperty MaxLengthProperty = BindableProperty.
+            Create(nameof(MaxLength), typeof(int), typeof(MaxLengthValidationBehavior));
+
+        public int MaxLength
+        {
+            get => (int)GetValue(MaxLengthProperty);
+            set => SetValue(MaxLengthProperty, value);
+        }
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += OnEntryTextChanged;
+        }        
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= OnEntryTextChanged;
+        }
+
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (e.NewTextValue.Length >= MaxLength)
+                ((Entry)sender).Text = e.NewTextValue.Substring(0, MaxLength);
+        }
+    }
+}
---
+++ XamarinCommunityToolkit/Behaviors/RegularExpressionValidationBehavior.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using Xamarin.Forms;
+
+namespace XamarinCommunityToolkit.Behaviors
+{
+    public class RegularExpressionValidationBehavior : BaseBehavior<Entry>
+    {
+        public static readonly BindableProperty RegularExpressionProperty = 
+            BindableProperty.Create(nameof(RegularExpression), typeof(string), typeof(RegularExpressionValidationBehavior));
+
+        public string RegularExpression
+        {
+            get => (string)GetValue(RegularExpressionProperty);
+            set => SetValue(RegularExpressionProperty, value);
+        }
+
+        public static readonly BindableProperty TextColorProperty = 
+            BindableProperty.Create(nameof(TextColor), typeof(Color), typeof(NumericValidationBehavior));
+
+        public Color TextColor
+        {
+            get => (Color)GetValue(TextColorProperty);
+            set => SetValue(TextColorProperty, value);
+        }
+
+        protected override void OnAttachedTo(Entry entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+
+        protected override void OnDetachingFrom(Entry entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+        }
+
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            bool isValid = false;
+            isValid = (Regex.IsMatch(e.NewTextValue, RegularExpression));
+            ((Entry)sender).TextColor = isValid ? Color.Default : (TextColor != null ? TextColor : Color.Red);
+        }
+    }
+}
