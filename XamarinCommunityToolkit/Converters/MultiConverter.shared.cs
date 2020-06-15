63d0f54bc15e411ecc3e6864b5b1d166ef8fb753
Steven Thewissen <github@thewissen.io>
2020-06-15 12:02:59 +0200

    Added MultiConverter

---
+++ XamarinCommunityToolkit/Converters/MultiConverter.shared.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace XamarinCommunityToolkit.Converters
+{
+    /// <summary>
+    /// Converts an incoming value using all of the incoming converters in sequence.
+    /// </summary>
+    public class MultiConverter : List<IValueConverter>, IValueConverter
+    {
+        /// <summary>
+        /// Uses the incoming converters to convert the value.
+        /// </summary>
+        /// <param name="value">Value to convert.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">Parameter to pass into subsequent converters.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>The converted value.</returns>
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+            => this.Aggregate(value, (current, converter) => converter.Convert(current, targetType, parameter, culture));
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
