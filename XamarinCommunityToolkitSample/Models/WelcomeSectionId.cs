e20caca4750eb8d708c37f35f635f3d11d805d7f
Andrei <andrei.misiukevich@gmail.com>
2020-06-18 17:15:49 +0300

    Sample app redesign [Part 1] (#143)
    * Sample app redesign
    
    * Used colors as static resource instead of hardcoded value
    
    * updated sample

---
+++ XamarinCommunityToolkitSample/Models/WelcomeSectionId.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace XamarinCommunityToolkitSample.Models
+{
+    public enum WelcomeSectionId
+    {
+        Behaviors,
+        Converters,
+        Extensions,
+        Views,
+        TestCases
+    }
+
+    public static class WelcomeSectionIdExtensions
+    {
+        public static string GetTitle(this WelcomeSectionId id)
+            => id switch
+            {
+                WelcomeSectionId.Behaviors => "Behaviors",
+                WelcomeSectionId.Converters => "Converters",
+                WelcomeSectionId.Extensions => "Extensions",
+                WelcomeSectionId.Views => "Views",
+                WelcomeSectionId.TestCases => "Test Cases",
+                _ => throw new NotImplementedException()
+            } + " Gallery";
+    }
+}
---
+++ XamarinCommunityToolkitSample/Views/WelcomePage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+using XamarinCommunityToolkitSample.Models;
+
+namespace XamarinCommunityToolkitSample.Views
+{
+    public partial class WelcomePage : ContentPage
+    {
+        ICommand navigateCommand;
+
+        public WelcomePage()
+            => InitializeComponent();
+
+        public ICommand NavigateCommand => navigateCommand ??= new Command(parameter
+            => Navigation.PushAsync(GetPage((WelcomeSectionId)parameter)));
+
+        Page GetPage(WelcomeSectionId id)
+        {
+            var page = id switch
+            {
+                WelcomeSectionId.Behaviors => new BehaviorsPage(),
+                WelcomeSectionId.Converters => new ContentPage(),
+                WelcomeSectionId.Views => new ContentPage(),
+                WelcomeSectionId.TestCases => new ContentPage(),
+                _ => throw new System.NotImplementedException()
+            };
+            page.Title = id.GetTitle();
+            return page;
+        }
+    }
+}
