606e7f65a6229ccdb5c35cf54705c59f7ebdf75f
Steven Thewissen <github@thewissen.io>
2020-06-18 10:45:02 +0200

    PR fixes

---
+++ XamarinCommunityToolkit/Converters/MultiConverterParameter.shared.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XamarinCommunityToolkit.Converters
+{
+    /// <summary>
+    /// Represents a parameter to be used in the MultiConverter.
+    /// </summary>
+    public class MultiConverterParameter : BindableObject
+    {
+        public Type ConverterType { get; set; }
+
+        public object Value { get; set; }
+    }
+}
