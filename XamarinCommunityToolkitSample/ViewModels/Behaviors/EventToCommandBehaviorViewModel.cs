c0fb6d3254ecbfdef3c5749ed83a386e48a45617
Andrei Misiukevich <andrei.misiukevich@gmail.com>
2020-06-26 00:35:13 +0300

    renamed command in the sample files

--- XamarinCommunityToolkitSample/ViewModels/Behaviors/EventToCommandBehaviorViewModel.cs
+++ XamarinCommunityToolkitSample/ViewModels/Behaviors/EventToCommandBehaviorViewModel.cs
@@ -6,11 +6,11 @@ namespace XamarinCommunityToolkitSample.ViewModels.Behaviors
 {
     public class EventToCommandBehaviorViewModel : BaseViewModel
     {
-        ICommand command;
+        ICommand incrementCommand;
 
         int clickCount;
 
-        public ICommand Command => command ??= new Command(() => ClickCount++);
+        public ICommand IncrementCommand => incrementCommand ??= new Command(() => ClickCount++);
 
         public int ClickCount
         {
