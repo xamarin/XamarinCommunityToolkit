d1d44eb69e19084433fd72bda0e1874f0b5b130c
Andrei Misiukevich <andrei.misiukevich@gmail.com>
2020-07-04 01:23:36 +0300

    TextValidationFlags: Fix #149

---
+++ XamarinCommunityToolkit/Behaviors/TextDecorationFlags.shared.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace XamarinCommunityToolkit.Behaviors
+{
+    [Flags]
+    public enum TextDecorationFlags
+    {
+        None = 0,
+        TrimStart = 1,
+        TrimEnd = 2,
+        Trim = 3,
+        NullToEmpty = 4,
+        ReduceWhiteSpaces = 8
+    }
+}
