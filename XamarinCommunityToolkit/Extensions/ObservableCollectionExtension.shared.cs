63c8dde3679c5b8ae5e0998ae22f442cff8a743d
Konrad Mueller <k.l.mueller@outlook.com>
2020-08-02 16:47:01 +0200

    Add observable collection extensions

---
+++ XamarinCommunityToolkit/Extensions/ObservableCollectionExtension.shared.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Microsoft.Toolkit.Xamarin.Forms.Extensions
+{
+    /// <summary>
+    /// Extension methods for <see cref="ObservableCollection{T}"/>.
+    /// </summary>
+    public static class ObservableCollectionExtension
+    {
+        /// <summary>
+        /// Adds a range of items to a <paramref name="collection"/>.
+        /// </summary>
+        /// <param name="collection">Collection where the items will be added to.</param>
+        /// <param name="items">Items to add to the collection.</param>
+        public static void AddRange<T>(this ObservableCollection<T> collection, IEnumerable<T> items)
+        {
+            foreach (var item in items)
+            {
+                collection.Add(item);
+            }
+        }
+
+        /// <summary>
+        /// Sets a range of items in a <paramref name="collection"/> and clears existing items.
+        /// </summary>
+        /// <param name="collection">Collection where the items will be added to.</param>
+        /// <param name="items">Items to set in the collection.</param>
+        public static void SetRange<T>(this ObservableCollection<T> collection, IEnumerable<T> items)
+        {
+            collection.Clear();
+            collection.AddRange(items);
+        }
+    }
+}
