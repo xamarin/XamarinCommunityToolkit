2bb0442765061983a2785107465b220e55a57c01
Charlin Agramonte <char0394@gmail.com>
2020-08-04 14:10:56 -0400

    Moving all strings to a Resource file

--- XamarinCommunityToolkitSample/ViewModels/Behaviors/BehaviorsGalleryViewModel.cs
+++ XamarinCommunityToolkitSample/ViewModels/Behaviors/BehaviorsGalleryViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using Microsoft.Toolkit.Xamarin.Forms.Sample.Models;
 using Microsoft.Toolkit.Xamarin.Forms.Sample.Pages.Behaviors;
+using Microsoft.Toolkit.Xamarin.Forms.Sample.Resx;
 
 namespace Microsoft.Toolkit.Xamarin.Forms.Sample.ViewModels.Behaviors
 {
@@ -10,22 +11,22 @@ namespace Microsoft.Toolkit.Xamarin.Forms.Sample.ViewModels.Behaviors
             new SectionModel(
                 typeof(EmailValidationBehaviorPage),
                 "EmailValidationBehavior",
-                "Changes an Entry's text color when an invalid e-mail address is provided."
+                AppResources.EmailValidationShortDescription
             ),
             new SectionModel(
                 typeof(EventToCommandBehaviorPage),
                 "EventToCommandBehavior",
-                "Turns any event into a command that can be bound to."
+                AppResources.EventToCommandShortDescription
             ),
             new SectionModel(
                 typeof(NumericValidationBehaviorPage),
                 "NumericValidationBehavior",
-                "Changes an Entry's text color when an invalid numeric value is provided."
+                AppResources.NumericValidationShortDescription
             ),
             new SectionModel(
                 typeof(AnimationBehaviorPage),
                 "AnimationBehavior",
-                "Perform animation when the specified UI element event is triggered"
+                AppResources.AnimatioShortDescription
             )
         };
     }
