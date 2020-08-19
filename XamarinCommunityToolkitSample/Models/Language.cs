f8e0ca3aa84fd1aa0c5c4af4125c84334dddd843
Charlin Agramonte <char0394@gmail.com>
2020-08-19 15:07:39 -0400

    Adding multilingual support

---
+++ XamarinCommunityToolkitSample/Models/Language.cs
@@ -0,0 +1,14 @@
+﻿namespace Xamarin.CommunityToolkit.Sample.Models
+{
+    public class Language
+    {
+        public Language(string name, string ci)
+        {
+            Name = name;
+            CI = ci;
+        }
+        public string Name { get; set; }
+        public string CI { get; set; }
+    }
+}
+
---
+++ XamarinCommunityToolkitSample/Pages/SettingPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Xamarin.CommunityToolkit.Sample.Pages
+{
+	public partial class SettingPage : BasePage
+	{
+		public SettingPage()
+		{
+			InitializeComponent();
+		}
+
+		async void OnCloseClicked(object sender, EventArgs e)
+			=> await Navigation.PopModalAsync();
+	}
+}
