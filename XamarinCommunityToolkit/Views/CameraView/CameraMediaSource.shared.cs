7377e1f2edaea547098791eda325df65e04fc7a8
Gerald Versluis <gerald@verslu.is>
2020-08-20 22:39:06 +0200

    Implemented iOS

---
+++ XamarinCommunityToolkit/Views/CameraView/CameraMediaSource.shared.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin.CommunityToolkit.UI.Views
+{
+	// TODO: Check if we can do these changes in Xamarin.Forms to not have our own version of the MediaSource
+	public class CameraMediaSource : MediaSource
+	{
+		public static MediaSource FromStream(Func<Stream> stream) => new StreamMediaSource { Stream = token => Task.Run(stream, token) };
+
+		public static MediaSource FromResource(string resource, Type resolvingType)
+		{
+			return FromResource(resource, resolvingType.GetTypeInfo().Assembly);
+		}
+
+		public static MediaSource FromResource(string resource, Assembly sourceAssembly = null)
+		{
+#if NETSTANDARD2_0
+			sourceAssembly = sourceAssembly ?? Assembly.GetCallingAssembly();
+#else
+			if (sourceAssembly == null)
+			{
+				var callingAssemblyMethod = typeof(Assembly).GetTypeInfo().GetDeclaredMethod("GetCallingAssembly");
+				if (callingAssemblyMethod != null)
+				{
+					sourceAssembly = (Assembly)callingAssemblyMethod.Invoke(null, new object[0]);
+				}
+				else
+				{
+					Forms.Internals.Log.Warning("Warning", "Can not find CallingAssembly, pass resolvingType to FromResource to ensure proper resolution");
+					return null;
+				}
+			}
+#endif
+			return FromStream(() => sourceAssembly.GetManifestResourceStream(resource));
+		}
+	}
+}
---
+++ XamarinCommunityToolkit/Views/CameraView/StreamMediaSource.shared.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin.CommunityToolkit.UI.Views
+{
+	public class StreamMediaSource : MediaSource, IStreamImageSource
+	{
+		readonly object synchandle = new object();
+		CancellationTokenSource cancellationTokenSource;
+
+		TaskCompletionSource<bool> completionSource;
+
+		public static readonly BindableProperty StreamProperty = BindableProperty.Create(nameof(Stream), typeof(Func<CancellationToken, Task<Stream>>), typeof(StreamMediaSource),
+			default(Func<CancellationToken, Task<Stream>>));
+
+		protected CancellationTokenSource CancellationTokenSource
+		{
+			get => cancellationTokenSource;
+			private set
+			{
+				if (cancellationTokenSource == value)
+					return;
+				if (cancellationTokenSource != null)
+				{
+					cancellationTokenSource.Cancel();
+					cancellationTokenSource.Dispose();
+				}
+				cancellationTokenSource = value;
+			}
+		}
+
+		bool IsLoading => cancellationTokenSource != null;
+
+		public virtual Func<CancellationToken, Task<Stream>> Stream
+		{
+			get => (Func<CancellationToken, Task<Stream>>)GetValue(StreamProperty);
+			set => SetValue(StreamProperty, value);
+		}
+
+		protected override void OnPropertyChanged(string propertyName)
+		{
+			if (propertyName == StreamProperty.PropertyName)
+				OnSourceChanged();
+			base.OnPropertyChanged(propertyName);
+		} 
+
+		async Task<Stream> IStreamImageSource.GetStreamAsync(CancellationToken userToken)
+		{
+			if (Stream == null)
+				return null;
+
+			OnLoadingStarted();
+			userToken.Register(CancellationTokenSource.Cancel);
+			try
+			{
+				var stream = await Stream(CancellationTokenSource.Token);
+				OnLoadingCompleted(false);
+				return stream;
+			}
+			catch (OperationCanceledException)
+			{
+				OnLoadingCompleted(true);
+				throw;
+			}
+		}
+
+		protected void OnLoadingCompleted(bool cancelled)
+		{
+			if (!IsLoading || completionSource == null)
+				return;
+
+			var tcs = Interlocked.Exchange(ref completionSource, null);
+			if (tcs != null)
+				tcs.SetResult(cancelled);
+
+			lock (synchandle)
+			{
+				CancellationTokenSource = null;
+			}
+		}
+
+		protected void OnLoadingStarted()
+		{
+			lock (synchandle)
+			{
+				CancellationTokenSource = new CancellationTokenSource();
+			}
+		}
+
+		public virtual Task<bool> Cancel()
+		{
+			if (!IsLoading)
+				return Task.FromResult(false);
+
+			var tcs = new TaskCompletionSource<bool>();
+			var original = Interlocked.CompareExchange(ref completionSource, tcs, null);
+
+			if (original == null)
+			{
+				CancellationTokenSource = null;
+			}
+			else
+			{
+				tcs = original;
+			}
+
+			return tcs.Task;
+		}
+	}
+}
