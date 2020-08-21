ee659b950ac55b39dcdf24750d5c81fe93f79171
Andrei <andrei.misiukevich@gmail.com>
2020-08-21 15:55:18 +0300

    Update XamarinCommunityToolkitSample/Pages/Converters/MultiConverterPage.xaml.cs

--- XamarinCommunityToolkitSample/Pages/Converters/MultiConverterPage.xaml.cs
+++ XamarinCommunityToolkitSample/Pages/Converters/MultiConverterPage.xaml.cs
@@ -7,8 +7,6 @@ namespace Xamarin.CommunityToolkit.Sample.Pages.Converters
     public partial class MultiConverterPage 
     {
         public MultiConverterPage()
-        {
-            InitializeComponent();
-        }
+            => InitializeComponent();
     }
 }
