83b89da7deb1da0a7267602818661852190d419c
Andrei Misiukevich <andrei.misiukevich@gmail.com>
2020-08-26 02:01:21 +0300

    Added MultiValidationBehavior

---
+++ XamarinCommunityToolkit/Behaviors/Validators/MultiValidationBehavior.shared.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace Xamarin.CommunityToolkit.Behaviors
+{
+	[ContentProperty(nameof(Children))]
+	public class MultiValidationBehavior : ValidationBehavior
+	{
+		public static readonly BindableProperty ErrorsProperty =
+			BindableProperty.Create(nameof(Errors), typeof(List<object>), typeof(MultiValidationBehavior), null, BindingMode.OneWayToSource);
+
+		public static readonly BindableProperty ErrorProperty =
+			BindableProperty.CreateAttached(nameof(GetError), typeof(object), typeof(MultiValidationBehavior), null);
+
+		readonly ObservableCollection<ValidationBehavior> children
+			= new ObservableCollection<ValidationBehavior>();
+
+		public MultiValidationBehavior()
+			=> children.CollectionChanged += OnChildrenCollectionChanged;
+
+		public List<object> Errors
+		{
+			get => (List<object>)GetValue(ErrorProperty);
+			private set => SetValue(ErrorProperty, value);
+		}
+
+		public IList<ValidationBehavior> Children
+			=> children;
+
+		public static object GetError(BindableObject bindable)
+			=> bindable.GetValue(ErrorProperty);
+
+		public static void SetError(BindableObject bindable, object value)
+			=> bindable.SetValue(ErrorProperty, value);
+
+		protected override bool Validate(object value)
+		{
+			var errors = children.Where(c =>
+			{
+				c.Value = value;
+				c.ForceValidate();
+				return !c.IsValid;
+			}).Select(c => GetError(c));
+
+			if (!Errors?.SequenceEqual(errors) ?? false)
+				Errors = errors.ToList();
+
+			return !errors.Any();
+		}
+
+		void OnChildrenCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+		{
+			if (e.NewItems != null)
+				foreach (var child in e.NewItems.OfType<ValidationBehavior>())
+					child.SetBinding(BindingContextProperty, new Binding
+					{
+						Path = BindingContextProperty.PropertyName,
+						Source = this
+					});
+
+			if (e.OldItems != null)
+				foreach (var child in e.OldItems.OfType<ValidationBehavior>())
+					child.RemoveBinding(BindingContextProperty);
+		}
+	}
+}
