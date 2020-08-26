ca9a4255735d582e3d78ebdbef35fd02d08ba856
Steven Thewissen <github@thewissen.io>
2020-08-26 12:10:25 +0200

    Added macOS sample + target

---
+++ Xamarin.CommunityToolkit.Sample.macOS/AppDelegate.cs
@@ -0,0 +1,42 @@
+﻿using AppKit;
+using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace Xamarin.CommunityToolkit.Sample.macOS
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : FormsApplicationDelegate
+	{
+		NSWindow _window;
+
+		public override NSWindow MainWindow
+		{
+			get { return _window; }
+		}
+
+		public AppDelegate()
+		{
+			var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+			var rect = new CoreGraphics.CGRect(200, 200, 800, 600);
+			_window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+			_window.Title = "Xamarin Community Toolkit for macOS";
+		}
+
+		public override void DidFinishLaunching(NSNotification notification)
+		{
+			// Insert code here to initialize your application
+			global::Xamarin.Forms.Forms.SetFlags("CollectionView_Experimental");
+			global::Xamarin.Forms.Forms.Init();
+
+			LoadApplication(new Sample.App());
+			base.DidFinishLaunching(notification);
+		}
+
+		public override void WillTerminate(NSNotification notification)
+		{
+			// Insert code here to tear down your application
+		}
+	}
+}
