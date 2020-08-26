58598e10d420f9b8757442e1ac4906db4c70ddc7
Gerald Versluis <gerald@verslu.is>
2020-08-26 16:24:43 +0200

    Review feedback

--- XamarinCommunityToolkit.UnitTests/ConvertersNamespace_Tests.cs
+++ XamarinCommunityToolkit.UnitTests/ConvertersNamespace_Tests.cs
@@ -1,6 +1,9 @@
 ﻿using System.Linq;
 using System.Reflection;
 using Xamarin.CommunityToolkit.Behaviors;
+using Xamarin.CommunityToolkit.Converters;
+using Xamarin.CommunityToolkit.Effects;
+using Xamarin.CommunityToolkit.Extensions;
 using Xamarin.Forms;
 using Xunit;
 
@@ -11,7 +14,7 @@ namespace Xamarin.CommunityToolkit.UnitTests
 		[Fact]
 		public void MakeSureConvertersAreInTheRightNamespace()
 		{
-			var allTheTypes = Assembly.GetAssembly(typeof(BaseBehavior)).GetTypes();
+			var allTheTypes = Assembly.GetAssembly(typeof(InvertedBoolConverter)).GetTypes();
 			foreach (var type in allTheTypes.Where(t => t.Name.EndsWith("Converter") && t.GetInterface("IValueConverter") != null))
 			{
 				Assert.True(type.Namespace.Equals("Xamarin.CommunityToolkit.Converters"), $"{type.FullName} not in Xamarin.CommunityToolkit.Converters namespace");
@@ -21,7 +24,7 @@ namespace Xamarin.CommunityToolkit.UnitTests
 		[Fact]
 		public void MakeSureEffectsAreInTheRightNamespace()
 		{
-			var allTheTypes = Assembly.GetAssembly(typeof(BaseBehavior)).GetTypes();
+			var allTheTypes = Assembly.GetAssembly(typeof(SafeAreaEffect)).GetTypes();
 			foreach (var type in allTheTypes.Where(t => t.Name.EndsWith("Effect") && t.IsClass && t.IsSealed && t.IsAbstract))
 			{
 				Assert.True(type.Namespace.Equals("Xamarin.CommunityToolkit.Effects"), $"{type.FullName} not in Xamarin.CommunityToolkit.Effects namespace");
@@ -31,7 +34,7 @@ namespace Xamarin.CommunityToolkit.UnitTests
 		[Fact]
 		public void MakeSureMarkupExtensionsAreInTheRightNamespace()
 		{
-			var allTheTypes = Assembly.GetAssembly(typeof(BaseBehavior)).GetTypes();
+			var allTheTypes = Assembly.GetAssembly(typeof(TranslateExtension)).GetTypes();
 			foreach (var type in allTheTypes.Where(t => t.Name.EndsWith("Extension") && t.GetInterface("IMarkupExtension") != null))
 			{
 				Assert.True(type.Namespace.Equals("Xamarin.CommunityToolkit.Extensions"), $"{type.FullName} not in Xamarin.CommunityToolkit.Extensions namespace");
@@ -52,7 +55,7 @@ namespace Xamarin.CommunityToolkit.UnitTests
 		public void MakeSureViewsAreInTheRightNamespace()
 		{
 			var allTheTypes = Assembly.GetAssembly(typeof(BaseBehavior)).GetTypes();
-			foreach (var type in allTheTypes.Where(t => t.Name.EndsWith("View") && t.IsSubclassOf(typeof(View))))
+			foreach (var type in allTheTypes.Where(t => t.IsSubclassOf(typeof(View))))
 			{
 				Assert.True(type.Namespace.Equals("Xamarin.CommunityToolkit.UI.Views"), $"{type.FullName} not in Xamarin.CommunityToolkit.UI.Views namespace");
 			}
