5fc7179075fdd2b6761806b3d60924020e999ecc
Mark Allibone <m.allibone@rey-automation.ch>
2020-08-28 10:18:20 +0200

    Adds sample

---
+++ XamarinCommunityToolkitSample/Pages/Converters/DateTimeOffsetConverterPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace Xamarin.CommunityToolkit.Sample.Pages.Converters
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DateTimeOffsetConverterPage : BasePage
+	{
+		public DateTimeOffsetConverterPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
