4740075a037670654e984b60c709f83848e4cb32
John Bijsterbosch - John.NET Development <john.bijsterbosch@live.com>
2020-08-29 16:13:00 +0200

    Implemented UserStoppedTypingBehavior sample page and view model logic

--- XamarinCommunityToolkitSample/ViewModels/Behaviors/UserStoppedTypingBehaviorViewModel.cs
+++ XamarinCommunityToolkitSample/ViewModels/Behaviors/UserStoppedTypingBehaviorViewModel.cs
@@ -1,6 +1,32 @@
-﻿namespace Xamarin.CommunityToolkit.Sample.ViewModels.Behaviors
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Xamarin.CommunityToolkit.Sample.ViewModels.Behaviors
 {
 	public class UserStoppedTypingBehaviorViewModel : BaseViewModel
 	{
+		#region Properties
+
+		private string _performedSearches;
+		public string PerformedSearches
+		{
+			get => _performedSearches;
+			set => Set(ref _performedSearches, value);
+		}
+
+		public ICommand SearchCommand { get; }
+
+		#endregion Properties
+
+		public UserStoppedTypingBehaviorViewModel()
+		{
+			SearchCommand = new Command<string>(PerformSearch);
+		}
+
+		private void PerformSearch(string searchTerms)
+		{
+			PerformedSearches += $"Performed search for '{searchTerms}'{Environment.NewLine}";
+		}
 	}
 }
