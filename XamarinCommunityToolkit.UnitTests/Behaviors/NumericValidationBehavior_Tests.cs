f7c1c1713b241ecca478ada7b9d1e9a49c33713d
Bernd Hirschmann <bhi@guidnew.com>
2020-08-31 15:40:00 +0200

    use decimal separator from current culture

--- XamarinCommunityToolkit.UnitTests/Behaviors/NumericValidationBehavior_Tests.cs
+++ XamarinCommunityToolkit.UnitTests/Behaviors/NumericValidationBehavior_Tests.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.CommunityToolkit.Behaviors;
+﻿using System.Globalization;
+using Xamarin.CommunityToolkit.Behaviors;
 using Xamarin.CommunityToolkit.UnitTests.Mocks;
 using Xamarin.Forms;
 using Xunit;
@@ -12,39 +13,62 @@ namespace Xamarin.CommunityToolkit.UnitTests.Behaviors
 
 		[Theory]
 		// Positive
-		[InlineData("15.2", 1.0, 16.0, 0, 16, true)]
-		[InlineData("15.", 1.0, 16.0, 0, 1, true)]
-		[InlineData("15.88", 1.0, 16.0, 2, 2, true)]
-		[InlineData("0.99", 0.9, 2.0, 0, 16, true)]
-		[InlineData(".99", 0.9, 2.0, 0, 16, true)]
+		[InlineData("en-US", "15.2", 1.0, 16.0, 0, 16, true)]
+		[InlineData("en-US", "15.", 1.0, 16.0, 0, 1, true)]
+		[InlineData("en-US", "15.88", 1.0, 16.0, 2, 2, true)]
+		[InlineData("en-US", "0.99", 0.9, 2.0, 0, 16, true)]
+		[InlineData("en-US", ".99", 0.9, 2.0, 0, 16, true)]
+		[InlineData("de-DE", "15,2", 1.0, 16.0, 0, 16, true)]
+		[InlineData("de-DE", "15,", 1.0, 16.0, 0, 1, true)]
+		[InlineData("de-DE", "15,88", 1.0, 16.0, 2, 2, true)]
+		[InlineData("de-DE", "0,99", 0.9, 2.0, 0, 16, true)]
+		[InlineData("de-DE", ",99", 0.9, 2.0, 0, 16, true)]
 		// Negative
-		[InlineData("15.3", 16.0, 20.0, 0, 16, false)]
-		[InlineData("15.3", 0.0, 15.0, 0, 16, false)]
-		[InlineData("15.", 1.0, 16.0, 0, 0, false)]
-		[InlineData(".7", 0.0, 16.0, 0, 0, false)]
-		[InlineData("15", 1.0, 16.0, 1, 16, false)]
-		[InlineData("", 0.0, 16.0, 0, 16, false)]
-		[InlineData(" ", 0.0, 16.0, 0, 16, false)]
-		[InlineData(null, 0.0, 16.0, 0, 16, false)]
-		public void IsValid(string value, double minValue, double maxValue, int minDecimalPlaces, int maxDecimalPlaces, bool expectedValue)
+		[InlineData("en-US", "15.3", 16.0, 20.0, 0, 16, false)]
+		[InlineData("en-US", "15.3", 0.0, 15.0, 0, 16, false)]
+		[InlineData("en-US", "15.", 1.0, 16.0, 0, 0, false)]
+		[InlineData("en-US", ".7", 0.0, 16.0, 0, 0, false)]
+		[InlineData("en-US", "15", 1.0, 16.0, 1, 16, false)]
+		[InlineData("en-US", "", 0.0, 16.0, 0, 16, false)]
+		[InlineData("en-US", " ", 0.0, 16.0, 0, 16, false)]
+		[InlineData("en-US", null, 0.0, 16.0, 0, 16, false)]
+		[InlineData("de-DE", "15,3", 16.0, 20.0, 0, 16, false)]
+		[InlineData("de-DE", "15,3", 0.0, 15.0, 0, 16, false)]
+		[InlineData("de-DE", "15,", 1.0, 16.0, 0, 0, false)]
+		[InlineData("de-DE", ",7", 0.0, 16.0, 0, 0, false)]
+		[InlineData("de-DE", "15", 1.0, 16.0, 1, 16, false)]
+		[InlineData("de-DE", "", 0.0, 16.0, 0, 16, false)]
+		[InlineData("de-DE", " ", 0.0, 16.0, 0, 16, false)]
+		[InlineData("de-DE", null, 0.0, 16.0, 0, 16, false)]
+		public void IsValid(string culture, string value, double minValue, double maxValue, int minDecimalPlaces, int maxDecimalPlaces, bool expectedValue)
 		{
-			var behavior = new NumericValidationBehavior
+			var origCulture = CultureInfo.CurrentCulture;
+			CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo(culture);
+
+			try
 			{
-				MinimumValue = minValue,
-				MaximumValue = maxValue,
-				MinimumDecimalPlaces = minDecimalPlaces,
-				MaximumDecimalPlaces = maxDecimalPlaces
-			};
-			new Entry
-			{
-				Text = value,
-				Behaviors =
+				var behavior = new NumericValidationBehavior
+				{
+					MinimumValue = minValue,
+					MaximumValue = maxValue,
+					MinimumDecimalPlaces = minDecimalPlaces,
+					MaximumDecimalPlaces = maxDecimalPlaces
+				};
+				new Entry
+				{
+					Text = value,
+					Behaviors =
 				{
 					behavior
 				}
-			};
-			behavior.ForceValidate();
-			Assert.Equal(expectedValue, behavior.IsValid);
+				};
+				behavior.ForceValidate();
+				Assert.Equal(expectedValue, behavior.IsValid);
+			}
+			finally
+			{
+				CultureInfo.CurrentCulture = origCulture;
+			}
 		}
 	}
 }
