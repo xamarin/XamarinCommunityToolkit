88bef7390ecbbe0dc462cbaf3051ee5219fb90cc
Mark Allibone <mallibone@users.noreply.github.com>
2020-09-01 14:06:51 +0200

    Date time offset converter (#248)
    * Adds date time offset converter
    
    * Adds tests for DateTimeOffsetConverter
    
    * Adopts naming convention for shared code
    
    * Add xml doc and some more tests
    
    * Removes explicit private declarations
    
    * Update XamarinCommunityToolkit/Converters/DateTimeOffsetConverter.shared.cs
    
    * Update XamarinCommunityToolkit/Converters/DateTimeOffsetConverter.shared.cs
    
    * Update XamarinCommunityToolkit/Converters/DateTimeOffsetConverter.shared.cs
    
    * Update XamarinCommunityToolkit/Converters/DateTimeOffsetConverter.shared.cs
    
    * Adds sample
    
    * Update XamarinCommunityToolkitSample/Pages/Converters/DateTimeOffsetConverterPage.xaml.cs
    
    * Remove VS inserts from project file
    
    * Revert sln back to pre-VS Windows added lines
    
    Co-authored-by: Andrei <andrei.misiukevich@gmail.com>

---
+++ XamarinCommunityToolkitSample/Pages/Converters/DateTimeOffsetConverterPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace Xamarin.CommunityToolkit.Sample.Pages.Converters
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DateTimeOffsetConverterPage : BasePage
+	{
+		public DateTimeOffsetConverterPage()
+			=> InitializeComponent();
+	}
+}
