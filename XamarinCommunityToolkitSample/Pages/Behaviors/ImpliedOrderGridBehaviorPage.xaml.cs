af14a4a1c09186c715a52545cf3a7673cd4e63c9
Brian Macomber <brian@agspectrum.com>
2020-09-07 16:11:24 -0500

    Code behind formatting

--- XamarinCommunityToolkitSample/Pages/Behaviors/ImpliedOrderGridBehaviorPage.xaml.cs
+++ XamarinCommunityToolkitSample/Pages/Behaviors/ImpliedOrderGridBehaviorPage.xaml.cs
@@ -8,28 +8,19 @@ namespace Xamarin.CommunityToolkit.Sample.Pages.Behaviors
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ImpliedOrderGridBehaviorPage
 	{
-		public ImpliedOrderGridBehaviorPage()
-		{
-			InitializeComponent();
-		}
+		public ImpliedOrderGridBehaviorPage() => InitializeComponent();
 
-		private void ButtonAddRow_OnClicked(object sender, EventArgs e)
-		{
-			TestGrid.RowDefinitions.Add(new RowDefinition());
-		}
+		void ButtonAddRow_OnClicked(object sender, EventArgs e) => TestGrid.RowDefinitions.Add(new RowDefinition());
 
-		private void ButtonAddColumn_OnClicked(object sender, EventArgs e)
-		{
-			TestGrid.ColumnDefinitions.Add(new ColumnDefinition());
-		}
+		void ButtonAddColumn_OnClicked(object sender, EventArgs e) => TestGrid.ColumnDefinitions.Add(new ColumnDefinition());
 
-		private void ButtonRemoveRow_OnClicked(object sender, EventArgs e)
+		void ButtonRemoveRow_OnClicked(object sender, EventArgs e)
 		{
 			var lastRow = TestGrid.RowDefinitions.Last();
 			TestGrid.RowDefinitions.Remove(lastRow);
 		}
 
-		private void ButtonRemoveColumn_OnClicked(object sender, EventArgs e)
+		void ButtonRemoveColumn_OnClicked(object sender, EventArgs e)
 		{
 			var lastColumn = TestGrid.ColumnDefinitions.Last();
 			TestGrid.ColumnDefinitions.Remove(lastColumn);
