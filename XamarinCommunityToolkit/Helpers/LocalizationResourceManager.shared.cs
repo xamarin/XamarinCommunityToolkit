0cf3d9ee53cff787b34f672ca1b003878d43f0b4
Pedro Jesus <pedrojesus.cefet@gmail.com>
2020-09-16 20:08:04 -0300

    Changed Space to Tabs

--- XamarinCommunityToolkit/Helpers/LocalizationResourceManager.shared.cs
+++ XamarinCommunityToolkit/Helpers/LocalizationResourceManager.shared.cs
@@ -6,35 +6,35 @@ using System.Threading;
 namespace Xamarin.CommunityToolkit.Helpers
 {
 #if !NETSTANDARD1_0
-    public class LocalizationResourceManager : INotifyPropertyChanged
-    {
-        public static LocalizationResourceManager Current { get; } = new LocalizationResourceManager();
+	public class LocalizationResourceManager : INotifyPropertyChanged
+	{
+		public static LocalizationResourceManager Current { get; } = new LocalizationResourceManager();
 
-        ResourceManager resourceManager;
+		ResourceManager resourceManager;
 
-        public void Init(ResourceManager resource)
+		public void Init(ResourceManager resource)
 		{
-            resourceManager = resource;
-        }
+			resourceManager = resource;
+		}
 
-        public string this[string text] => resourceManager.GetString(text, CurrentCulture);
+		public string this[string text] => resourceManager.GetString(text, CurrentCulture);
 
-        public void SetCulture(CultureInfo language)
-        {
-            Thread.CurrentThread.CurrentUICulture = language;
-            Invalidate();
-        }
+		public void SetCulture(CultureInfo language)
+		{
+			Thread.CurrentThread.CurrentUICulture = language;
+			Invalidate();
+		}
 
-        public string GetValue(string text) => resourceManager.GetString(text, CultureInfo.CurrentCulture);
+		public string GetValue(string text) => resourceManager.GetString(text, CultureInfo.CurrentCulture);
 
-        public CultureInfo CurrentCulture => Thread.CurrentThread.CurrentUICulture;
+		public CultureInfo CurrentCulture => Thread.CurrentThread.CurrentUICulture;
 
-        public event PropertyChangedEventHandler PropertyChanged;
+		public event PropertyChangedEventHandler PropertyChanged;
 
-        public void Invalidate()
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
-        }
-    }
+		public void Invalidate()
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+		}
+	}
 #endif
 }
