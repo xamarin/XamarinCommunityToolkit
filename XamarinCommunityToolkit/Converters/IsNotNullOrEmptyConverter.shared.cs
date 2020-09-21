6433b352edcf0ebe9fe954130c29ea73fd982baf
Ieuan Walker <ieuan.walker007@gmail.com>
2020-09-21 21:36:49 +0100

    Reverted code

--- XamarinCommunityToolkit/Converters/IsNotNullOrEmptyConverter.shared.cs
+++ XamarinCommunityToolkit/Converters/IsNotNullOrEmptyConverter.shared.cs
@@ -18,13 +18,8 @@ namespace Xamarin.CommunityToolkit.Converters
 		/// <param name="parameter">Additional parameter for the converter to handle. Not implemented.</param>
 		/// <param name="culture">The culture to use in the converter.</param>
 		/// <returns>A boolean indicating if the incoming value is not null and not empty.</returns>
-		public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
-			value switch
-			{
-				null => false,
-				string x => !string.IsNullOrWhiteSpace(x),
-				_ => true,
-			};
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+			=> value is string str ? !string.IsNullOrWhiteSpace(str) : value != null;
 
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
 			=> throw new NotImplementedException();
