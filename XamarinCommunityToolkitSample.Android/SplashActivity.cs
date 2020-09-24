cbff475b51bfbc185953c4c1519188ad80555efe
Aden Earnshaw <aearnshaw@outlook.com>
2020-09-24 09:23:29 +0100

    Adding Android SplashScreen

---
+++ XamarinCommunityToolkitSample.Android/SplashActivity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace Xamarin.CommunityToolkit.Sample.Droid
+{
+	[Activity(Label = "XamarinCommunityToolkitSample", Icon = "@mipmap/icon", Theme = "@style/SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class SplashActivity : AppCompatActivity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			var intent = new Intent(this, typeof(MainActivity));
+			StartActivity(intent);
+			Finish();
+		}
+	}
+}
