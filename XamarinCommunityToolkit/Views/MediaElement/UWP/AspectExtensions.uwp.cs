481c5f8310105f5e2134672a6765ecb3caf33be0
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-09-28 14:43:45 +0200

    Added UWP MediaElementRenderer

---
+++ XamarinCommunityToolkit/Views/MediaElement/UWP/AspectExtensions.uwp.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+using WStretch = Windows.UI.Xaml.Media.Stretch;
+
+namespace Xamarin.CommunityToolkit.UI.Views
+{
+	public static class AspectExtensions
+	{
+		public static WStretch ToStretch(this Aspect aspect)
+		{
+			switch (aspect)
+			{
+				case Aspect.Fill:
+					return WStretch.Fill;
+
+				case Aspect.AspectFill:
+					return WStretch.UniformToFill;
+
+				case Aspect.AspectFit:
+				default:
+					return WStretch.Uniform;
+			}
+		}
+	}
+}
