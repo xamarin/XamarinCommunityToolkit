cac1e889d83a776c8160f49aebd4348784f20650
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-09-28 13:32:26 +0200

    Added MediaElement Android and iOS implementation

---
+++ XamarinCommunityToolkit/Views/MediaElement/UriTypeConverter.shared.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin.CommunityToolkit.UI.Views
+{
+	[TypeConversion(typeof(Uri))]
+	public class UriTypeConverter : TypeConverter
+	{
+		public override object ConvertFromInvariantString(string value)
+		{
+			if (string.IsNullOrWhiteSpace(value))
+				return null;
+			return new Uri(value, UriKind.RelativeOrAbsolute);
+		}
+
+		bool CanConvert(Type type)
+		{
+			if (type == typeof(string))
+				return true;
+			if (type == typeof(Uri))
+				return true;
+
+			return false;
+		}
+	}
+}
