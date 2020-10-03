47b9ac474392c4c10db29dff689fa6861b3e085b
Depechie <glenn.versweyveld@gmail.com>
2020-10-03 16:34:33 +0200

    Fixes remarks on PR

--- XamarinCommunityToolkit/AttachedProperties/ProgressBarAttachedProperties.shared.cs
+++ XamarinCommunityToolkit/AttachedProperties/ProgressBarAttachedProperties.shared.cs
@@ -5,12 +5,11 @@ namespace Xamarin.CommunityToolkit.AttachedProperties
 {
 	public static class ProgressBarAttachedProperties
 	{
-        public static BindableProperty AnimatedProgressProperty = BindableProperty.CreateAttached("AnimatedProgress", typeof(double), typeof(ProgressBar), 0.0d,BindingMode.OneWay, propertyChanged: (b, o, n) => ProgressBarProgressChanged((ProgressBar)b, (double)n));
+        public static BindableProperty AnimatedProgressProperty = BindableProperty.CreateAttached("AnimatedProgress", typeof(double), typeof(ProgressBar), 0.0d, BindingMode.OneWay, propertyChanged: (b, o, n) => ProgressBarProgressChanged((ProgressBar)b, (double)n));
 
         static void ProgressBarProgressChanged(ProgressBar progressBar, double progress)
         {
             ViewExtensions.CancelAnimations(progressBar);
-
             progressBar.ProgressTo(progress, Convert.ToUInt32(Math.Max(0, 500)), Easing.Linear);
         }
     }
---
+++ XamarinCommunityToolkitSample/ViewModels/AttachedProperties/AttachedPropertiesGalleryViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Xamarin.CommunityToolkit.Sample.Models;
+using Xamarin.CommunityToolkit.Sample.Pages.AttachedProperties;
+using Xamarin.CommunityToolkit.Sample.Resx;
+using Xamarin.Forms;
+
+namespace Xamarin.CommunityToolkit.Sample.ViewModels.AttachedProperties
+{
+	public class AttachedPropertiesGalleryViewModel : BaseViewModel
+	{
+		public IEnumerable<SectionModel> Items { get; } = new List<SectionModel>
+		{
+			new SectionModel(
+				typeof(ProgressBarAttachedPropertiesPage),
+				"ProgressBarAttachedProperties",
+				Color.FromHex("#498205"),
+				AppResources.ProgressBarAttachedPropertiesShortDescription)
+		};
+	}
+}
