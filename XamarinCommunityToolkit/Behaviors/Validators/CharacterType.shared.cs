51da14ed6eaeed47c228ea35010ab4393a8cd0b7
Andrei Misiukevich <andrei.misiukevich@gmail.com>
2020-10-03 21:43:38 +0300

    Added TextCharactersValidationBehavior

---
+++ XamarinCommunityToolkit/Behaviors/Validators/CharacterType.shared.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Xamarin.CommunityToolkit.Behaviors
+{
+	[Flags]
+	public enum CharacterType
+	{
+		LowerLetter = 1,
+		UpperLetter = 2,
+		Letter = LowerLetter | UpperLetter,
+		Digit = 4,
+		LetterOrDigit = Letter | Digit,
+		Symbol = 8,
+		WhiteSpace = 16,
+		Any = LetterOrDigit | Symbol | WhiteSpace
+	}
+}
---
+++ XamarinCommunityToolkit/Behaviors/Validators/TextCharactersValidationBehavior.shared.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace Xamarin.CommunityToolkit.Behaviors
+{
+	public class TextCharactersValidationBehavior : TextValidationBehavior
+	{
+		IEnumerable<Predicate<char>> characterPredicates;
+
+		public static readonly BindableProperty CharacterTypeProperty =
+			BindableProperty.Create(nameof(CharacterType), typeof(CharacterType), typeof(TextCharactersValidationBehavior), CharacterType.Any, propertyChanged: OnCharacterTypePropertyChanged);
+
+		public static readonly BindableProperty MinimumCharactersNumberProperty =
+			BindableProperty.Create(nameof(MinimumCharactersNumber), typeof(int), typeof(TextCharactersValidationBehavior), 0, propertyChanged: OnValidationPropertyChanged);
+
+		public static readonly BindableProperty MaximumCharactersNumberProperty =
+			BindableProperty.Create(nameof(MaximumCharactersNumber), typeof(int), typeof(TextCharactersValidationBehavior), int.MaxValue, propertyChanged: OnValidationPropertyChanged);
+
+		public TextCharactersValidationBehavior()
+			=> OnCharacterTypePropertyChanged();
+
+		public CharacterType CharacterType
+		{
+			get => (CharacterType)GetValue(CharacterTypeProperty);
+			set => SetValue(CharacterTypeProperty, value);
+		}
+
+		public int MinimumCharactersNumber
+		{
+			get => (int)GetValue(MinimumCharactersNumberProperty);
+			set => SetValue(MinimumCharactersNumberProperty, value);
+		}
+
+		public int MaximumCharactersNumber
+		{
+			get => (int)GetValue(MaximumCharactersNumberProperty);
+			set => SetValue(MaximumCharactersNumberProperty, value);
+		}
+
+		protected override bool Validate(object value)
+			=> base.Validate(value) && Validate(value?.ToString());
+
+		static void OnCharacterTypePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+		{
+			((TextCharactersValidationBehavior)bindable).OnCharacterTypePropertyChanged();
+			OnValidationPropertyChanged(bindable, oldValue, newValue);
+		}
+
+		void OnCharacterTypePropertyChanged()
+			=> characterPredicates = GetCharacterPredicates();
+
+		IEnumerable<Predicate<char>> GetCharacterPredicates()
+		{
+			if (CharacterType.HasFlag(CharacterType.LowerLetter))
+				yield return char.IsLower;
+
+			if (CharacterType.HasFlag(CharacterType.UpperLetter))
+				yield return char.IsUpper;
+
+			if (CharacterType.HasFlag(CharacterType.Digit))
+				yield return char.IsDigit;
+
+			if (CharacterType.HasFlag(CharacterType.Symbol))
+				yield return char.IsSymbol;
+
+			if (CharacterType.HasFlag(CharacterType.WhiteSpace))
+				yield return char.IsWhiteSpace;
+		}
+
+		bool Validate(string value)
+		{
+			var count = value?.ToCharArray().Count(character => characterPredicates.Any(predicate => predicate.Invoke(character))) ?? 0;
+			return count >= MinimumCharactersNumber
+				&& count <= MaximumCharactersNumber;
+		}
+	}
+}
