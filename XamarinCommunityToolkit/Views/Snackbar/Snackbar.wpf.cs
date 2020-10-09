0ce43f7e013ecf17cab7f0a96973b59f92f7298b
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2020-10-09 12:07:57 +0300

    Combine UWP and WPF

--- XamarinCommunityToolkit/Views/Snackbar/Snackbar.wpf.cs
+++ XamarinCommunityToolkit/Views/Snackbar/Snackbar.wpf.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.WPF.Controls;
-using System.Windows.Controls;
 using Xamarin.Forms.Platform.WPF.Helpers;
-using RowDefinition = System.Windows.Controls.RowDefinition;
-using ColumnDefinition = System.Windows.Controls.ColumnDefinition;
-using Label = System.Windows.Controls.Label;
-using Grid = System.Windows.Controls.Grid;
-using Button = System.Windows.Controls.Button;
+using Xamarin.CommunityToolkit.UI.Views.Helpers;
 
 namespace Xamarin.CommunityToolkit.UI.Views
 {
@@ -17,11 +10,11 @@ namespace Xamarin.CommunityToolkit.UI.Views
 	{
 		Timer snackbarTimer;
 
-		internal void Show(Xamarin.Forms.Page page, SnackbarArguments arguments)
+		internal void Show(Page page, SnackbarArguments arguments)
 		{
 			var formsAppBar = System.Windows.Application.Current.MainWindow.FindChild<FormsAppBar>("PART_BottomAppBar");
 			var currentContent = formsAppBar.Content;
-			var snackBar = new SnackbarWpfLayout(arguments.Message, arguments.ActionButtonText, arguments.Action);
+			var snackBar = new SnackbarLayout(arguments.Message, arguments.ActionButtonText, arguments.Action);
 			snackbarTimer = new Timer { Interval = arguments.Duration };
 			snackbarTimer.Tick += (sender, e) =>
 			{
@@ -37,37 +30,6 @@ namespace Xamarin.CommunityToolkit.UI.Views
 			};
 			snackbarTimer.Start();
 			formsAppBar.Content = snackBar;
-		}
-
-		class SnackbarWpfLayout : Grid
-		{
-			public SnackbarWpfLayout(string message, string actionButtonText, Func<Task> action)
-			{
-				RowDefinitions.Add(new RowDefinition());
-				ColumnDefinitions.Add(new ColumnDefinition());
-				ColumnDefinitions.Add(new ColumnDefinition() { Width = System.Windows.GridLength.Auto });
-				var messageLabel = new Label() { Content = message };
-				Children.Add(messageLabel);
-				SetRow(messageLabel, 0);
-				SetColumn(messageLabel, 0);
-				if (!string.IsNullOrEmpty(actionButtonText) && action != null)
-				{
-					var button = new Button
-					{
-						Content = actionButtonText,
-						Command = new Command(async () =>
-						{
-							OnSnackbarActionExecuted?.Invoke();
-							await action();
-						})
-					};
-					Children.Add(button);
-					SetRow(button, 0);
-					SetColumn(button, 1);
-				}
-			}
-
-			public Action OnSnackbarActionExecuted;
 		}
 	}
 }
