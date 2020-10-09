aa58a1511603cda2832212d834ee1a01c8e814e3
Pedro Jesus <pedrojesus.cefet@gmail.com>
2020-10-09 20:06:18 -0300

    Update XamarinCommunityToolkit/Core/MediaSourceConverter.shared.cs
    Co-authored-by: Andrei <andrei.misiukevich@gmail.com>

--- XamarinCommunityToolkit/Core/MediaSourceConverter.shared.cs
+++ XamarinCommunityToolkit/Core/MediaSourceConverter.shared.cs
@@ -8,10 +8,12 @@ namespace Xamarin.CommunityToolkit.Core
 	{
 		public override object ConvertFromInvariantString(string value)
 		{
-			if (value != null)
-				return Uri.TryCreate(value, UriKind.Absolute, out var uri) && uri.Scheme != "file" ? MediaSource.FromUri(uri) : MediaSource.FromFile(value);
-
-			throw new InvalidOperationException($"Cannot convert \"{value}\" into {typeof(MediaSource)}");
+			if (value == null)
+				throw new InvalidOperationException($"Cannot convert \"{value}\" into {typeof(MediaSource)}");
+				
+			return Uri.TryCreate(value, UriKind.Absolute, out var uri) && uri.Scheme != "file" 
+				? MediaSource.FromUri(uri) 
+				: MediaSource.FromFile(value);
 		}
 	}
 }
