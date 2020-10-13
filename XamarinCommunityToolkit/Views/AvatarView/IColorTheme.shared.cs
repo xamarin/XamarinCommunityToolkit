d7443eb96b33e0e6121aa30e3eae76b44fad08a2
Axemasta <33064621+Axemasta@users.noreply.github.com>
2020-10-13 13:32:50 +0100

    Added ColorTheme Property To AvatarView (#435)
    * Added Color Themes To AvatarView
    
    Added a new bindable property to avatar view, IColorTheme. This allows a developer to use a prepackages color theme that changes based on the text in the view. Alternatively a developer can implement their own custom theme using the ColorTheme class.
    
    * Removed unused using statements
    
    Forgot to cleanup unused using statements in previous commit 🙉
    
    * Implemented code review feedback
    
    * Removed avatar view references from csproj
    
    * Update XamarinCommunityToolkit/Views/AvatarView/AvatarView.shared.cs
    
    * Update XamarinCommunityToolkit/Views/AvatarView/AvatarView.shared.cs
    
    * Update XamarinCommunityToolkit/Views/AvatarView/AvatarView.shared.cs
    
    * Update XamarinCommunityToolkit/Views/AvatarView/AvatarView.shared.cs
    
    * Update XamarinCommunityToolkit/Views/AvatarView/ColorTheme.shared.cs
    
    * Update XamarinCommunityToolkit/Views/AvatarView/ColorTheme.shared.cs
    
    * More cleanup of ColorTheme.shared.cs
    
    * Readded pictures
    
    * Update XamarinCommunityToolkit/Views/AvatarView/ColorTheme.shared.cs
    
    * Update XamarinCommunityToolkit/Views/AvatarView/ColorTheme.shared.cs
    
    Co-authored-by: Andrei <andrei.misiukevich@gmail.com>

---
+++ XamarinCommunityToolkit/Views/AvatarView/IColorTheme.shared.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Xamarin.CommunityToolkit.UI.Views
+{
+	public interface IColorTheme
+	{
+		Color GetForegroundColor(string text);
+
+		Color GetBackgroundColor(string text);
+	}
+}
