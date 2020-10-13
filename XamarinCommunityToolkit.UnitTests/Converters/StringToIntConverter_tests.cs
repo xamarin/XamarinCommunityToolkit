b17df662a2aed09e45a856e2dd378cb8b4adfa23
Nour ElGafy <Nour-Elgafy@outlook.com>
2020-10-13 18:54:00 +0200

    String to int Converter

---
+++ XamarinCommunityToolkit.UnitTests/Converters/StringToIntConverter_tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.CommunityToolkit.Converters;
+using Xunit;
+
+namespace Xamarin.CommunityToolkit.UnitTests.Converters
+{
+	public class StringToIntConverter_tests
+	{
+		[Theory]
+		[InlineData("2", 2)]
+		[InlineData("3", 3)]
+		[InlineData("255", 255)]
+		[InlineData("99191", 99191)]
+		public void StringToIntConverter(string value, int expectedResult)
+		{
+			var stringToIntConverter = new StringToIntConverter();
+
+			var result = stringToIntConverter.Convert(value, typeof(StringToIntConverter_tests), null, CultureInfo.CurrentCulture);
+
+			Assert.Equal(result, expectedResult);
+		}
+
+		[Theory]
+		[InlineData(2, "2")]
+		[InlineData(3, "3")]
+		[InlineData(255, "255")]
+		[InlineData(99191, "99191")]
+		public void IntToStringConverter(int value, string expectedResult)
+		{
+			var stringToIntConverter = new StringToIntConverter();
+
+			var result = stringToIntConverter.ConvertBack(value, typeof(StringToIntConverter_tests), null, CultureInfo.CurrentCulture);
+
+			Assert.Equal(result, expectedResult);
+		}
+	}
+}
---
+++ XamarinCommunityToolkit/Converters/StringToIntConverter.shared.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.CommunityToolkit.Extensions;
+using Xamarin.Forms;
+
+namespace Xamarin.CommunityToolkit.Converters
+{
+	/// <summary>
+	/// Converts The Provided string into the equivlant intger.
+	/// </summary>
+	public class StringToIntConverter : ValueConverterExtension, IValueConverter
+	{
+		/// <summary>
+		/// Converts The Provided string into the equivlant intger.
+		/// </summary>
+		/// <param name="value">The value to convert.</param>
+		/// <param name="targetType">The type of the binding target property.</param>
+		/// <param name="parameter">Additional parameter for the converter to handle. Not implemented.</param>
+		/// <param name="culture">The culture to use in the converter.</param>
+		/// <returns>The Intger value of the provided string</returns>
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+			=> value is string numstr && !string.IsNullOrWhiteSpace(numstr) ? System.Convert.ToInt32(numstr) : throw new ArgumentException("Value is not a valid integer", nameof(value));
+
+		/// <summary>
+		/// Converts The Provided intger into the equivlant string.
+		/// </summary>
+		/// <param name="value">The value to convert.</param>
+		/// <param name="targetType">The type of the binding target property.</param>
+		/// <param name="parameter">Additional parameter for the converter to handle. Not implemented.</param>
+		/// <param name="culture">The culture to use in the converter.</param>
+		/// <returns>The string version of the provided int</returns>
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+			=> value is int num ? $"{num}" : throw new ArgumentException("Value is not a valid integer", nameof(value));
+	}
+}
