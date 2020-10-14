ae8b56a20f27bf0d2232e0ed04fcdbd712e344a0
Andrei <andrei.misiukevich@gmail.com>
2020-10-15 02:51:02 +0300

    Update XamarinCommunityToolkit/Converters/StringToDoubleConverter.shared.cs

--- XamarinCommunityToolkit/Converters/StringToDoubleConverter.shared.cs
+++ XamarinCommunityToolkit/Converters/StringToDoubleConverter.shared.cs
@@ -11,7 +11,7 @@ namespace Xamarin.CommunityToolkit.Converters
 	public class StringToDoubleConverter : ValueConverterExtension, IValueConverter
 	{
 		/// <summary>
-		///     Converts The Provided string into the equivalent double.
+		/// Converts The Provided string into the equivalent double.
 		/// </summary>
 		/// <param name="value">The value to convert.</param>
 		/// <param name="targetType">The type of the binding target property.</param>
