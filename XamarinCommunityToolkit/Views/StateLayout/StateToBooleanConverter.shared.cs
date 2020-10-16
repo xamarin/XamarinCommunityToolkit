a9d585924e78999266ff4c56ef6fa932cfb1adfb
Steven Thewissen <github@thewissen.io>
2020-10-16 14:33:28 +0200

    Added the StateLayout bits

---
+++ XamarinCommunityToolkit/Views/StateLayout/StateToBooleanConverter.shared.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+
+namespace Xamarin.Forms.StateSquid
+{
+	public class StateToBooleanConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+			=> value is State state && parameter is State stateToCompare && state == stateToCompare;
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+			=> State.None;
+	}
+}
