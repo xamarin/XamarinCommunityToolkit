8266f3964d8e9518ed3cb867e6c561ce05af237e
Andrei <andrei.misiukevich@gmail.com>
2020-10-18 20:19:49 +0300

    Update XamarinCommunityToolkit/Views/AvatarView/ImageSourceValidator.shared.cs

--- XamarinCommunityToolkit/Views/AvatarView/ImageSourceValidator.shared.cs
+++ XamarinCommunityToolkit/Views/AvatarView/ImageSourceValidator.shared.cs
@@ -6,7 +6,7 @@ namespace Xamarin.CommunityToolkit.UI.Views
 #if NETSTANDARD || __TVOS__ || __WATCHOS__
 	class ImageSourceValidator : IImageSourceValidator
 	{
-		// we should still try to load image
+		// We should still try to load an image
 		public Task<bool> IsImageSourceValidAsync(ImageSource source) => Task.FromResult(true);
 	}
 #endif
