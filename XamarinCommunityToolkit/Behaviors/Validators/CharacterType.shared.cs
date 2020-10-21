b983c2d0916b0fd893b95a1798e3a9cc4d86a0c3
Andrei <andrei.misiukevich@gmail.com>
2020-10-21 17:00:51 +0300

    Added TextCharactersValidationBehavior (#363)
    * Added TextCharactersValidationBehavior
    
    * Added unit tests
    
    * Fixed props naming
    
    * Update names. Added basic latin letters
    
    * Fixed build on Mac
    
    * Fixed warning
    
    * Removed redundant conditions
    
    * Restored removed line
    
    * Renamed class
    
    * Updated character type

---
+++ XamarinCommunityToolkit/Behaviors/Validators/CharacterType.shared.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Xamarin.CommunityToolkit.Behaviors
+{
+	[Flags]
+	public enum CharacterType
+	{
+		LowercaseLetter = 1,
+		UppercaseLetter = 2,
+		Letter = LowercaseLetter | UppercaseLetter,
+		Digit = 4,
+		Alphanumeric = Letter | Digit,
+		Whitespace = 8,
+		NonAlphanumericSymbol = 16,
+		LowercaseLatinLetter = 32,
+		UppercaseLatinLetter = 64,
+		LatinLetter = LowercaseLatinLetter | UppercaseLatinLetter,
+		Any = Alphanumeric | NonAlphanumericSymbol | Whitespace
+	}
+}
