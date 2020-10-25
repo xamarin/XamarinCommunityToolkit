824a45ac5bb8b65d56be1d82ca631dabc58d61b4
Andrei <andrei.misiukevich@gmail.com>
2020-10-26 02:13:20 +0300

    Sample app: Implemented filter for gallery pages (#495)

---
+++ XamarinCommunityToolkitSample/ViewModels/Base/BaseGalleryViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using Xamarin.CommunityToolkit.ObjectModel;
+using Xamarin.CommunityToolkit.Sample.Models;
+using Xamarin.Forms;
+
+namespace Xamarin.CommunityToolkit.Sample.ViewModels
+{
+	public abstract class BaseGalleryViewModel : BaseViewModel
+	{
+		ICommand filterCommand;
+
+		public BaseGalleryViewModel() => Filter();
+
+		public abstract IEnumerable<SectionModel> Items { get; }
+
+		public IEnumerable<SectionModel> FilteredItems { get; private set; }
+
+		public ICommand FilterCommand => filterCommand ??= new Command(Filter);
+
+		public string FilterValue { private get; set; }
+
+		void Filter()
+		{
+			FilterValue ??= string.Empty;
+			FilteredItems = Items.Where(item => item.Title.IndexOf(FilterValue, StringComparison.InvariantCultureIgnoreCase) >= 0);
+			OnPropertyChanged(nameof(FilteredItems));
+		}
+	}
+}
