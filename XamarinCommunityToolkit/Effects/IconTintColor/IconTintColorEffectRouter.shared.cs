277e79f7f05da13dd1d58d012390eaedaa8d9340
John Bijsterbosch - John.NET Development <john.bijsterbosch@live.com>
2020-10-30 15:17:46 +0100

    Implemented IconTintColorEffect for Android

---
+++ XamarinCommunityToolkit/Effects/IconTintColor/IconTintColorEffectRouter.shared.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+
+namespace Xamarin.CommunityToolkit.Effects
+{
+	public class IconTintColorEffectRouter : RoutingEffect
+	{
+		public IconTintColorEffectRouter()
+			: base($"{nameof(CommunityToolkit)}.{nameof(IconTintColorEffectRouter)}")
+		{
+#if MONOANDROID
+			if (System.DateTime.Now.Ticks < 0)
+				_ = new Xamarin.CommunityToolkit.Android.Effects.IconTintColorEffectRouter();
+#endif
+		}
+	}
+}
