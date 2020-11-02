016ada38366f53df617e94b8044f6e9ea2a59b19
Vincent Hoogendoorn <dev@vincenth.net>
2020-11-02 14:28:06 +0100

    add MarkupSample solution

---
+++ Xamarin.CommunityToolkit.MarkupSample/Xamarin.CommunityToolkit.MarkupSample/MainPage.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+using Xamarin.CommunityToolkit.Markup;
+
+namespace Xamarin.CommunityToolkit.MarkupSample
+{
+	public class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			Content = new Label { Text = "Hi" } .FontSize(30) .Italic() .Center();
+		}
+	}
+}
