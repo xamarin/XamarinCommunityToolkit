0514c55d823237acb2db7dc9809d67424c6e5038
Javier Suárez <javiersuarezruiz@hotmail.com>
2020-11-06 11:43:07 +0100

    TabView (#525)
    * Added TabView
    
    * Added TabView samples
    Added new effects
    
    * Added more TabView samples
    
    * Updated MaterialTabViewItemTemplate converter
    
    * Updated helpers classes accesibility
    
    Co-authored-by: Gerald Versluis <gerald.versluis@microsoft.com>

---
+++ XamarinCommunityToolkit/Effects/VisualFeedback/VisualFeedbackEffectRouter.android.cs
@@ -0,0 +1,188 @@
+﻿using Android.Content.Res;
+using Android.Graphics.Drawables;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.CommunityToolkit.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using AColor = Android.Graphics.Color;
+using AView = Android.Views.View;
+using Effects = Xamarin.CommunityToolkit.Android.Effects;
+
+[assembly: ExportEffect(typeof(Effects.VisualFeedbackEffectRouter), nameof(VisualFeedbackEffect))]
+
+namespace Xamarin.CommunityToolkit.Android.Effects
+{
+	[Preserve(AllMembers = true)]
+	public class VisualFeedbackEffectRouter : PlatformEffect
+	{
+		AView view;
+		RippleDrawable ripple;
+		Drawable orgDrawable;
+		FrameLayout rippleOverlay;
+		FastRendererOnLayoutChangeListener fastListener;
+
+		bool IsClickable => !(Element is Layout || Element is BoxView);
+
+		protected override void OnAttached()
+		{
+			view = Control ?? Container;
+
+			SetUpRipple();
+
+			if (IsClickable)
+				view.Touch += OnViewTouch;
+
+			UpdateEffectColor();
+		}
+
+		protected override void OnDetached()
+		{
+			if (!IsClickable)
+			{
+				view.Touch -= OnOverlayTouch;
+				view.RemoveOnLayoutChangeListener(fastListener);
+
+				fastListener.Dispose();
+				fastListener = null;
+				rippleOverlay.Dispose();
+				rippleOverlay = null;
+			}
+			else
+			{
+				view.Touch -= OnViewTouch;
+				view.Background = orgDrawable;
+				orgDrawable = null;
+			}
+
+			ripple?.Dispose();
+			ripple = null;
+
+			view = null;
+		}
+
+		protected override void OnElementPropertyChanged(System.ComponentModel.PropertyChangedEventArgs args)
+		{
+			base.OnElementPropertyChanged(args);
+
+			if (args.PropertyName == VisualFeedbackEffect.FeedbackColorProperty.PropertyName)
+			{
+				UpdateEffectColor();
+			}
+		}
+
+		void UpdateEffectColor()
+		{
+			var color = VisualFeedbackEffect.GetFeedbackColor(Element);
+
+			var nativeColor = color.ToAndroid();
+			nativeColor.A = 80;
+
+			ripple?.SetColor(GetPressedColorSelector(nativeColor));
+		}
+
+		void SetUpRipple()
+		{
+			ripple = CreateRipple(AColor.Transparent);
+
+			if (!IsClickable)
+			{
+				rippleOverlay = new FrameLayout(view.Context)
+				{
+					Clickable = true,
+					LongClickable = true,
+					Foreground = ripple
+				};
+				fastListener = new FastRendererOnLayoutChangeListener(this);
+				view.AddOnLayoutChangeListener(fastListener);
+				view.RequestLayout();
+			}
+			else
+			{
+				orgDrawable = view.Background;
+				view.Background = ripple;
+			}
+		}
+
+		void SetUpOverlay()
+		{
+			var parent = view.Parent as ViewGroup;
+
+			parent.AddView(rippleOverlay);
+
+			rippleOverlay.BringToFront();
+			rippleOverlay.Touch += OnOverlayTouch;
+		}
+
+		void OnViewTouch(object sender, AView.TouchEventArgs e) => e.Handled = false;
+
+		void OnOverlayTouch(object sender, AView.TouchEventArgs e)
+		{
+			view?.DispatchTouchEvent(e.Event);
+
+			e.Handled = false;
+		}
+
+		RippleDrawable CreateRipple(AColor color)
+		{
+			if (!IsClickable)
+			{
+				var mask = new ColorDrawable(AColor.White);
+				return new RippleDrawable(GetPressedColorSelector(color), null, mask);
+			}
+
+			var back = view.Background;
+
+			if (back == null)
+			{
+				var mask = new ColorDrawable(AColor.White);
+				return new RippleDrawable(GetPressedColorSelector(color), null, mask);
+			}
+			else
+				return new RippleDrawable(GetPressedColorSelector(color), back, null);
+		}
+
+		ColorStateList GetPressedColorSelector(int pressedColor) => new ColorStateList(
+				new int[][]
+				{
+					new int[] { }
+				},
+				new int[]
+				{
+					pressedColor
+				});
+
+		internal class FastRendererOnLayoutChangeListener : Java.Lang.Object, AView.IOnLayoutChangeListener
+		{
+			bool hasParent = false;
+			VisualFeedbackEffectRouter effect;
+
+			public FastRendererOnLayoutChangeListener(VisualFeedbackEffectRouter effect)
+			{
+				this.effect = effect;
+			}
+
+			public void OnLayoutChange(AView v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom)
+			{
+				effect.rippleOverlay.Layout(v.Left, v.Top, v.Right, v.Bottom);
+
+				if (hasParent)
+				{
+					return;
+				}
+
+				hasParent = true;
+				effect.SetUpOverlay();
+			}
+
+			protected override void Dispose(bool disposing)
+			{
+				if (disposing)
+					effect = null;
+
+				base.Dispose(disposing);
+			}
+		}
+	}
+}
---
+++ XamarinCommunityToolkitSample/ViewModels/Views/TabItemsSourceViewModel.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Xamarin.CommunityToolkit.Sample.ViewModels.Views
+{
+	public class Monkey
+	{
+		public string Index { get; set; }
+		public string Name { get; set; }
+		public string Location { get; set; }
+		public string Details { get; set; }
+		public string Image { get; set; }
+		public Color Color { get; set; }
+	}
+
+	public class TabItemsSourceViewModel : BaseViewModel
+	{
+		public TabItemsSourceViewModel()
+		{
+			LoadMonkeys();
+		}
+
+		public ObservableCollection<Monkey> Monkeys { get; set; }
+
+		public ICommand ClearDataCommand => new Command(ClearData);
+
+		public ICommand UpdateDataCommand => new Command(UpdateData);
+
+		void LoadMonkeys()
+		{
+			Monkeys = new ObservableCollection<Monkey>
+			{
+				new Monkey
+				{
+					Index = "0",
+					Name = "Baboon",
+					Location = "Africa & Asia",
+					Details = "Baboons are African and Arabian Old World monkeys belonging to the genus Papio, part of the subfamily Cercopithecinae.",
+					Image = "http://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Papio_anubis_%28Serengeti%2C_2009%29.jpg/200px-Papio_anubis_%28Serengeti%2C_2009%29.jpg",
+					Color = Color.LightSalmon
+				},
+
+				new Monkey
+				{
+					Index = "1",
+					Name = "Capuchin Monkey",
+					Location = "Central & South America",
+					Details = "The capuchin monkeys are New World monkeys of the subfamily Cebinae. Prior to 2011, the subfamily contained only a single genus, Cebus.",
+					Image = "http://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Capuchin_Costa_Rica.jpg/200px-Capuchin_Costa_Rica.jpg",
+					Color = Color.LightBlue
+				},
+
+				new Monkey
+				{
+					Index = "2",
+					Name = "Blue Monkey",
+					Location = "Central and East Africa",
+					Details = "The blue monkey or diademed monkey is a species of Old World monkey native to Central and East Africa, ranging from the upper Congo River basin east to the East African Rift and south to northern Angola and Zambia",
+					Image = "http://upload.wikimedia.org/wikipedia/commons/thumb/8/83/BlueMonkey.jpg/220px-BlueMonkey.jpg",
+					Color = Color.LightSlateGray
+				},
+
+				new Monkey
+				{
+					Index = "3",
+					Name = "Squirrel Monkey",
+					Location = "Central & South America",
+					Details = "The squirrel monkeys are the New World monkeys of the genus Saimiri. They are the only genus in the subfamily Saimirinae. The name of the genus Saimiri is of Tupi origin, and was also used as an English name by early researchers.",
+					Image = "http://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Saimiri_sciureus-1_Luc_Viatour.jpg/220px-Saimiri_sciureus-1_Luc_Viatour.jpg",
+					Color = Color.Chocolate
+				},
+
+				new Monkey
+				{
+					Index = "4",
+					Name = "Golden Lion Tamarin",
+					Location = "Brazil",
+					Details = "The golden lion tamarin also known as the golden marmoset, is a small New World monkey of the family Callitrichidae.",
+					Image = "http://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Golden_lion_tamarin_portrait3.jpg/220px-Golden_lion_tamarin_portrait3.jpg",
+					Color = Color.Violet
+				},
+
+				new Monkey
+				{
+					Index = "5",
+					Name = "Howler Monkey",
+					Location = "South America",
+					Details = "Howler monkeys are among the largest of the New World monkeys. Fifteen species are currently recognised. Previously classified in the family Cebidae, they are now placed in the family Atelidae.",
+					Image = "http://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Alouatta_guariba.jpg/200px-Alouatta_guariba.jpg",
+					Color = Color.Aqua
+				},
+
+				new Monkey
+				{
+					Index = "6",
+					Name = "Japanese Macaque",
+					Location = "Japan",
+					Details = "The Japanese macaque, is a terrestrial Old World monkey species native to Japan. They are also sometimes known as the snow monkey because they live in areas where snow covers the ground for months each",
+					Image = "http://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Macaca_fuscata_fuscata1.jpg/220px-Macaca_fuscata_fuscata1.jpg",
+					Color = Color.OrangeRed
+				},
+
+				new Monkey
+				{
+					Index = "7",
+					Name = "Mandrill",
+					Location = "Southern Cameroon, Gabon, Equatorial Guinea, and Congo",
+					Details = "The mandrill is a primate of the Old World monkey family, closely related to the baboons and even more closely to the drill. It is found in southern Cameroon, Gabon, Equatorial Guinea, and Congo.",
+					Image = "http://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Mandrill_at_san_francisco_zoo.jpg/220px-Mandrill_at_san_francisco_zoo.jpg",
+					Color = Color.MediumPurple
+				},
+
+				new Monkey
+				{
+					Index = "8",
+					Name = "Proboscis Monkey",
+					Location = "Borneo",
+					Details = "The proboscis monkey or long-nosed monkey, known as the bekantan in Malay, is a reddish-brown arboreal Old World monkey that is endemic to the south-east Asian island of Borneo.",
+					Image = "http://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Proboscis_Monkey_in_Borneo.jpg/250px-Proboscis_Monkey_in_Borneo.jpg",
+					Color = Color.Pink
+				}
+			};
+		}
+
+		void ClearData()
+		{
+			Monkeys.Clear();
+		}
+
+		void UpdateData()
+		{
+			Monkeys.Clear();
+
+			Monkeys.Add(new Monkey
+			{
+				Index = "0",
+				Name = "Howler Monkey",
+				Location = "South America",
+				Details = "Howler monkeys are among the largest of the New World monkeys. Fifteen species are currently recognised. Previously classified in the family Cebidae, they are now placed in the family Atelidae.",
+				Image = "http://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Alouatta_guariba.jpg/200px-Alouatta_guariba.jpg",
+				Color = Color.Aqua
+			});
+
+			Monkeys.Add(new Monkey
+			{
+				Index = "1",
+				Name = "Japanese Macaque",
+				Location = "Japan",
+				Details = "The Japanese macaque, is a terrestrial Old World monkey species native to Japan. They are also sometimes known as the snow monkey because they live in areas where snow covers the ground for months each",
+				Image = "http://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Macaca_fuscata_fuscata1.jpg/220px-Macaca_fuscata_fuscata1.jpg",
+				Color = Color.OrangeRed
+			});
+		}
+	}
+}
