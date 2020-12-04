d3569733115406afcbb6d8b94bf27dffd7ec2e7f
Cfun <15718354+Cfun1@users.noreply.github.com>
2020-12-04 17:16:19 +0100

    Update GetMargins() BottomLeft

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Views/BadgeView/BadgeView.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Views/BadgeView/BadgeView.shared.cs
@@ -302,7 +302,7 @@ namespace Xamarin.CommunityToolkit.UI.Views
 					verticalMargin = size / 2;
 					var bottomLeftverticalMargin = BadgeContent.Height - verticalMargin;
 					containerMargin = new Thickness(0, bottomLeftverticalMargin, 0, 0);
-					BadgeContent.Margin = new Thickness(verticalMargin, 0, 0, 0);
+					contentMargin = new Thickness(verticalMargin, 0, 0, 0);
 					break;
 				case BadgePosition.BottomRight:
 					verticalMargin = size / 2;
