9dac2c904e2e43c3d12d29e5b1affed69f27ddc9
Gerald Versluis <gerald@verslu.is>
2020-12-07 12:56:34 +0100

    Remove Path from EventArgs

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Views/CameraView/MediaCapturedEventArgs.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Views/CameraView/MediaCapturedEventArgs.shared.cs
@@ -7,23 +7,27 @@ namespace Xamarin.CommunityToolkit.UI.Views
 {
 	public class MediaCapturedEventArgs : EventArgs
 	{
+		readonly string path;
 		readonly Lazy<ImageSource> imageSource;
-		readonly Lazy<XCT.MediaSource> mediaSource;
+		readonly Lazy<XCT.FileMediaSource> mediaSource;
 
 		internal MediaCapturedEventArgs(
 			string path = null,
 			byte[] imageData = null)
 		{
-			Path = path;
+			//Path = path;
+			this.path = path;
 			ImageData = imageData;
 			imageSource = new Lazy<ImageSource>(GetImageSource);
-			mediaSource = new Lazy<XCT.MediaSource>(GetMediaSource);
+			mediaSource = new Lazy<XCT.FileMediaSource>(GetMediaSource);
 		}
 
+		// TODO See note on CameraView.SavePhotoToFile.
+		// Taken out Path for now, just return the stream data
 		/// <summary>
 		/// Path of the saved file, only filled when taking a video or a picture and SavePhotoToFile is true
 		/// </summary>
-		public string Path { get; }
+		//public string Path { get; }
 
 		/// <summary>
 		/// Raw image data, only filled when taking a picture and SavePhotoToFile is false
@@ -32,15 +36,16 @@ namespace Xamarin.CommunityToolkit.UI.Views
 
 		public ImageSource Image => imageSource.Value;
 
-		public XCT.MediaSource Video => mediaSource.Value;
+		public XCT.FileMediaSource Video => mediaSource.Value;
 
 		ImageSource GetImageSource()
 		{
 			if (ImageData != null)
 				return ImageSource.FromStream(() => new MemoryStream(ImageData));
-			return !string.IsNullOrEmpty(Path) ? Path : null;
+
+			return !string.IsNullOrEmpty(path) ? path : null;
 		}
 
-		XCT.MediaSource GetMediaSource() => !string.IsNullOrEmpty(Path) ? Path : null;
+		XCT.FileMediaSource GetMediaSource() => !string.IsNullOrEmpty(path) ? path : null;
 	}
 }
