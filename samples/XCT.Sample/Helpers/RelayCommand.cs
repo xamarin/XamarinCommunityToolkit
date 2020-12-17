913fa7963273971c42ed97c3223eb212eff2d5c1
Gerald Versluis <gerald.versluis@microsoft.com>
2020-12-17 10:33:35 +0100

    Move sample to root and merge Markup sample (#685)
    * Move sample to root and rename
    
    * Move Markup folder
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Fix GTK build
    
    * Fixing references
    
    * Add Markup
    
    * Last fixes
    
    * Folder rename to be inline with .NET Foundation guidelines

---
+++ samples/XCT.Sample/Helpers/RelayCommand.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Xamarin.CommunityToolkit.Sample
+{
+    public class RelayCommand : ICommand
+    {
+        readonly Action execute;
+        readonly Func<Task> asyncExecute;
+
+        Func<bool> canExecute;
+        int executingCount;
+
+        public RelayCommand(Action execute, Func<bool> canExecute = null)
+        {
+            if (execute == null)
+				throw new ArgumentNullException(nameof(execute));
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        protected RelayCommand(Func<Task> execute, Func<bool> canExecute = null) // This ctor is protected here and public in a derived class, to allow simple initialization like new RelayCommand(MyMethod) without errors due to ambiguity
+        {
+            if (execute == null)
+				throw new ArgumentNullException(nameof(execute));
+            asyncExecute = execute;
+            this.canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// If no canExecute deletage is provided, CanExecute allows a single simultaneous command execution
+        /// </summary>
+        /// <param name="parameter">Ignored; this is the paremeterless command class</param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter = null)
+        {
+            try
+            {
+                return canExecute != null ? canExecute() : executingCount == 0;
+            }
+			catch (Exception ex)
+			{
+				XLog.Trace(ex);
+				return true;
+			}
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        // Asynchronous command handling based on http://stackoverflow.com/a/31595509/6043538
+        public async void Execute(object parameter = null)
+        {
+            var couldExecuteBeforeExecute = CanExecute();
+            if (!couldExecuteBeforeExecute)
+				return;
+
+            Interlocked.Increment(ref executingCount);
+            var couldExecuteDuringExecute = CanExecute();
+            if (couldExecuteDuringExecute != couldExecuteBeforeExecute)
+				RaiseCanExecuteChanged(); // TODO: test if always raising canexecutechanged does not disturb normal UI element update during a short synchronous command. If so, keep preventing multiple execution but only raise CanExecuteChanged for asynchrounous commands
+
+            try
+            {
+                if (execute != null)
+					execute();
+				else
+					await asyncExecute();
+            }
+			catch (Exception ex)
+			{
+				XLog.Trace(ex);
+			}
+			finally
+            {
+                Interlocked.Decrement(ref executingCount);
+                var couldExecuteAfterExecute = CanExecute();
+                if (couldExecuteAfterExecute != couldExecuteDuringExecute)
+					RaiseCanExecuteChanged();
+            }
+        }
+    }
+
+    public class RelayCommandAsync : RelayCommand
+    {
+        public RelayCommandAsync(Func<Task> execute, Func<bool> canExecute = null)
+			: base(execute, canExecute) { } // This ctor is public here and protected in the base class, to allow simple initialization like new RelayCommandAsync(MyMethod) without errors due to ambiguity
+    }
+
+    public class RelayCommand<TParameter> : ICommand
+    {
+        readonly Action<TParameter> execute;
+        readonly Func<TParameter, Task> asyncExecute;
+
+        Func<TParameter, bool> canExecute;
+        int executingCount;
+
+        public RelayCommand(Action<TParameter> execute, Func<TParameter, bool> canExecute = null)
+        {
+            if (execute == null)
+				throw new ArgumentNullException(nameof(execute));
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        protected RelayCommand(Func<TParameter, Task> execute, Func<TParameter, bool> canExecute = null) // This ctor is protected here and public in a derived class, to allow simple initialization like new RelayCommand(MyMethod) without errors due to ambiguity
+        {
+            if (execute == null)
+				throw new ArgumentNullException(nameof(execute));
+            asyncExecute = execute;
+            this.canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// If no canExecute deletage is provided, CanExecute allows a single simultaneous command execution
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter = null)
+        {
+            try
+            {
+                return canExecute != null ? canExecute((TParameter)parameter) : executingCount == 0;
+            }
+			catch (Exception ex)
+			{
+				XLog.Trace(ex);
+				return true;
+            }
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        // Asynchronous command handling based on http://stackoverflow.com/a/31595509/6043538
+        public async void Execute(object parameterAsObject)
+        {
+            var couldExecuteBeforeExecute = CanExecute(parameterAsObject);
+            if (!couldExecuteBeforeExecute)
+				return;
+
+            Interlocked.Increment(ref executingCount);
+            var couldExecuteDuringExecute = CanExecute(parameterAsObject);
+            if (couldExecuteDuringExecute != couldExecuteBeforeExecute)
+				RaiseCanExecuteChanged(); // TODO: test if always raising canexecutechanged does not disturb normal UI element update during a short synchronous command. If so, keep preventing multiple execution but only raise CanExecuteChanged for asynchrounous commands
+
+            try
+            {
+                var parameter = (TParameter)parameterAsObject;
+
+                if (execute != null)
+                {
+                    execute(parameter);
+                }
+                else
+                {
+                    await asyncExecute(parameter);
+                }
+            }
+			catch (Exception ex)
+			{
+				XLog.Trace(ex);
+			}
+            finally
+            {
+                Interlocked.Decrement(ref executingCount);
+                var couldExecuteAfterExecute = CanExecute(parameterAsObject);
+                if (couldExecuteAfterExecute != couldExecuteDuringExecute)
+					RaiseCanExecuteChanged();
+            }
+        }
+    }
+
+    public class RelayCommandAsync<TParameter> : RelayCommand<TParameter>
+    {
+        public RelayCommandAsync(Func<TParameter, Task> execute, Func<TParameter, bool> canExecute = null)
+			: base(execute, canExecute) { } // This ctor is public here and protected in the base class, to allow simple initialization like new RelayCommandAsync(MyMethod) without errors due to ambiguity
+    }
+}
---
+++ samples/XCT.Sample/ViewModels/Markup/SearchViewModel.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Essentials;
+
+namespace Xamarin.CommunityToolkit.Sample.ViewModels.Markup
+{
+    public class SearchViewModel : BaseViewModel
+    {
+        ICommand backCommand, likeCommand, openTwitterSearchCommand, openHelpCommand;
+
+        public string SearchText { get; set; }
+
+        public List<Tweet> SearchResults { get; set; }
+
+        public SearchViewModel()
+        {
+            SearchText = "#CSharpForMarkup";
+
+            SearchResults = new List<Tweet>
+            {
+                new Tweet
+                {
+                    AuthorImage = "https://pbs.twimg.com/profile_images/1267649264391503873/I6w-glU8_400x400.jpg",
+                    Header = "David Ortinau @davidortinau · 25/01/2020",
+                    Body = new List<TextFragment>
+                    {
+                        new TextFragment { Text = "would it surprise you to know that the last project I personally shipped was Xamarin.Forms, C# w/ " },
+                        new TextFragment { Text = "#CSharpForMarkup", IsMatch = true },
+                        new TextFragment { Text = ", & @ReactiveXUI?" }
+                    },
+                },
+                new Tweet
+                {
+                    AuthorImage = "https://pbs.twimg.com/profile_images/2159034926/MACAW_vincenth_LThumb_400x400.jpg",
+                    Header = "VincentH.NET @vincenth_net · 26/03/2020",
+                    Body = new List<TextFragment>
+                    {
+                        new TextFragment { Text = "Had an inspiring call with @matthewrdev about supporting #XamarinForms C# Markup in @mfractor\uD83D\uDE0E\n\nSo many great ideas. Autoformat, Convert XAML to C# Markup (all examples on internet!), auto split UI logic and markup...\n\nExcited " },
+                        new TextFragment { Text = "#CSharpForMarkup", IsMatch = true },
+                        new TextFragment { Text = "\uD83D\uDD25" }
+                    },
+                },
+                new Tweet
+                {
+                    AuthorImage = "https://pbs.twimg.com/profile_images/1175428143944847361/0kfeW53l_400x400.jpg",
+                    Header = "RK @rkonit · 05/02/2020",
+                    Body = new List<TextFragment>
+                    {
+                        new TextFragment { Text = "\"Never Say Never\" in Open-source space. It's happening and reminds me early days of winforms.\n\n" },
+                        new TextFragment { Text = "#CSharpForMarkup", IsMatch = true },
+                        new TextFragment { Text = " #Xamarin #XamarinForms" }
+                    },
+                }
+            };
+        }
+
+        public ICommand BackCommand => backCommand ??= new RelayCommand(Back);
+
+        public ICommand LikeCommand => likeCommand ??= new RelayCommand<Tweet>(Like);
+
+        public ICommand OpenTwitterSearchCommand => openTwitterSearchCommand ??= new RelayCommandAsync(OpenTwitterSearch);
+
+        public ICommand OpenHelpCommand => openHelpCommand ??= new RelayCommandAsync(OpenHelp);
+
+        void Back() { }
+
+        void Like(Tweet tweet) => tweet.IsLikedByMe = !tweet.IsLikedByMe;
+
+        Task OpenHelp() => Launcher.OpenAsync(new Uri("https://github.com/MicrosoftDocs/xamarin-communitytoolkit/blob/master/docs/markup.md"));
+
+        Task OpenTwitterSearch() => Launcher.OpenAsync(new Uri("https://twitter.com/search?q=%23CSharpForMarkup"));
+
+        public class Tweet : BaseViewModel
+        {
+            public string AuthorImage { get; set; }
+
+            public string Header { get; set; }
+
+            public List<TextFragment> Body { get; set; }
+
+            public bool IsLikedByMe { get; set; }
+        }
+
+        public class TextFragment
+        {
+            public string Text { get; set; }
+
+            public bool IsMatch { get; set; }
+        }
+    }
+}
