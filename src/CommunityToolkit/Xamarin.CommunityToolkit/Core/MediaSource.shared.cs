22b5bd2e807977b4202de861d519ae2619817ed9
Pedro Jesus <pedrojesus.cefet@gmail.com>
2021-01-02 18:18:09 -0300

    Make sure that linker will not remove the implicit operator

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Core/MediaSource.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Core/MediaSource.shared.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using Xamarin.CommunityToolkit.Helpers;
 using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 
 namespace Xamarin.CommunityToolkit.Core
 {
+	[Forms.Xaml.TypeConversion(typeof(MediaSourceConverter))]
 	public abstract class MediaSource : Element
 	{
 		readonly WeakEventManager weakEventManager = new WeakEventManager();
@@ -14,11 +16,15 @@ namespace Xamarin.CommunityToolkit.Core
 		public static MediaSource FromUri(Uri uri) =>
 			!uri.IsAbsoluteUri ? throw new ArgumentException("Uri must be be absolute", nameof(uri)) : new UriMediaSource { Uri = uri };
 
+		public static MediaSource FromUri(string uri) => FromUri(new Uri(uri));
+
+		[Preserve(Conditional = true)]
 		public static implicit operator MediaSource(string source) =>
 			Uri.TryCreate(source, UriKind.Absolute, out var uri) && uri.Scheme != "file"
 				? FromUri(uri)
 				: FromFile(source);
 
+		[Preserve(Conditional = true)]
 		public static implicit operator MediaSource(Uri uri) => uri == null ? null : FromUri(uri);
 
 		protected void OnSourceChanged() =>
