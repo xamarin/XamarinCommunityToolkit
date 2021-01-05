1bc8a6ba2b3ac4c4d2a734ad23f1a24cdf0d477f
Andrei Misiukevich <andrei.misiukevich@gmail.com>
2021-01-05 19:32:55 +0300

    https://github.com/xamarin/XamarinCommunityToolkit/issues/461

---
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/Shadow/PlatformShadowEffect.android.cs
@@ -0,0 +1,101 @@
+﻿using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+using Android.Views;
+using AView = Android.Views.View;
+using Android.Content;
+using Android.OS;
+using System.ComponentModel;
+using Xamarin.CommunityToolkit.Effects;
+using Xamarin.CommunityToolkit.Android.Effects;
+using Android.Widget;
+
+[assembly: ExportEffect(typeof(PlatformShadowEffect), nameof(ShadowEffect))]
+
+namespace Xamarin.CommunityToolkit.Android.Effects
+{
+	public class PlatformShadowEffect : PlatformEffect
+	{
+		const float defaultRadius = 10f;
+
+		const float defaultOpacity = 1f;
+
+		AView View => Control ?? Container;
+
+		protected override void OnAttached()
+		{
+			if (View == null)
+				return;
+
+			Update();
+		}
+
+		protected override void OnDetached()
+		{
+			if (View == null)
+				return;
+
+			View.SetElevation(0);
+		}
+
+		protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+		{
+			base.OnElementPropertyChanged(args);
+
+			if (View == null)
+				return;
+
+			switch (args.PropertyName)
+			{
+				case nameof(ShadowEffect.ColorPropertyName):
+				case nameof(ShadowEffect.OpacityPropertyName):
+				case nameof(ShadowEffect.RadiusPropertyName):
+				case nameof(ShadowEffect.OffsetXPropertyName):
+				case nameof(ShadowEffect.OffsetYPropertyName):
+					View.Invalidate();
+					Update();
+					break;
+			}
+		}
+
+		void Update()
+		{
+			if (Build.VERSION.SdkInt < BuildVersionCodes.Lollipop)
+				return;
+
+			var color = ShadowEffect.GetColor(Element);
+			if (color.IsDefault)
+			{
+				View.SetElevation(0);
+				return;
+			}
+
+			var radius = (float)ShadowEffect.GetRadius(Element);
+			if (radius < 0)
+				radius = defaultRadius;
+
+			var opacity = ShadowEffect.GetOpacity(Element);
+			if (opacity < 0)
+				opacity = defaultOpacity;
+
+			var androidColor = color.MultiplyAlpha(opacity).ToAndroid();
+
+			if (View is TextView textView)
+			{
+				var offsetX = (float)ShadowEffect.GetOffsetX(Element);
+				var offsetY = (float)ShadowEffect.GetOffsetY(Element);
+				textView.SetShadowLayer(radius, offsetX, offsetY, androidColor);
+				return;
+			}
+
+			View.SetElevation(View.Context.ToPixels(radius));
+
+			if (Build.VERSION.SdkInt < BuildVersionCodes.P)
+				return;
+
+#pragma warning disable XA0001 // Find issues with Android API usage
+			View.SetOutlineAmbientShadowColor(androidColor);
+			View.SetOutlineSpotShadowColor(androidColor);
+#pragma warning restore XA0001 // Find issues with Android API usage
+		}
+	}
+}
---
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/Shadow/PlatformShadowEffect.ios.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.ComponentModel;
+using CoreGraphics;
+using UIKit;
+using Xamarin.CommunityToolkit.Effects;
+using Xamarin.CommunityToolkit.iOS.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportEffect(typeof(PlatformShadowEffect), nameof(ShadowEffect))]
+
+namespace Xamarin.CommunityToolkit.iOS.Effects
+{
+	public class PlatformShadowEffect : PlatformEffect
+	{
+		const float defaultRadius = 10f;
+
+		const float defaultOpacity = .5f;
+
+		UIView View => Control ?? Container;
+
+		protected override void OnAttached()
+		{
+			if (View == null)
+				return;
+
+			UpdateColor();
+			UpdateOpacity();
+			UpdateRadius();
+			UpdateOffset();
+		}
+
+		protected override void OnDetached()
+		{
+			if (View == null)
+				return;
+
+			View.Layer.ShadowOpacity = 0;
+		}
+
+		protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+		{
+			base.OnElementPropertyChanged(args);
+
+			if (View == null)
+				return;
+
+			switch (args.PropertyName)
+			{
+				case nameof(ShadowEffect.ColorPropertyName):
+					UpdateColor();
+					break;
+				case nameof(ShadowEffect.OpacityPropertyName):
+					UpdateOpacity();
+					break;
+				case nameof(ShadowEffect.RadiusPropertyName):
+					UpdateRadius();
+					break;
+				case nameof(ShadowEffect.OffsetXPropertyName):
+				case nameof(ShadowEffect.OffsetYPropertyName):
+					UpdateOffset();
+					break;
+			}
+		}
+
+		void UpdateColor()
+			=> View.Layer.ShadowColor = ShadowEffect.GetColor(Element).ToCGColor();
+
+		void UpdateOpacity()
+		{
+			var opacity = (float)ShadowEffect.GetOpacity(Element);
+			View.Layer.ShadowOpacity = opacity < 0
+				? defaultOpacity
+				: opacity;
+		}
+
+		void UpdateRadius()
+		{
+			var radius = (nfloat)ShadowEffect.GetRadius(Element);
+			View.Layer.ShadowRadius = radius < 0
+				? defaultRadius
+				: radius;
+		}
+
+		void UpdateOffset()
+			=> View.Layer.ShadowOffset = new CGSize((double)ShadowEffect.GetOffsetX(Element), (double)ShadowEffect.GetOffsetY(Element));
+	}
+}
