af6aaf1431a3c39366253d16aca746a7b9c30671
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2021-01-11 21:53:21 +0200

    Add LocalizedString

---
+++ src/CommunityToolkit/Xamarin.CommunityToolkit.UnitTests/Helpers/LocalizedStringTests/LocalizedStringTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+using System.Resources;
+using Xamarin.CommunityToolkit.Helpers;
+using Xamarin.CommunityToolkit.UnitTests.Mocks;
+using Xunit;
+
+namespace Xamarin.CommunityToolkit.UnitTests.Helpers.LocalizedStringTests
+{
+	public class LocalizedStringTests
+	{
+		public LocalizedStringTests()
+		{
+			resourceManager = new MockResourceManager();
+			localizationManager = new LocalizationResourceManager();
+			localizationManager.Init(resourceManager);
+			localizationManager.SetCulture(initialCulture);
+		}
+
+		readonly LocalizationResourceManager localizationManager;
+		readonly ResourceManager resourceManager;
+		readonly CultureInfo initialCulture = CultureInfo.InvariantCulture;
+
+		[Fact]
+		public void LocalizationResourceManager_GetCulture_Equal_Indexer()
+		{
+			// Arrange
+			var testString = "test";
+			var culture2 = new CultureInfo("en");
+			var localizedString = new LocalizedString(() => localizationManager[testString]);
+
+			string responceOnCultureChanged = null;
+			localizedString.PropertyChanged += (_, _) => responceOnCultureChanged = localizedString.Localized;
+
+			// Act
+			var responceCulture1 = localizedString.Localized;
+			var responceResourceManagerCulture1 = resourceManager.GetString(testString, culture2);
+			localizationManager.SetCulture(culture2);
+			var responceCulture2 = localizedString.Localized;
+			var responceResourceManagerCulture2 = resourceManager.GetString(testString, culture2);
+
+			// Assert
+			Assert.Equal(responceResourceManagerCulture1, responceCulture1);
+			Assert.Equal(responceResourceManagerCulture2, responceOnCultureChanged);
+			Assert.Equal(responceResourceManagerCulture2, responceResourceManagerCulture2);
+		}
+	}
+}
---
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Helpers/LocalizedString.shared.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Xamarin.CommunityToolkit.Helpers
+{
+	public class LocalizedString : INotifyPropertyChanged
+	{
+		public LocalizedString(Func<string> generator = null)
+		{
+			Generator = generator;
+			LocalizationResourceManager.Current.PropertyChanged += (_, _) => Invalidate();
+		}
+
+		Func<string> generator;
+
+		public Func<string> Generator
+		{
+			get => generator;
+			set
+			{
+				generator = value;
+				Invalidate();
+			}
+		}
+
+		public string Localized => Generator?.Invoke();
+
+		public static implicit operator LocalizedString(Func<string> func) => new LocalizedString(func);
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void Invalidate()
+			=> PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+	}
+}
