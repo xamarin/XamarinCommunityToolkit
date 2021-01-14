1b508cf9e193a960f59f0458a495dde69c60d108
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2021-01-14 09:33:22 +0200

    Update LocalizedString to use ObservableObject

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Helpers/LocalizedString.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Helpers/LocalizedString.shared.cs
@@ -1,14 +1,13 @@
 ﻿using System;
-using System.ComponentModel;
+using Xamarin.CommunityToolkit.ObjectModel;
 using Xamarin.Forms.Internals;
 
 namespace Xamarin.CommunityToolkit.Helpers
 {
 #if !NETSTANDARD1_0
-	public class LocalizedString : INotifyPropertyChanged, IDisposable
+	public class LocalizedString : ObservableObject
 	{
 		readonly Func<string> generator;
-		readonly LocalizationResourceManager localizationManager;
 
 		public LocalizedString(Func<string> generator = null)
             : this(LocalizationResourceManager.Current, generator)
@@ -17,24 +16,14 @@ namespace Xamarin.CommunityToolkit.Helpers
 
 		public LocalizedString(LocalizationResourceManager localizationManager, Func<string> generator = null)
 		{
-			this.localizationManager = localizationManager;
 			this.generator = generator;
-			localizationManager.PropertyChanged += Invalidate;
+			localizationManager.PropertyChanged += (sender, e) => OnPropertyChanged(null);
 		}
 
 		public string Localized => generator?.Invoke();
 
 		[Preserve(Conditional = true)]
 		public static implicit operator LocalizedString(Func<string> func) => new LocalizedString(func);
-
-		public event PropertyChangedEventHandler PropertyChanged;
-
-		void Invalidate(object sender, PropertyChangedEventArgs e)
-			=> PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
-
-		public void Dispose() => localizationManager.PropertyChanged -= Invalidate;
-
-		~LocalizedString() => Dispose();
 	}
 #endif
 }
