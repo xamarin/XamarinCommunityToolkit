e8d769fd62622800f31caad173d6dd9a1944aea3
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2021-01-15 22:58:03 +0200

    Add test to ensure instance of LocalizedString is disposed

--- src/CommunityToolkit/Xamarin.CommunityToolkit.UnitTests/Helpers/LocalizedStringTests/LocalizedStringTests.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit.UnitTests/Helpers/LocalizedStringTests/LocalizedStringTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using System.Resources;
 using Xamarin.CommunityToolkit.Helpers;
 using Xamarin.CommunityToolkit.UnitTests.Mocks;
@@ -19,6 +20,7 @@ namespace Xamarin.CommunityToolkit.UnitTests.Helpers.LocalizedStringTests
 		readonly LocalizationResourceManager localizationManager;
 		readonly ResourceManager resourceManager;
 		readonly CultureInfo initialCulture = CultureInfo.InvariantCulture;
+		LocalizedString localizedString;
 
 		[Fact]
 		public void LocalizedStringTests_Localized_ValidImplementation()
@@ -26,7 +28,7 @@ namespace Xamarin.CommunityToolkit.UnitTests.Helpers.LocalizedStringTests
 			// Arrange
 			var testString = "test";
 			var culture2 = new CultureInfo("en");
-			var localizedString = new LocalizedString(localizationManager, () => localizationManager[testString]);
+			localizedString = new LocalizedString(localizationManager, () => localizationManager[testString]);
 
 			string responceOnCultureChanged = null;
 			localizedString.PropertyChanged += (sender, args) => responceOnCultureChanged = localizedString.Localized;
@@ -42,6 +44,27 @@ namespace Xamarin.CommunityToolkit.UnitTests.Helpers.LocalizedStringTests
 			Assert.Equal(responceResourceManagerCulture1, responceCulture1);
 			Assert.Equal(responceResourceManagerCulture2, responceOnCultureChanged);
 			Assert.Equal(responceResourceManagerCulture2, responceResourceManagerCulture2);
+		}
+
+		[Fact]
+		public void LocalizedStringTests_Disposed_IfNoReferences()
+		{
+			// Arrange
+			var testString = "test";
+			SetLocalizedString();
+			var weaklocalizedString = new WeakReference(localizedString);
+			localizedString = null;
+
+			// Act
+			GC.Collect();
+
+			// Assert
+			Assert.False(weaklocalizedString.IsAlive);
+
+			void SetLocalizedString()
+			{
+				localizedString = new LocalizedString(localizationManager, () => localizationManager[testString]);
+			}
 		}
 	}
 }
