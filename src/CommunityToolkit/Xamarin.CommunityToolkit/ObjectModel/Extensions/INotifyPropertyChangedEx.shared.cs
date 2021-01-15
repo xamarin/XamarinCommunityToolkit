e2b916c72d98725bedc8df080740b74517a43521
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2021-01-15 22:56:53 +0200

    Add WeakSubscribe

---
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/ObjectModel/Extensions/INotifyPropertyChangedEx.shared.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Xamarin.CommunityToolkit.ObjectModel.Extensions
+{
+	public static class INotifyPropertyChangedEx
+	{
+		public static void WeakSubscribe<T>(this INotifyPropertyChanged target, T subscriber, Action<T, object, EventArgs> action)
+		{
+			var weakSubscriber = new WeakReference(subscriber, false);
+			target.PropertyChanged += handler;
+
+			void handler(object sender, PropertyChangedEventArgs e)
+			{
+				var s = (T)weakSubscriber.Target;
+				if (s is null)
+				{
+					target.PropertyChanged -= handler;
+					return;
+				}
+
+				action.Invoke(s, sender, e);
+			}
+		}
+	}
+}
