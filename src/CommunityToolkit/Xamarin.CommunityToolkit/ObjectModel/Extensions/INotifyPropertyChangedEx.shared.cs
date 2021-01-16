04be0c8eab2fdaba0acb6d379db03ac26944bbd1
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2021-01-17 01:40:50 +0200

    Add null checks to WeakSubscribe

--- src/CommunityToolkit/Xamarin.CommunityToolkit/ObjectModel/Extensions/INotifyPropertyChangedEx.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/ObjectModel/Extensions/INotifyPropertyChangedEx.shared.cs
@@ -7,13 +7,19 @@ namespace Xamarin.CommunityToolkit.ObjectModel.Extensions
 	{
 		public static void WeakSubscribe<T>(this INotifyPropertyChanged target, T subscriber, Action<T, object, EventArgs> action)
 		{
+			_ = target ?? throw new ArgumentNullException(nameof(target));
+			if (subscriber == null || action == null)
+			{
+				return;
+			}
+
 			var weakSubscriber = new WeakReference(subscriber, false);
 			target.PropertyChanged += handler;
 
 			void handler(object sender, PropertyChangedEventArgs e)
 			{
 				var s = (T)weakSubscriber.Target;
-				if (s is null)
+				if (s == null)
 				{
 					target.PropertyChanged -= handler;
 					return;
