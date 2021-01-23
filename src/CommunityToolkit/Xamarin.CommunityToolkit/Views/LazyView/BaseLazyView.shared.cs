98cd9b2fe7a97c08326322a3e7d98df45c1c893f
Pedro Jesus <pedrojesus.cefet@gmail.com>
2021-01-23 00:55:54 -0300

    Created LazyView and implemented it on TabView

---
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Views/LazyView/BaseLazyView.shared.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Xamarin.CommunityToolkit.UI.Views
+{
+	[EditorBrowsable(EditorBrowsableState.Never)]
+	public abstract class BaseLazyView : ContentView, IDisposable
+	{
+		internal static readonly BindablePropertyKey IsLoadedPropertyKey = BindableProperty.CreateReadOnly(nameof(IsLoaded), typeof(bool), typeof(BaseLazyView), default);
+
+		public static readonly BindableProperty IsLoadedProperty = IsLoadedPropertyKey.BindableProperty;
+
+		public bool IsLoaded => (bool)GetValue(IsLoadedProperty);
+
+		internal void SetIsLoaded(bool isLoaded) => SetValue(IsLoadedPropertyKey, isLoaded);
+
+		public abstract void LoadView();
+
+		public void Dispose()
+		{
+			if (Content is IDisposable disposable)
+				disposable.Dispose();
+		}
+
+		protected override void OnBindingContextChanged()
+		{
+			if (Content != null && !(Content is ActivityIndicator))
+				Content.BindingContext = BindingContext;
+		}
+	}
+}
---
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Views/LazyView/LazyView.shared.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+
+namespace Xamarin.CommunityToolkit.UI.Views
+{
+	public class LazyView<TView> : BaseLazyView where TView : View, new()
+	{
+		public override void LoadView()
+		{
+			View view = new TView { BindingContext = BindingContext };
+
+			Content = view;
+
+			SetIsLoaded(true);
+		}
+	}
+}
