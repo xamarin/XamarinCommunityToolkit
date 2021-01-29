5dd6a85920bdf60dc1a2501d15436eea619d8add
Andrei <andrei.misiukevich@gmail.com>
2021-01-29 23:57:49 +0100

    ShadowEffect (#725)
    * https://github.com/xamarin/XamarinCommunityToolkit/issues/461
    
    * Added mac support
    
    * Added sample
    
    * Updated shadows
    
    * Fixed iOS crash
    
    * removed pragma
    
    * Remove effect when color if default
    
    * cleaned code

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/EffectIds.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/EffectIds.shared.cs
@@ -40,5 +40,10 @@ namespace Xamarin.CommunityToolkit.Effects
 		/// Effect Id for <see cref="TouchEffect"/>
 		/// </summary>
 		public static string TouchEffect => $"{effectResolutionGroupName}.{nameof(TouchEffect)}";
+
+		/// <summary>
+		/// Effect Id for <see cref="ShadowEffect"/>
+		/// </summary>
+		public static string ShadowEffect => $"{effectResolutionGroupName}.{nameof(ShadowEffect)}";
 	}
 }
