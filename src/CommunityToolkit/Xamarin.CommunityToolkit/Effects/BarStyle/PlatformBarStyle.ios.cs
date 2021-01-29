d53959c3148a99f434eb9ada0867e77fce12c560
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2021-01-29 19:12:35 +0200

    Add dummy implementations for iOS and UWP

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/BarStyle/PlatformBarStyle.ios.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/BarStyle/PlatformBarStyle.ios.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using CoreGraphics;
 using Foundation;
 using UIKit;
@@ -14,6 +15,43 @@ namespace Xamarin.CommunityToolkit.iOS.Effects
 {
 	public class PlatformBarStyle : PlatformEffect
 	{
-		
+		protected override void OnAttached()
+		{
+			SetStatusBarColor(BarStyle.GetStatusBarColor(Element).ToUIColor());
+			SetStatusBarStyle(BarStyle.GetStatusBarStyle(Element));
+		}
+
+		protected override void OnDetached()
+		{
+			if (BarStyle.GetStatusBarColor(Element) != (Forms.Color)BarStyle.StatusBarColorProperty.DefaultValue)
+			{
+				SetStatusBarColor(UIColor.Black);
+			}
+			if (BarStyle.GetStatusBarStyle(Element) != (StatusBarStyle)BarStyle.StatusBarStyleProperty.DefaultValue)
+			{
+				SetStatusBarStyle(StatusBarStyle.Default);
+			}
+		}
+
+		protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+		{
+			base.OnElementPropertyChanged(args);
+			if (args.PropertyName == BarStyle.StatusBarColorProperty.PropertyName)
+			{
+				SetStatusBarColor(BarStyle.GetStatusBarColor(Element).ToUIColor());
+			}
+			else if (args.PropertyName == BarStyle.StatusBarStyleProperty.PropertyName)
+			{
+				SetStatusBarStyle(BarStyle.GetStatusBarStyle(Element));
+			}
+		}
+
+		public void SetStatusBarColor(UIColor color)
+		{
+		}
+
+		public void SetStatusBarStyle(StatusBarStyle style)
+		{
+		}
 	}
 }
--- src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/BarStyle/PlatformBarStyle.uwp.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/BarStyle/PlatformBarStyle.uwp.cs
@@ -7,6 +7,8 @@ using System;
 using System.Collections.Generic;
 using Xamarin.CommunityToolkit.UWP.Effects;
 using Xamarin.CommunityToolkit.Effects;
+using System.ComponentModel;
+using Color = Windows.UI.Color;
 
 [assembly: ExportEffect(typeof(PlatformBarStyle), nameof(BarStyle))]
 
@@ -14,6 +16,43 @@ namespace Xamarin.CommunityToolkit.UWP.Effects
 {
 	public class PlatformBarStyle : PlatformEffect
 	{
-		
+		protected override void OnAttached()
+		{
+			SetStatusBarColor(BarStyle.GetStatusBarColor(Element).ToWindowsColor());
+			SetStatusBarStyle(BarStyle.GetStatusBarStyle(Element));
+		}
+
+		protected override void OnDetached()
+		{
+			if (BarStyle.GetStatusBarColor(Element) != (Forms.Color)BarStyle.StatusBarColorProperty.DefaultValue)
+			{
+				SetStatusBarColor(Color.FromArgb(0, 0, 0, 0));
+			}
+			if (BarStyle.GetStatusBarStyle(Element) != (StatusBarStyle)BarStyle.StatusBarStyleProperty.DefaultValue)
+			{
+				SetStatusBarStyle(StatusBarStyle.Default);
+			}
+		}
+
+		protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+		{
+			base.OnElementPropertyChanged(args);
+			if (args.PropertyName == BarStyle.StatusBarColorProperty.PropertyName)
+			{
+				SetStatusBarColor(BarStyle.GetStatusBarColor(Element).ToWindowsColor());
+			}
+			else if (args.PropertyName == BarStyle.StatusBarStyleProperty.PropertyName)
+			{
+				SetStatusBarStyle(BarStyle.GetStatusBarStyle(Element));
+			}
+		}
+
+		public void SetStatusBarColor(Color color)
+		{
+		}
+
+		public void SetStatusBarStyle(StatusBarStyle style)
+		{
+		}
 	}
 }
