7fa64333874728134a8b5432a8201efaefc58844
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-01-29 13:39:19 -0800

    Add PopupDismissedEventArgs<T>

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Views/Popup/PopupDismissedEventArgs.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Views/Popup/PopupDismissedEventArgs.shared.cs
@@ -5,7 +5,18 @@ namespace Xamarin.CommunityToolkit.UI.Views
 	/// <summary>
 	/// Popup dismissed event arguments used when a popup is dismissed.
 	/// </summary>
-	public class PopupDismissedEventArgs : EventArgs
+	public class PopupDismissedEventArgs : PopupDismissedEventArgs<object>
+	{
+		public PopupDismissedEventArgs(object result)
+			: base(result)
+		{
+		}
+	}
+
+	/// <summary>
+	/// Popup dismissed event arguments used when a popup is dismissed.
+	/// </summary>
+	public class PopupDismissedEventArgs<T> : EventArgs
 	{
 		/// <summary>
 		/// Initialization an instance of <see cref="PopupDismissedEventArgs"/>.
@@ -13,12 +24,12 @@ namespace Xamarin.CommunityToolkit.UI.Views
 		/// <param name="result">
 		/// The result of the popup.
 		/// </param>
-		public PopupDismissedEventArgs(object result) =>
+		public PopupDismissedEventArgs(T result) =>
 			Result = result;
 
 		/// <summary>
 		/// The resulting object to return.
 		/// </summary>
-		public object Result { get; }
+		public T Result { get; }
 	}
 }
