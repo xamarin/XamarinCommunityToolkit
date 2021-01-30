88a4bfbed0dadef4ae28eff3bc598b48376b2e28
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2021-01-30 11:48:45 +0200

    Add Command canExecute description where it was missing

--- src/CommunityToolkit/Xamarin.CommunityToolkit/ObjectModel/CommandFactory.Command.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/ObjectModel/CommandFactory.Command.shared.cs
@@ -20,6 +20,7 @@ namespace Xamarin.CommunityToolkit.ObjectModel
 		/// Initializes Xamarin.Forms.Command
 		/// </summary>
 		/// <param name="execute">The Function executed when Execute is called. This does not check canExecute before executing and will execute even if canExecute is false</param>
+		/// <param name="canExecute">The Function that verifies whether or not Command should execute.</param>
 		/// <returns>Xamarin.Forms.Command</returns>
 		public static Command Create(Action execute, Func<bool> canExecute) =>
 			new Command(execute, canExecute);
@@ -36,6 +37,7 @@ namespace Xamarin.CommunityToolkit.ObjectModel
 		/// Initializes Xamarin.Forms.Command
 		/// </summary>
 		/// <param name="execute">The Function executed when Execute is called. This does not check canExecute before executing and will execute even if canExecute is false</param>
+		/// <param name="canExecute">The Function that verifies whether or not Command should execute.</param>
 		/// <returns>Xamarin.Forms.Command</returns>
 		public static Command Create(Action<object> execute, Func<object, bool> canExecute) =>
 			new Command(execute, canExecute);
@@ -52,6 +54,7 @@ namespace Xamarin.CommunityToolkit.ObjectModel
 		/// Initializes Xamarin.Forms.Command<typeparamref name="T"/>
 		/// </summary>
 		/// <param name="execute">The Function executed when Execute is called. This does not check canExecute before executing and will execute even if canExecute is false</param>
+		/// <param name="canExecute">The Function that verifies whether or not Command should execute.</param>
 		/// <returns>Xamarin.Forms.Command<typeparamref name="T"/></returns>
 		public static Command<T> Create<T>(Action<T> execute, Func<T, bool> canExecute) =>
 			new Command<T>(execute, canExecute);
