1b80bbe87c4e8617707c24f9171b2fe69996c517
Pedro Jesus <pedrojesus.cefet@gmail.com>
2021-02-03 20:44:01 -0300

    Changed LoadView to ValueTask LoadViewAsync and added summaries

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Views/LazyView/LazyView.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Views/LazyView/LazyView.shared.cs
@@ -1,16 +1,30 @@
-﻿using Xamarin.Forms;
+﻿using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace Xamarin.CommunityToolkit.UI.Views
 {
+	/// <summary>
+	/// This a basic implementation of the LazyView based on <see cref="BaseLazyView"/> use this an exemple to create yours
+	/// </summary>
+	/// <typeparam name="TView">Any <see cref="View"/></typeparam>
 	public class LazyView<TView> : BaseLazyView where TView : View, new()
 	{
-		public override void LoadView()
+		/// <summary>
+		/// This method initialize your <see cref="LazyView{TView}"/>.
+		/// </summary>
+		/// <returns><see cref="ValueTask"/></returns>
+		public override ValueTask LoadViewAsync()
 		{
 			View view = new TView { BindingContext = BindingContext };
 
 			Content = view;
 
 			SetIsLoaded(true);
+#if !NETSTANDARD1_0
+			return new ValueTask(Task.CompletedTask);
+#else
+			return new ValueTask(Task.FromResult<object>(null));
+#endif
 		}
 	}
 }
