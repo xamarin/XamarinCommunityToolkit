0d6d6b4c963685341f41a6e5fef9a66ffdd6afe5
Cfun <15718354+Cfun1@users.noreply.github.com>
2021-02-05 22:16:29 +0100

    Fix popup out of horizontal bounds
    https://github.com/xamarin/XamarinCommunityToolkit/pull/653#issuecomment-757399625

--- samples/XCT.Sample/Pages/Views/Popups/PopupSize.cs
+++ samples/XCT.Sample/Pages/Views/Popups/PopupSize.cs
@@ -4,7 +4,7 @@ namespace Xamarin.CommunityToolkit.Sample.Pages.Views.Popups
 {
 	static class PopupSize
 	{
-		public static Size Android => new Size(1200, 1000);
+		public static Size Android => new Size(1000, 1200);
 
 		public static Size iOS => new Size(250, 350);
 
