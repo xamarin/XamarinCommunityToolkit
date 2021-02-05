553ab66f574cadb6199cd96a0e37ebbdaa734c18
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-02-04 19:24:17 -0800

    Add BaseCommand.wpf.cs

---
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/ObjectModel/Internals/BaseCommand.wpf.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Xamarin.CommunityToolkit.ObjectModel.Internals
+{
+	public abstract partial class BaseCommand<TCanExecute>
+	{
+		static bool IsMainThread => Thread.CurrentThread == System.Windows.Threading.Dispatcher.CurrentDispatcher.Thread;
+
+		static void BeginInvokeOnMainThread(Action action) => System.Windows.Application.Current?.Dispatcher.BeginInvoke(action);
+	}
+}
