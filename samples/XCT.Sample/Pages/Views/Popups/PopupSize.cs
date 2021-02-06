c9b606ac6109353db1c0d298b32bdbda2f9fd0a3
Cfun <15718354+Cfun1@users.noreply.github.com>
2021-02-06 03:36:27 +0100

    Popup out of the screen horizontally in Android sample (#839)
    * Fix popup out of horizontal bounds
    
    https://github.com/xamarin/XamarinCommunityToolkit/pull/653#issuecomment-757399625
    
    * Device Size dependent
    
    * Essentials namespace
    
    * revert

--- samples/XCT.Sample/Pages/Views/Popups/PopupSize.cs
+++ samples/XCT.Sample/Pages/Views/Popups/PopupSize.cs
@@ -4,10 +4,10 @@ namespace Xamarin.CommunityToolkit.Sample.Pages.Views.Popups
 {
 	static class PopupSize
 	{
-		public static Size Android => new Size(1200, 1000);
+		public static Size Android => new Size(1000, 1200);
 
-		public static Size iOS => new Size(250, 350);
-
+ 		public static Size iOS => new Size(250, 350);
+		
 		public static Size UWP => new Size(300, 400);
 	}
 }
