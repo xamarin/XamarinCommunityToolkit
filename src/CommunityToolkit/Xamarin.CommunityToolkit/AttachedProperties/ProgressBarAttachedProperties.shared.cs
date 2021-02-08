abd90e9d45c971a72bdd1bae7e6125ed9dc85d47
Gerald Versluis <gerald@verslu.is>
2021-02-08 11:00:43 +0100

    Update ProgressBarAttachedProperties.shared.cs

--- src/CommunityToolkit/Xamarin.CommunityToolkit/AttachedProperties/ProgressBarAttachedProperties.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/AttachedProperties/ProgressBarAttachedProperties.shared.cs
@@ -3,10 +3,16 @@ using Xamarin.Forms;
 
 namespace Xamarin.CommunityToolkit.AttachedProperties
 {
+	/// <summary>
+	/// This class defines attached properties that can be uses with a <see cref="ProgressBar"/>.
+	/// </summary>
 	public static class ProgressBarAttachedProperties
 	{
 		const string animatedProgress = "AnimatedProgress";
 
+		/// <summary>
+		/// Backing BindableProperty that is used to attach to a <see cref="ProgressBar"/> and animate the showing of progress.
+		/// </summary>
 		public static BindableProperty AnimatedProgressProperty = BindableProperty.CreateAttached(animatedProgress, typeof(double), typeof(ProgressBar), 0.0d, propertyChanged: ProgressBarProgressChanged);
 
 		static void ProgressBarProgressChanged(BindableObject bindable, object oldValue, object newValue)
