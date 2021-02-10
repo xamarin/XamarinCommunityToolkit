ccf02c46d4382f6e6439c9d0c984b0809693a92e
Vladislav Antonyuk <33021114+VladislavAntonyuk@users.noreply.github.com>
2021-02-11 00:01:22 +0200

    #704 [Bug] No padding around text in a Toast or Snackbar (#714)

---
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Views/Snackbar/Helpers/PaddedLabel.ios.macos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using CoreGraphics;
+#if __IOS__
+using UIKit;
+#else
+using AppKit;
+#endif
+
+namespace Xamarin.CommunityToolkit.UI.Views.Helpers
+{
+#if __IOS__
+	class PaddedLabel : UILabel
+#else
+	class PaddedLabel : NSTextField
+#endif
+	{
+		public PaddedLabel(nfloat left, nfloat top, nfloat right, nfloat bottom)
+		{
+			Left = left;
+			Top = top;
+			Right = right;
+			Bottom = bottom;
+		}
+
+		public nfloat Left { get; }
+
+		public nfloat Top { get; }
+
+		public nfloat Right { get; }
+
+		public nfloat Bottom { get; }
+
+		public override CGSize IntrinsicContentSize => new CGSize(
+			base.IntrinsicContentSize.Width + Left + Right,
+			base.IntrinsicContentSize.Height + Top + Bottom);
+
+#if __IOS__
+		public override void DrawText(CGRect rect)
+		{
+			var insets = new UIEdgeInsets(Top, Left, Bottom, Right);
+			base.DrawText(insets.InsetRect(rect));
+		}
+#endif
+	}
+}
--- src/CommunityToolkit/Xamarin.CommunityToolkit/Views/Snackbar/Helpers/iOS/SnackbarViews/ActionMessageSnackBarView.ios.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Views/Snackbar/Helpers/iOS/SnackbarViews/ActionMessageSnackBarView.ios.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using UIKit;
+﻿using UIKit;
 using Xamarin.CommunityToolkit.UI.Views.Helpers.iOS.SnackBarViews;
+using Xamarin.CommunityToolkit.Views.Snackbar.Helpers;
 
 namespace Xamarin.CommunityToolkit.UI.Views.Helpers.iOS
 {
@@ -11,39 +11,12 @@ namespace Xamarin.CommunityToolkit.UI.Views.Helpers.iOS
 		{
 		}
 
-		// Gets the maximum width of the action button. Possible values 0 to 1.
-		protected virtual nfloat ActionButtonMaxWidth => 1f;
-
 		protected override void Initialize()
 		{
 			base.Initialize();
 
-			foreach (var action in SnackBar.Actions)
+			foreach (var actionButton in SnackBar.Actions)
 			{
-				var actionButton = new UIButton(UIButtonType.System);
-				if (action.Appearance.Background != NativeSnackButtonAppearance.DefaultColor)
-				{
-					actionButton.BackgroundColor = action.Appearance.Background;
-				}
-
-				if (action.Appearance.Foreground != NativeSnackButtonAppearance.DefaultColor)
-				{
-					actionButton.SetTitleColor(action.Appearance.Foreground, UIControlState.Normal);
-				}
-
-				if (action.Appearance.Font != NativeSnackButtonAppearance.DefaultFont)
-				{
-					actionButton.Font = action.Appearance.Font;
-				}
-
-				actionButton.SetTitle(action.ActionButtonText, UIControlState.Normal);
-				actionButton.TitleLabel.LineBreakMode = action.Appearance.LineBreakMode;
-				actionButton.TouchUpInside += async (s, e) =>
-				{
-					await action.Action();
-					Dismiss();
-				};
-
 				StackView.AddArrangedSubview(actionButton);
 			}
 		}
