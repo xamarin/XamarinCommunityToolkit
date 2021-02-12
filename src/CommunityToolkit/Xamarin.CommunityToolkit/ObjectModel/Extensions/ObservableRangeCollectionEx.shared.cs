d70c20d0b8995214467911c23a245e5bc44f993d
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2021-02-12 02:59:32 +0200

    Create ObservableRangeCollectionEx.shared.cs

---
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/ObjectModel/Extensions/ObservableRangeCollectionEx.shared.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Xamarin.CommunityToolkit.ObjectModel
+{
+	public static class ObservableRangeCollectionEx
+    {
+        /// <summary>
+        /// To be used in collection initializer
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static void Add<T>(this ObservableRangeCollection<T> source, IEnumerable<T> collection)
+        {
+            source.AddRange(collection);
+        }
+    }
+}
