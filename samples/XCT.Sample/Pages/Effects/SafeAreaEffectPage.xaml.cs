6614253b8aa46fc941da7bf47ccd93b195c4401a
Cfun <15718354+Cfun1@users.noreply.github.com>
2021-02-13 19:30:55 +0100

    Housekeeping remove XamlCompilation from code-behind. (#897)

--- samples/XCT.Sample/Pages/Effects/SafeAreaEffectPage.xaml.cs
+++ samples/XCT.Sample/Pages/Effects/SafeAreaEffectPage.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using Xamarin.CommunityToolkit.Effects;
-using Xamarin.Forms.Xaml;
 
 namespace Xamarin.CommunityToolkit.Sample.Pages.Effects
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class SafeAreaEffectPage
 	{
 		public SafeAreaEffectPage() => InitializeComponent();
