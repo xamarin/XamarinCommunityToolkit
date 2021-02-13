068783ae11b35e9af69270bd6cf6fcce14113f02
Cfun <15718354+Cfun1@users.noreply.github.com>
2021-02-13 16:12:46 +0100

    Housekeeping remove XamlCompilation from code-behind.

--- samples/XCT.Sample/Pages/Behaviors/MaxLengthReachedBehaviorPage.xaml.cs
+++ samples/XCT.Sample/Pages/Behaviors/MaxLengthReachedBehaviorPage.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using Xamarin.CommunityToolkit.Behaviors;
-using Xamarin.Forms.Xaml;
 
 namespace Xamarin.CommunityToolkit.Sample.Pages.Behaviors
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class MaxLengthReachedBehaviorPage : BasePage
 	{
 		public MaxLengthReachedBehaviorPage()
