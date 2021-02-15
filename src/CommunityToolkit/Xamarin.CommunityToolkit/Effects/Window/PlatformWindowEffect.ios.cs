704e88756b0cf66f018fc41c191d49b42cf8977c
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2021-02-16 01:51:29 +0200

    Add iOS implemetation (not tested)

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/Window/PlatformWindowEffect.ios.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/Window/PlatformWindowEffect.ios.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.ComponentModel;
-using CoreGraphics;
+﻿using System.ComponentModel;
 using Foundation;
 using UIKit;
 using Xamarin.CommunityToolkit.Effects;
-using Xamarin.CommunityToolkit.Helpers;
 using Xamarin.CommunityToolkit.iOS.Effects;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
@@ -40,10 +37,58 @@ namespace Xamarin.CommunityToolkit.iOS.Effects
 
 		public void SetStatusBarColor(UIColor color)
 		{
+			if (UIDevice.CurrentDevice.CheckSystemVersion(13, 0))
+			{
+				foreach (var window in UIApplication.SharedApplication.Windows)
+				{
+					const int statusBarTag = 38482;
+					var statusBar = window.ViewWithTag(statusBarTag) ?? new UIView(UIApplication.SharedApplication.StatusBarFrame);
+					statusBar.Tag = statusBarTag;
+					statusBar.BackgroundColor = color;
+					statusBar.TintColor = color;
+					window.AddSubview(statusBar);
+				}
+			}
+			else
+			{
+				var statusBar = UIApplication.SharedApplication.ValueForKey(new NSString("statusBar")) as UIView;
+				if (statusBar != null && statusBar.RespondsToSelector(new ObjCRuntime.Selector("setBackgroundColor:")))
+				{
+					statusBar.BackgroundColor = color;
+				}
+			}
+
+			GetCurrentViewController().SetNeedsStatusBarAppearanceUpdate();
 		}
 
 		public void SetStatusBarStyle(StatusBarStyle style)
 		{
+			switch (style)
+			{
+				case StatusBarStyle.Default:
+					UIApplication.SharedApplication.SetStatusBarStyle(UIStatusBarStyle.Default, false);
+					break;
+				case StatusBarStyle.LightContent:
+					UIApplication.SharedApplication.SetStatusBarStyle(UIStatusBarStyle.LightContent, false);
+					break;
+				case StatusBarStyle.DarkContent:
+					UIApplication.SharedApplication.SetStatusBarStyle(UIStatusBarStyle.DarkContent, false);
+					break;
+			}
+
+			GetCurrentViewController().SetNeedsStatusBarAppearanceUpdate();
+		}
+
+		UIViewController GetCurrentViewController()
+		{
+			var window = UIApplication.SharedApplication.KeyWindow;
+
+			var vc = window.RootViewController;
+			while (vc.PresentedViewController != null)
+			{
+				vc = vc.PresentedViewController;
+			}
+			return vc;
 		}
 	}
 }
