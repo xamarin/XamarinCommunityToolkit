62c43e1a34699381486ea4e9ea477b337b0dd019
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2021-02-18 22:46:35 +0200

    Convert "Not" converters to use existing logic

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Converters/NotEqualConverter.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Converters/NotEqualConverter.shared.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Globalization;
-using Xamarin.CommunityToolkit.Extensions.Internals;
-using Xamarin.Forms;
 
 namespace Xamarin.CommunityToolkit.Converters
 {
 	/// <summary>
 	/// Checks whether the incoming value doesn't equal the provided parameter.
 	/// </summary>
-	public class NotEqualConverter : ValueConverterExtension, IValueConverter
+	public class NotEqualConverter : EqualConverter
 	{
 		/// <summary>
 		/// Checks whether the incoming value doesn't equal the provided parameter.
@@ -18,18 +16,7 @@ namespace Xamarin.CommunityToolkit.Converters
 		/// <param name="parameter">The second object to compare.</param>
 		/// <param name="culture">The culture to use in the converter. This is not implemented.</param>
 		/// <returns>True if <paramref name="value"/> and <paramref name="parameter"/> are not equal, False if they are equal.</returns>
-		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-			=> !((value != null && value.Equals(parameter)) || (value == null && parameter == null));
-
-		/// <summary>
-		/// This method is not implemented and will throw a <see cref="NotImplementedException"/>.
-		/// </summary>
-		/// <param name="value">N/A</param>
-		/// <param name="targetType">N/A</param>
-		/// <param name="parameter">N/A</param>
-		/// <param name="culture">N/A</param>
-		/// <returns>N/A</returns>
-		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-			=> throw new NotImplementedException();
+		public override object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+			!(bool)base.Convert(value, targetType, parameter, culture);
 	}
 }
