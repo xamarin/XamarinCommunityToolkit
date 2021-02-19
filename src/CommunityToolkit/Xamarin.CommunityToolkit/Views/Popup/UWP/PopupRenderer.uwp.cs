30981b2414eae9703345f0764827d0826acbadcf
Thomas Stocker <thomas.stocker@gmail.com>
2021-02-19 19:26:56 +0100

    Uwp Fixes

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Views/Popup/UWP/PopupRenderer.uwp.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Views/Popup/UWP/PopupRenderer.uwp.cs
@@ -155,10 +155,11 @@ namespace Xamarin.CommunityToolkit.UI.Views
 
 			flyoutStyle.Setters.Add(new Windows.UI.Xaml.Setter(FlyoutPresenter.BackgroundProperty, Element.Color.ToWindowsColor()));
 
-#if UWP_18362
-			if (Element.Color == Color.Transparent)
-				flyoutStyle.Setters.Add(new Windows.UI.Xaml.Setter(FlyoutPresenter.IsDefaultShadowEnabledProperty, false));
-#endif
+			if (ApiInformation.IsPropertyPresent(typeof(FlyoutPresenter).FullName, nameof(FlyoutPresenter.IsDefaultShadowEnabledProperty)))
+			{
+				if (Element.Color == Color.Transparent)
+					flyoutStyle.Setters.Add(new Windows.UI.Xaml.Setter(FlyoutPresenter.IsDefaultShadowEnabledProperty, false));
+			}
 		}
 
 		void ApplyStyles()
