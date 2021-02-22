6b8b7ef85eda2e7931d5aa5a38b1afc2413c4f46
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2021-02-22 14:09:02 +0200

    Add null checks to first extension method argument (#943)
    Co-authored-by: Gerald Versluis <gerald.versluis@microsoft.com>

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Views/TabView/LayoutExtensions.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Views/TabView/LayoutExtensions.shared.cs
@@ -1,10 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Xamarin.Forms;
 
 namespace Xamarin.CommunityToolkit.UI.Views
 {
 	public static class LayoutExtensions
 	{
-		public static IReadOnlyList<Element> GetChildren(this ILayoutController source) => source.Children;
+		public static IReadOnlyList<Element> GetChildren(this ILayoutController source)
+		{
+			_ = source ?? throw new ArgumentNullException(nameof(source));
+
+			return source.Children;
+		}
 	}
 }
