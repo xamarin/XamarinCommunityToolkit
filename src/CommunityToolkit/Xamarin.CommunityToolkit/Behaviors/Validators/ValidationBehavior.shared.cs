64313570bbfaf116ab066c9a0ae0bbff32aa6d86
Andrei <andrei.misiukevich@gmail.com>
2021-03-02 22:58:55 +0100

    Update src/CommunityToolkit/Xamarin.CommunityToolkit/Behaviors/Validators/ValidationBehavior.shared.cs

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Behaviors/Validators/ValidationBehavior.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Behaviors/Validators/ValidationBehavior.shared.cs
@@ -3,7 +3,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
-using System.Linq;
 
 namespace Xamarin.CommunityToolkit.Behaviors.Internals
 {
