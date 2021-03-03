c08efc1940c4341447cda37550c05c96ffbead54
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-03-03 12:51:13 -0800

    Add Collection to LocalizationResourceManagerTests
    The `Collection` attribute prevents these tests from running in parallel https://xunit.net/docs/running-tests-in-parallel.html

--- src/CommunityToolkit/Xamarin.CommunityToolkit.UnitTests/Helpers/LocalizationResourceManagerTests/LocalizationResourceManagerTests.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit.UnitTests/Helpers/LocalizationResourceManagerTests/LocalizationResourceManagerTests.cs
@@ -6,6 +6,7 @@ using Xunit;
 
 namespace Xamarin.CommunityToolkit.UnitTests.Helpers.LocalizationResourceManagerTests
 {
+	[Collection(nameof(LocalizationResourceManager))]
 	public class LocalizationResourceManagerTests
 	{
 		public LocalizationResourceManagerTests()
--- src/CommunityToolkit/Xamarin.CommunityToolkit.UnitTests/Helpers/LocalizedStringTests/LocalizedStringTests.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit.UnitTests/Helpers/LocalizedStringTests/LocalizedStringTests.cs
@@ -7,6 +7,7 @@ using Xunit;
 
 namespace Xamarin.CommunityToolkit.UnitTests.Helpers.LocalizedStringTests
 {
+	[Collection(nameof(LocalizationResourceManager))]
 	public class LocalizedStringTests
 	{
 		public LocalizedStringTests()
