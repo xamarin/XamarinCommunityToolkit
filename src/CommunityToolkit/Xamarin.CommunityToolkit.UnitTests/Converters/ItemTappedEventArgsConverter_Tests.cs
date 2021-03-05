30d2cb41e8c36bc0cf75424375d787043d7a5787
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-03-05 14:51:38 -0800

    Update ItemTappedEventArgsConverter_Tests.cs

--- src/CommunityToolkit/Xamarin.CommunityToolkit.UnitTests/Converters/ItemTappedEventArgsConverter_Tests.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit.UnitTests/Converters/ItemTappedEventArgsConverter_Tests.cs
@@ -11,14 +11,12 @@ namespace Xamarin.CommunityToolkit.UnitTests.Converters
 	{
 		static object expectedValue = 100;
 
-		public static IEnumerable<object[]> GetData() => new List<object[]>
+		public static IEnumerable<object?[]> GetData() => new List<object?[]>
 			{
             // We know it's deprecated, still good to test it
 #pragma warning disable CS0618 // Type or member is obsolete
-#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
-                new object[] { new ItemTappedEventArgs(null, expectedValue), expectedValue },
-                new object[] { new ItemTappedEventArgs(null, null), null },
-#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+                new object?[] { new ItemTappedEventArgs(null, expectedValue), expectedValue },
+                new object?[] { new ItemTappedEventArgs(null, null), null },
 #pragma warning restore CS0618 // Type or member is obsolete
 			};
 
