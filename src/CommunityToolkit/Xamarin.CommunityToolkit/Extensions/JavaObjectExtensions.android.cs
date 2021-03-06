24d4074e948b2ab128bac7852fed06a430513a20
Cfun <15718354+Cfun1@users.noreply.github.com>
2021-03-06 21:42:04 +0100

    Simplified syntax as requested

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Extensions/JavaObjectExtensions.android.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Extensions/JavaObjectExtensions.android.cs
@@ -5,29 +5,15 @@ namespace Xamarin.CommunityToolkit.Extensions
 	internal static class JavaObjectExtensions
 	{
 		public static bool IsDisposed(this Java.Lang.Object obj)
-		{
-			return obj.Handle == IntPtr.Zero;
-		}
+			=> obj.Handle == IntPtr.Zero;
 
 		public static bool IsAlive(this Java.Lang.Object obj)
-		{
-			if (obj == null)
-				return false;
-
-			return !obj.IsDisposed();
-		}
+			=> obj != null && !obj.IsDisposed();
 
 		public static bool IsDisposed(this global::Android.Runtime.IJavaObject obj)
-		{
-			return obj.Handle == IntPtr.Zero;
-		}
+			=> obj.Handle == IntPtr.Zero;
 
 		public static bool IsAlive(this global::Android.Runtime.IJavaObject obj)
-		{
-			if (obj == null)
-				return false;
-
-			return !obj.IsDisposed();
-		}
+			=> obj != null && !obj.IsDisposed();
 	}
 }
