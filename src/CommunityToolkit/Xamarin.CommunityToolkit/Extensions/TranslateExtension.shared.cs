00b615b75171ae8e316f9e5b0a9048ddb053fba9
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-03-06 14:21:11 -0800

    Update TranslateExtension.shared.cs

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Extensions/TranslateExtension.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Extensions/TranslateExtension.shared.cs
@@ -16,7 +16,9 @@ namespace Xamarin.CommunityToolkit.Extensions
 
 		public BindingBase ProvideValue(IServiceProvider serviceProvider)
 		{
-#if !NETSTANDARD1_0
+#if NETSTANDARD1_0
+throw new NotSupportedException("Translate XAML MarkupExtension is not supported on .NET Standard 1.0");
+#endif
 			#region Required work-around to prevent linker from removing the implementation
 			if (DateTime.Now.Ticks < 0)
 				_ = LocalizationResourceManager.Current[Text];
@@ -30,9 +32,6 @@ namespace Xamarin.CommunityToolkit.Extensions
 				StringFormat = StringFormat
 			};
 			return binding;
-#else
-			throw new NotSupportedException("Translate XAML MarkupExtension is not supported on .NET Standard 1.0");
-#endif
 		}
 	}
 }
