6323fd2808d8be0351ed3fca6deda79da0c5c8f0
Cfun <15718354+Cfun1@users.noreply.github.com>
2021-03-06 12:21:12 +0100

    Implement ImageResourceConverter

---
+++ samples/XCT.Sample/ViewModels/Converters/ImageResourceConverterViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Xamarin.CommunityToolkit.Sample.ViewModels.Converters
+{
+	public class ImageResourceConverterViewModel : BaseViewModel
+	{
+		readonly string img1 = "button.png";
+		readonly string img2 = "logo.png";
+		readonly string imagesPath = "Images";
+		string defaultNamespace;
+
+		string imageName;
+
+		public string ImageName
+		{
+			get => imageName;
+			set
+			{
+				imageName = value;
+				OnPropertyChanged();
+			}
+		}
+
+		ICommand changeImageCommand;
+
+		public ICommand ChangeImageCommand => changeImageCommand ??= new Command(() =>
+			{
+				ImageName = ImageName.Equals(BuildEmbededImagePath(img1)) ?
+							BuildEmbededImagePath(img2) :
+							BuildEmbededImagePath(img1);
+			});
+
+		public ImageResourceConverterViewModel()
+		{
+			defaultNamespace = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+			ImageName = BuildEmbededImagePath(img1);
+		}
+
+		string BuildEmbededImagePath(string imgName)
+			=> $"{defaultNamespace}.{imagesPath}.{imgName}";
+	}
+}
