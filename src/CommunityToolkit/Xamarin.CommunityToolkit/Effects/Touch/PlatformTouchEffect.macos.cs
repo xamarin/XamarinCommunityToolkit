aceb9d39c7c0e90525eca78a100754d53a4dc8cf
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-03-06 19:03:44 -0800

    Update PlatformTouchEffect.macos.cs

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/Touch/PlatformTouchEffect.macos.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/Touch/PlatformTouchEffect.macos.cs
@@ -40,13 +40,17 @@ namespace Xamarin.CommunityToolkit.macOS.Effects
 
 			mouseTrackingView?.RemoveFromSuperview();
 			mouseTrackingView?.Dispose();
+
 			mouseTrackingView = null;
 			effect.Element = null;
 			effect = null;
-			if (gesture != null)
-				Container?.RemoveGestureRecognizer(gesture);
 
-			gesture?.Dispose();
+			if (gesture != null)
+			{
+				Container?.RemoveGestureRecognizer(gesture);
+				gesture.Dispose();
+			}
+
 			gesture = null;
 		}
 	}
@@ -75,18 +79,18 @@ namespace Xamarin.CommunityToolkit.macOS.Effects
 
 		public override void MouseEntered(NSEvent theEvent)
 		{
-			if (effect?.IsDisabled ?? true)
+			if (effect == null || effect.IsDisabled)
 				return;
 
-			effect?.HandleHover(HoverStatus.Entered);
+			effect.HandleHover(HoverStatus.Entered);
 		}
 
 		public override void MouseExited(NSEvent theEvent)
 		{
-			if (effect?.IsDisabled ?? true)
+			if (effect == null || effect.IsDisabled)
 				return;
 
-			effect?.HandleHover(HoverStatus.Exited);
+			effect.HandleHover(HoverStatus.Exited);
 		}
 
 		protected override void Dispose(bool disposing)
@@ -100,6 +104,7 @@ namespace Xamarin.CommunityToolkit.macOS.Effects
 				}
 				effect = null;
 			}
+
 			base.Dispose(disposing);
 		}
 	}
@@ -121,28 +126,31 @@ namespace Xamarin.CommunityToolkit.macOS.Effects
 			{
 				var frame = container?.Frame ?? throw new NullReferenceException();
 				var parent = container?.Superview;
+
 				while (parent != null)
 				{
 					frame = new CoreGraphics.CGRect(frame.X + parent.Frame.X, frame.Y + parent.Frame.Y, frame.Width, frame.Height);
 					parent = parent.Superview;
 				}
+
 				return frame.ToRectangle();
 			}
 		}
 
 		public override void MouseDown(NSEvent mouseEvent)
 		{
-			if (effect?.IsDisabled ?? true)
+			if (effect == null || effect.IsDisabled)
 				return;
 
-			effect?.HandleUserInteraction(TouchInteractionStatus.Started);
-			effect?.HandleTouch(TouchStatus.Started);
+			effect.HandleUserInteraction(TouchInteractionStatus.Started);
+			effect.HandleTouch(TouchStatus.Started);
+
 			base.MouseDown(mouseEvent);
 		}
 
 		public override void MouseUp(NSEvent mouseEvent)
 		{
-			if (effect?.IsDisabled ?? true)
+			if (effect == null || effect.IsDisabled)
 				return;
 
 			if (effect.HoverStatus == HoverStatus.Entered)
@@ -152,26 +160,26 @@ namespace Xamarin.CommunityToolkit.macOS.Effects
 					? TouchStatus.Completed
 					: TouchStatus.Canceled;
 
-				effect?.HandleTouch(status);
+				effect.HandleTouch(status);
 			}
-			effect?.HandleUserInteraction(TouchInteractionStatus.Completed);
+			effect.HandleUserInteraction(TouchInteractionStatus.Completed);
 
 			base.MouseUp(mouseEvent);
 		}
 
 		public override void MouseDragged(NSEvent mouseEvent)
 		{
-			if (effect?.IsDisabled ?? true)
+			if (effect == null || effect.IsDisabled)
 				return;
 
 			var status = ViewRect.Contains(mouseEvent.LocationInWindow.ToPoint()) ? TouchStatus.Started : TouchStatus.Canceled;
 
 			if ((status == TouchStatus.Canceled && effect.HoverStatus == HoverStatus.Entered) ||
 				(status == TouchStatus.Started && effect.HoverStatus == HoverStatus.Exited))
-				effect?.HandleHover(status == TouchStatus.Started ? HoverStatus.Entered : HoverStatus.Exited);
+				effect.HandleHover(status == TouchStatus.Started ? HoverStatus.Entered : HoverStatus.Exited);
 
-			if (effect?.Status != status)
-				effect?.HandleTouch(status);
+			if (effect.Status != status)
+				effect.HandleTouch(status);
 
 			base.MouseDragged(mouseEvent);
 		}
@@ -183,6 +191,7 @@ namespace Xamarin.CommunityToolkit.macOS.Effects
 				effect = null;
 				container = null;
 			}
+
 			base.Dispose(disposing);
 		}
 	}
