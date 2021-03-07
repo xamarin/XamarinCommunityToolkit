256b64e0b36f56108bf14edf882fca875cfa4046
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-03-06 17:25:38 -0800

    Update IsNotNullOrEmptyConverter_Tests.cs

--- src/CommunityToolkit/Xamarin.CommunityToolkit.UnitTests/Converters/IsNotNullOrEmptyConverter_Tests.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit.UnitTests/Converters/IsNotNullOrEmptyConverter_Tests.cs
@@ -4,20 +4,20 @@ using Xunit;
 
 namespace Xamarin.CommunityToolkit.UnitTests.Converters
 {
-    public class IsNotNullOrEmptyConverter_Tests
-    {
-        [Theory]
-        [InlineData("Test", true)]
-        [InlineData(typeof(IsNotNullOrEmptyConverter), true)]
-        [InlineData(null, false)]
-        [InlineData("", false)]
-        public void IsNotNullOrEmptyConverter(object value, bool expectedResult)
-        {
-            var isNotNullOrEmptyConverter = new IsNotNullOrEmptyConverter();
+	public class IsNotNullOrEmptyConverter_Tests
+	{
+		[Theory]
+		[InlineData("Test", true)]
+		[InlineData(typeof(IsNotNullOrEmptyConverter), true)]
+		[InlineData(null, false)]
+		[InlineData("", false)]
+		public void IsNotNullOrEmptyConverter(object value, bool expectedResult)
+		{
+			var isNotNullOrEmptyConverter = new IsNotNullOrEmptyConverter();
 
-            var result = isNotNullOrEmptyConverter.Convert(value, typeof(IsNotNullOrEmptyConverter_Tests), null, CultureInfo.CurrentCulture);
+			var result = isNotNullOrEmptyConverter.Convert(value, typeof(IsNotNullOrEmptyConverter_Tests), null, CultureInfo.CurrentCulture);
 
-            Assert.Equal(result, expectedResult);
-        }
-    }
+			Assert.Equal(result, expectedResult);
+		}
+	}
 }
