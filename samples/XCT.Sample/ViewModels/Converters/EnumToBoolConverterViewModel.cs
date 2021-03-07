ca479129c9ff86f6a190ca9361aad1b4fc87f108
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-03-06 17:18:47 -0800

    Update EnumToBoolConverterViewModel.cs

--- samples/XCT.Sample/ViewModels/Converters/EnumToBoolConverterViewModel.cs
+++ samples/XCT.Sample/ViewModels/Converters/EnumToBoolConverterViewModel.cs
@@ -1,25 +1,25 @@
 ﻿namespace Xamarin.CommunityToolkit.Sample.ViewModels.Converters
 {
-    public class EnumToBoolConverterViewModel : BaseViewModel
-    {
-        private IssueState selectedState = IssueState.None;
+	public class EnumToBoolConverterViewModel : BaseViewModel
+	{
+		private IssueState selectedState = IssueState.None;
 
-        public IssueState SelectedState
-        {
-            get => selectedState;
-            set => SetProperty(ref selectedState, value);
-        }
-    }
+		public IssueState SelectedState
+		{
+			get => selectedState;
+			set => SetProperty(ref selectedState, value);
+		}
+	}
 
-    public enum IssueState
-    {
-        None = 0,
-        New = 1,
-        Open = 2,
-        Waiting = 3,
-        Developing = 4,
-        WantFix = 5,
-        Rejected = 6,
-        Resolved = 7
-    }
+	public enum IssueState
+	{
+		None = 0,
+		New = 1,
+		Open = 2,
+		Waiting = 3,
+		Developing = 4,
+		WantFix = 5,
+		Rejected = 6,
+		Resolved = 7
+	}
 }
