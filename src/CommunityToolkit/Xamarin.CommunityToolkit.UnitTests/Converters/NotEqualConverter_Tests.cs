88eff3069fd21a43d8d8becaba2639ddef44df8d
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-03-06 17:30:55 -0800

    Update NotEqualConverter_Tests.cs

--- src/CommunityToolkit/Xamarin.CommunityToolkit.UnitTests/Converters/NotEqualConverter_Tests.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit.UnitTests/Converters/NotEqualConverter_Tests.cs
@@ -4,20 +4,20 @@ using Xunit;
 
 namespace Xamarin.CommunityToolkit.UnitTests.Converters
 {
-    public class NotEqualConverter_Tests
-    {
-        [Theory]
-        [InlineData(true, true, false)]
-        [InlineData(int.MaxValue, int.MinValue, true)]
-        [InlineData("Test", true, true)]
-        [InlineData(null, null, false)]
-        public void NotEqualConverter(object value, object comparedValue, bool expectedResult)
-        {
-            var notEqualConverter = new NotEqualConverter();
+	public class NotEqualConverter_Tests
+	{
+		[Theory]
+		[InlineData(true, true, false)]
+		[InlineData(int.MaxValue, int.MinValue, true)]
+		[InlineData("Test", true, true)]
+		[InlineData(null, null, false)]
+		public void NotEqualConverter(object value, object comparedValue, bool expectedResult)
+		{
+			var notEqualConverter = new NotEqualConverter();
 
-            var result = notEqualConverter.Convert(value, typeof(NotEqualConverter_Tests), comparedValue, CultureInfo.CurrentCulture);
+			var result = notEqualConverter.Convert(value, typeof(NotEqualConverter_Tests), comparedValue, CultureInfo.CurrentCulture);
 
-            Assert.Equal(result, expectedResult);
-        }
-    }
+			Assert.Equal(result, expectedResult);
+		}
+	}
 }
