1c3fcb51a72f45dbf462d1fdd80d376e258dad6a
Cfun <15718354+Cfun1@users.noreply.github.com>
2021-03-13 23:49:29 +0100

    Fix GalleryPage and VM

---
+++ samples/XCT.Sample/ViewModels/Extensions/ExtensionsGalleryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Xamarin.CommunityToolkit.Extensions;
+using Xamarin.CommunityToolkit.Sample.Models;
+using Xamarin.CommunityToolkit.Sample.Pages.Effects;
+
+namespace Xamarin.CommunityToolkit.Sample.ViewModels.Effects
+{
+	public class ExtensionsGalleryViewModel : BaseGalleryViewModel
+	{
+		protected override IEnumerable<SectionModel> CreateItems() => new[]
+		{
+			new SectionModel(
+				typeof(ImageResourceExtensionPage),
+				nameof(ImageResourceExtension),
+				"The ImageResourceExtension is a xaml extension that will help to display an image from an embedded resource."),
+		};
+	}
+}
