a122c80a4b38f7f2d4e587ce828239a0d67282ee
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-03-16 17:53:32 -0700

    Update TextCaseConverter_Tests.cs

--- src/CommunityToolkit/Xamarin.CommunityToolkit.UnitTests/Converters/TextCaseConverter_Tests.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit.UnitTests/Converters/TextCaseConverter_Tests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Globalization;
 using NUnit.Framework;
 using Xamarin.CommunityToolkit.Converters;
@@ -12,24 +11,22 @@ namespace Xamarin.CommunityToolkit.UnitTests.Converters
 		const string test = nameof(test);
 		const string t = nameof(t);
 
-		static IEnumerable<object[]> GetTestData()
+		static IEnumerable<object?[]> GetTestData()
 		{
-			yield return new object[] { test, TextCaseType.Lower, test };
-			yield return new object[] { test, TextCaseType.Upper, "TEST" };
-			yield return new object[] { test, TextCaseType.None, test };
-			yield return new object[] { test, TextCaseType.FirstUpperRestLower, "Test" };
-			yield return new object[] { t, TextCaseType.Upper, "T" };
-			yield return new object[] { t, TextCaseType.Lower, t };
-			yield return new object[] { t, TextCaseType.None, t };
-			yield return new object[] { t, TextCaseType.FirstUpperRestLower, "T" };
-			yield return new object[] { string.Empty, TextCaseType.FirstUpperRestLower, string.Empty };
-#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
-			yield return new object[] { null, TextCaseType.None, null };
-#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
-			yield return new object[] { MockEnum.Foo, TextCaseType.Lower, "foo" };
-			yield return new object[] { MockEnum.Bar, TextCaseType.None, "Bar" };
-			yield return new object[] { MockEnum.Baz, TextCaseType.Upper, "BAZ" };
-			yield return new object[] { new MockItem { Title = "Test Item", Completed = true }, TextCaseType.Upper, "TEST ITEM IS COMPLETED" };
+			yield return new object?[] { test, TextCaseType.Lower, test };
+			yield return new object?[] { test, TextCaseType.Upper, "TEST" };
+			yield return new object?[] { test, TextCaseType.None, test };
+			yield return new object?[] { test, TextCaseType.FirstUpperRestLower, "Test" };
+			yield return new object?[] { t, TextCaseType.Upper, "T" };
+			yield return new object?[] { t, TextCaseType.Lower, t };
+			yield return new object?[] { t, TextCaseType.None, t };
+			yield return new object?[] { t, TextCaseType.FirstUpperRestLower, "T" };
+			yield return new object?[] { string.Empty, TextCaseType.FirstUpperRestLower, string.Empty };
+			yield return new object?[] { null, TextCaseType.None, null };
+			yield return new object?[] { MockEnum.Foo, TextCaseType.Lower, "foo" };
+			yield return new object?[] { MockEnum.Bar, TextCaseType.None, "Bar" };
+			yield return new object?[] { MockEnum.Baz, TextCaseType.Upper, "BAZ" };
+			yield return new object?[] { new MockItem { Title = "Test Item", Completed = true }, TextCaseType.Upper, "TEST ITEM IS COMPLETED" };
 		}
 
 		[TestCaseSource(nameof(GetTestData))]
