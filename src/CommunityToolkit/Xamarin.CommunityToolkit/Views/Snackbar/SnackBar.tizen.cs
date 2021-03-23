ee5dda55e1d27f022a07a5b0610696890bcdfee6
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-03-23 10:09:33 -0700

    Update SnackBar.tizen.cs

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Views/Snackbar/SnackBar.tizen.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Views/Snackbar/SnackBar.tizen.cs
@@ -35,13 +35,13 @@ namespace Xamarin.CommunityToolkit.UI.Views
 				};
 			}
 
-			snackBarDialog.TimedOut += (s, evt) => DismissSnackBar();
+			snackBarDialog.TimedOut += DismissSnackBar;
 
-			snackBarDialog.BackButtonPressed += (s, evt) => DismissSnackBar();
+			snackBarDialog.BackButtonPressed += DismissSnackBar;
 
 			snackBarDialog.Show();
 
-			void DismissSnackBar()
+			void DismissSnackBar(object? sender, EventArgs e)
 			{
 				snackBarDialog.Dismiss();
 				arguments.SetResult(false);
