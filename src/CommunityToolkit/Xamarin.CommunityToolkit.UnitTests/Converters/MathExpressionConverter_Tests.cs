6b99357b1e20b61ce9f3d3170487ec1096fd3531
Matthew S <sattew@yandex.com>
2021-03-24 01:58:30 +0300

    implemented rpn algorithm

---
+++ src/CommunityToolkit/Xamarin.CommunityToolkit.UnitTests/Converters/MathExpressionConverter_Tests.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using NUnit.Framework;
+using Xamarin.CommunityToolkit.Converters;
+
+namespace Xamarin.CommunityToolkit.UnitTests.Converters
+{
+	public class MathExpressionConverter_Tests
+	{
+		[TestCase("3 + 4 * 2 + cos(100 + 20) / (1 - 5)^2 + pi + pow(20,2)", 2d)]
+		[TestCase("3 + 4 * 2 / (1 - 5)^2", 2d)]
+		public void BoolToObjectConvert(string value, object expectedResult)
+		{
+			var boolObjectConverter = new MathExpressionConverter();
+
+			var result = boolObjectConverter.Convert(value, typeof(MathExpressionConverter_Tests), null, CultureInfo.CurrentCulture);
+
+			Assert.AreEqual(result, expectedResult);
+		}
+	}
+}
---
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Converters/MathExpressionConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.CommunityToolkit.Core;
+using Xamarin.CommunityToolkit.Extensions.Internals;
+using Xamarin.Forms;
+
+namespace Xamarin.CommunityToolkit.Converters
+{
+	public class MathExpressionConverter : ValueConverterExtension, IValueConverter
+	{
+		/// <summary>
+		/// Calculate the incoming expression string.
+		/// </summary>
+		/// <param name="value">The expression to calculate.</param>
+		/// <param name="targetType">The type of the binding target property. This is not implemented.</param>
+		/// <param name="parameter">Additional parameter for the converter to handle. This is not implemented.</param>
+		/// <param name="culture">The culture to use in the converter. This is not implemented.</param>
+		/// <returns>A <see cref="double"/> The result of calculating an expression.</returns>
+		public object Convert(object? value, Type? targetType, object? parameter, CultureInfo culture)
+		{
+			if (value is not string expression)
+
+				throw new ArgumentException("The value should be of type String");
+
+
+			var math = new MathExpression(expression);
+			var result = math.Calculate();
+			return result;
+		}
+
+		/// <summary>
+		/// This method is not implemented and will throw a <see cref="NotImplementedException"/>.
+		/// </summary>
+		/// <param name="value">N/A</param>
+		/// <param name="targetType">N/A</param>
+		/// <param name="parameter">N/A</param>
+		/// <param name="culture">N/A</param>
+		/// <returns>N/A</returns>
+		public object ConvertBack(object? value, Type? targetType, object? parameter, CultureInfo? culture)
+			=> throw new NotImplementedException();
+	}
+}
