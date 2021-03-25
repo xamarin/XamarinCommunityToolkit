b6b7189d8bc61b5235ad15208500e9c309003694
Andrei Misiukevich <andrei.misiukevich@gmail.com>
2021-03-26 00:24:21 +0100

    Show initials if stream is empty https://github.com/xamarin/XamarinCommunityToolkit/issues/1120

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Views/AvatarView/AvatarView.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Views/AvatarView/AvatarView.shared.cs
@@ -360,7 +360,7 @@ namespace Xamarin.CommunityToolkit.UI.Views
 
 						var stream = await getStreamAsync(uriImageSource, imageLoadingTokenSource.Token);
 
-						source = stream != null
+						source = stream != null && stream != Stream.Null && stream.CanRead && stream.Length > 0
 							? ImageSource.FromStream(async (token) => stream?.CanRead ?? true ? stream : (stream = await getStreamAsync(uriImageSource, token)))
 							: null;
 
