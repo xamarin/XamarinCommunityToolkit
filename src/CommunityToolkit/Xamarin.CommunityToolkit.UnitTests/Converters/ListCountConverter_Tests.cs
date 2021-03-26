5ad049e9586e712dafbb23dd7f74902f212196c3
Gustavo Oliveira <gu.ol@hotmail.com>
2021-03-26 20:15:59 +1000

    Added UnitTests

---
+++ src/CommunityToolkit/Xamarin.CommunityToolkit.UnitTests/Converters/ListCountConverter_Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.CommunityToolkit.Converters;
+
+namespace Xamarin.CommunityToolkit.UnitTests.Converters
+{
+	public class ListCountConverter_Tests
+	{
+		public static IEnumerable<object?[]> GetData() => new List<object?[]>
+		{
+			new object[] { new List<string>(), 0 },
+			new object[] { new List<string>() { "TestValue" }, 1 },
+			new object?[] { null, 0 },
+			new object[] { Enumerable.Range(1, 3), 3 },
+		};
+
+		[TestCaseSource(nameof(GetData))]
+		public void ListCountConverter(object value, bool expectedResult)
+		{
+			var listCountConverter = new ListCountConverter();
+
+			var result = listCountConverter.Convert(value, typeof(ListCountConverter), null, CultureInfo.CurrentCulture);
+
+			Assert.AreEqual(result, expectedResult);
+		}
+
+		[TestCase(0)]
+		public void InValidConverterValuesThrowArgumenException(object value)
+		{
+			var listCountConverter = new ListCountConverter();
+
+			Assert.Throws<ArgumentException>(() => listCountConverter.Convert(value, typeof(ListCountConverter), null, CultureInfo.CurrentCulture));
+		}
+	}
+}
