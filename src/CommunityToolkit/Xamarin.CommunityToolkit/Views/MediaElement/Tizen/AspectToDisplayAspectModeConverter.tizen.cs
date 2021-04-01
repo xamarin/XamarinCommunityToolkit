3c1968b30fce0fb334305f5c82c85212e99e1d3b
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-03-31 19:18:28 -0700

    Implement Nullable

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Views/MediaElement/Tizen/AspectToDisplayAspectModeConverter.tizen.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Views/MediaElement/Tizen/AspectToDisplayAspectModeConverter.tizen.cs
@@ -6,12 +6,15 @@ namespace Xamarin.CommunityToolkit.UI.Views
 {
 	public class AspectToDisplayAspectModeConverter : IValueConverter
 	{
-		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		public object Convert(object? value, Type? targetType, object? parameter, CultureInfo? culture)
 		{
+			if (value == null)
+				throw new ArgumentNullException(nameof(value));
+
 			return ((Aspect)value).ToDisplayAspectMode();
 		}
 
-		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		public object ConvertBack(object? value, Type? targetType, object? parameter, CultureInfo? culture)
 		{
 			throw new NotImplementedException();
 		}
