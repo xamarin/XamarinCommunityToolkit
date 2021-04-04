b912f27bc6b41fbf9037d5bd0adb8afcc77108c7
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-04-03 18:26:26 -0700

    Update AspectToDisplayAspectModeConverter.tizen.cs

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Views/MediaElement/Tizen/AspectToDisplayAspectModeConverter.tizen.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Views/MediaElement/Tizen/AspectToDisplayAspectModeConverter.tizen.cs
@@ -14,9 +14,6 @@ namespace Xamarin.CommunityToolkit.UI.Views
 			return ((Aspect)value).ToDisplayAspectMode();
 		}
 
-		public object? ConvertBack(object? value, Type? targetType, object? parameter, CultureInfo? culture)
-		{
-			throw new NotImplementedException();
-		}
+		public object? ConvertBack(object? value, Type? targetType, object? parameter, CultureInfo? culture) => throw new NotImplementedException();
 	}
 }
