b1bababaf1f2597de7d1bf792a1cca7bab1f9a6a
Pedro Jesus <pedrojesus.cefet@gmail.com>
2021-04-06 14:50:26 -0300

    fixed namespace

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Views/RangeSlider/ContentButton.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Views/RangeSlider/ContentButton.shared.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Input;
 using Xamarin.CommunityToolkit.Helpers;
+using Xamarin.CommunityToolkit.UI.Views.Internals;
 using Xamarin.Forms;
 
 namespace Xamarin.CommunityToolkit.UI.Views
