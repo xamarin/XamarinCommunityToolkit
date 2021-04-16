6fc519effca74d96eec26d01efbc9725224f345a
Stephan Kieburg <stephan.kieburg@gmail.com>
2021-04-16 15:42:11 +0200

    Adressed review comments
    Throw ArgumentException if the value passed to convert is not an enum
    Reverted accidental change to Xamarin.CommunityToolkit.Sample.csproj

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Converters/EnumToIntConverter.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Converters/EnumToIntConverter.shared.cs
@@ -11,7 +11,7 @@ namespace Xamarin.CommunityToolkit.Converters
 	public class EnumToIntConverter : ValueConverterExtension, IValueConverter
 	{
 		/// <summary>
-		/// Convert an <see cref="Enum"/> to corresponding underlying <see cref="int"/>
+		/// Convert a default <see cref="Enum"/> (i.e., extending <see cref="int"/>) to corresponding underlying <see cref="int"/>
 		/// </summary>
 		/// <param name="value"><see cref="Enum"/> value to convert</param>
 		/// <param name="targetType">The type of the binding target property. This is not used.</param>
@@ -19,7 +19,7 @@ namespace Xamarin.CommunityToolkit.Converters
 		/// <param name="culture">The culture to use in the converter. This is not used.</param>
 		/// <returns>The underlying <see cref="int"/> value of the passed value, or -1 if the value is not an enumeration type</returns>
 		public object Convert(object? value, Type? targetType, object? parameter, CultureInfo? culture) =>
-			value is Enum ? System.Convert.ToInt32(value) : -1;
+			value is Enum ? System.Convert.ToInt32(value) : throw new ArgumentException($"{value?.GetType().Name} is not a valid enumeration type");
 
 		/// <summary>
 		/// Returns the <see cref="Enum"/> associated with the specified <see cref="int"/> value defined in the targetType
