97c899f27a2567eb9c7ec05910ec0b7d393f1902
Stephan Kieburg <stephan.kieburg@gmail.com>
2021-04-16 22:18:49 +0200

    Update EnumToIntConverter.shared.cs

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Converters/EnumToIntConverter.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Converters/EnumToIntConverter.shared.cs
@@ -14,10 +14,11 @@ namespace Xamarin.CommunityToolkit.Converters
 		/// Convert a default <see cref="Enum"/> (i.e., extending <see cref="int"/>) to corresponding underlying <see cref="int"/>
 		/// </summary>
 		/// <param name="value"><see cref="Enum"/> value to convert</param>
-		/// <param name="targetType">The type of the binding target property. This is not used.</param>
-		/// <param name="parameter">Additional parameter for converter. This is not used.</param>
-		/// <param name="culture">The culture to use in the converter. This is not used.</param>
-		/// <returns>The underlying <see cref="int"/> value of the passed value, or -1 if the value is not an enumeration type</returns>
+		/// <param name="targetType">Unused: type of the binding target property</param>
+		/// <param name="parameter">Unused: Additional parameter for converter</param>
+		/// <param name="culture">Unused: Culture to use in the converter</param>
+		/// <returns>The underlying <see cref="int"/> value of the passed enum value</returns>
+		/// <exception cref="ArgumentException">If value is not an enumeration type</exception>
 		public object Convert(object? value, Type? targetType, object? parameter, CultureInfo? culture) =>
 			value is Enum ? System.Convert.ToInt32(value) : throw new ArgumentException($"{value?.GetType().Name} is not a valid enumeration type");
 
@@ -26,9 +27,9 @@ namespace Xamarin.CommunityToolkit.Converters
 		/// </summary>
 		/// <param name="value"><see cref="Enum"/> value to convert</param>
 		/// <param name="targetType">The type of the binding target property. Expected to be an enum.</param>
-		/// <param name="parameter">Additional parameter for converter. This is not used.</param>
-		/// <param name="culture">The culture to use in the converter. This is not used.</param>
-		/// <returns>The underlying <see cref="Enum"/> of the associated targetType, or 0 if the value is not an enumeration type</returns>
+		/// <param name="parameter">Unused: Additional parameter for converter</param>
+		/// <param name="culture">Unused: Culture to use in the converter</param>
+		/// <returns>The underlying <see cref="Enum"/> of the associated targetType</returns>
 		/// <exception cref="ArgumentException">If value is not a valid value in the targetType enum</exception>
 		public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo? culture) =>
 			value is int enumIntVal && Enum.IsDefined(targetType, enumIntVal) ? Enum.ToObject(targetType, enumIntVal) : throw new ArgumentException($"{value} is not valid for {targetType.Name}");
