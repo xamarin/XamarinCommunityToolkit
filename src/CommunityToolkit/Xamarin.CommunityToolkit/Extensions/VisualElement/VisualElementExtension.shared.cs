a53e0bd6fb1d76b9bf1591ed98d640d66fd52ce0
Vladislav Antonyuk <vlad.antonyuk@outlook.com>
2021-04-16 14:29:11 +0300

    Add summary for the toast/snackbar public methods

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Extensions/VisualElement/VisualElementExtension.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Extensions/VisualElement/VisualElementExtension.shared.cs
@@ -41,6 +41,13 @@ namespace Xamarin.CommunityToolkit.Extensions
 				element.AbortAnimation(name);
 		}
 
+		/// <summary>
+		/// Display toast with the default visual configuration
+		/// </summary>
+		/// <param name="visualElement">Toast anchor</param>
+		/// <param name="message">Toast text</param>
+		/// <param name="durationMilliseconds">Toast duration (milliseconds)</param>
+		/// <returns>Task</returns>
 		public static async Task DisplayToastAsync(this VisualElement visualElement, string message, int durationMilliseconds = 3000)
 		{
 			_ = visualElement ?? throw new ArgumentNullException(nameof(visualElement));
@@ -61,6 +68,12 @@ namespace Xamarin.CommunityToolkit.Extensions
 			await args.Result.Task;
 		}
 
+		/// <summary>
+		/// Display toast with the custom options
+		/// </summary>
+		/// <param name="visualElement">Toast anchor</param>
+		/// <param name="toastOptions">Toast options</param>
+		/// <returns>Task</returns>
 		public static Task DisplayToastAsync(this VisualElement visualElement, ToastOptions toastOptions)
 		{
 			_ = visualElement ?? throw new ArgumentNullException(nameof(visualElement));
@@ -79,6 +92,15 @@ namespace Xamarin.CommunityToolkit.Extensions
 			return options.Result.Task;
 		}
 
+		/// <summary>
+		/// Display snackbar with the default visual configuration
+		/// </summary>
+		/// <param name="visualElement">Anchor element</param>
+		/// <param name="message">Text of the snackbar</param>
+		/// <param name="actionButtonText">Text of the snackbar button</param>
+		/// <param name="action">Action of the snackbar button</param>
+		/// <param name="duration">Snackbar duration</param>
+		/// <returns>True if snackbar action is executed. False if snackbar is closed by timeout</returns>
 		public static async Task<bool> DisplaySnackBarAsync(this VisualElement visualElement, string message, string actionButtonText, Func<Task> action, TimeSpan? duration = null)
 		{
 			_ = visualElement ?? throw new ArgumentNullException(nameof(visualElement));
@@ -108,6 +130,12 @@ namespace Xamarin.CommunityToolkit.Extensions
 			return result;
 		}
 
+		/// <summary>
+		/// Display snackbar with custom configuration
+		/// </summary>
+		/// <param name="visualElement">Snackbar anchor</param>
+		/// <param name="snackBarOptions">Snackbar options</param>
+		/// <returns>True if snackbar action is executed. False if snackbar is closed by timeout</returns>
 		public static async Task<bool> DisplaySnackBarAsync(this VisualElement visualElement, SnackBarOptions snackBarOptions)
 		{
 			_ = visualElement ?? throw new ArgumentNullException(nameof(visualElement));
