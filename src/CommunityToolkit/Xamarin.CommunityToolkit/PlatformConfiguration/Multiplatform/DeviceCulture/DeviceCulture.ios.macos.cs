146c315f47fdac0da1891c1420c735fbb88a7cf1
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2021-04-26 15:57:36 +0300

    Change PreferredLanguages to PreferredLocalizations on iOS

--- src/CommunityToolkit/Xamarin.CommunityToolkit/PlatformConfiguration/Multiplatform/DeviceCulture/DeviceCulture.ios.macos.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/PlatformConfiguration/Multiplatform/DeviceCulture/DeviceCulture.ios.macos.cs
@@ -6,6 +6,6 @@ namespace Xamarin.CommunityToolkit.PlatformConfiguration.Multiplatform
 	public static partial class DeviceCulture
 	{
 		private static partial IReadOnlyList<string> GetPreferredCultureStrings() =>
-			NSLocale.PreferredLanguages;
+			NSBundle.MainBundle.PreferredLocalizations;
 	}
 }
