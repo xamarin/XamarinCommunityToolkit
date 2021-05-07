54e3e9444269e269d4a13cf414ce76e7ca1025ba
James Montemagno <james.montemagno@gmail.com>
2021-05-07 08:11:34 -0700

    Base implementations for SemanticEffect and SemanticOrderView (#1240)
    * Base implementations for SemanticEffect and SemanticOrderView
    
    * - uwp and iOS fix
    
    * - uwp tab index
    
    * - hint and description
    
    * - fix iOS to auto set to important for accessibility
    
    * Add samples and fix up crashes
    
    * - for UI Test reason switch to using a delegate for reading content Description on Android
    
    * - use the delegate if the automation id is set
    
    * - fixes
    
    * - set yes less often
    
    * Remove IsInAccessibleTree from PancakeView
    
    doesn't make a big difference on Android, but the property worsens accessibility on iOS
    
    * Update SemanticEffectPage.xaml
    
    * Update SemanticOrderViewPage.xaml
    
    * Update SemanticOrderViewPage.xaml.cs
    
    * Fix Semantic Description on Android
    
    * Reword SemanticEffect about
    
    * - added temporary workaround for making a screen clickable
    
    * Fix SemanticOrderViewPage sample
    
    * Fix NRE thrown on back nav from iOS SemanticOrderView page
    
    * - fix UWP router
    
    * - fix null checks
    
    * - changed first button to button so uwp sample is interesting
    
    * Clean up code - Pedro's feedback
    
    Co-authored-by: Shane Neuville <shneuvil@microsoft.com>
    Co-authored-by: Javier Suárez <javiersuarezruiz@hotmail.com>
    Co-authored-by: Rachel Kang <rachelkang@microsoft.com>
    Co-authored-by: Gerald Versluis <gerald.versluis@microsoft.com>

---
+++ samples/XCT.Sample/Pages/Effects/SemanticEffectPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace Xamarin.CommunityToolkit.Sample.Pages.Effects
+{
+	public partial class SemanticEffectPage : BasePage
+	{
+		public SemanticEffectPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/Semantic/SemanticEffectRouter.uwp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Automation;
+using Windows.UI.Xaml.Automation.Peers;
+using Xamarin.CommunityToolkit.Effects;
+using Effects = Xamarin.CommunityToolkit.UWP.Effects;
+
+[assembly: Xamarin.Forms.ExportEffect(typeof(Effects.SemanticEffectRouter), nameof(SemanticEffectRouter))]
+
+namespace Xamarin.CommunityToolkit.UWP.Effects
+{
+	/// <summary>
+	/// UWP implementation of the <see cref="SemanticEffect" />
+	/// </summary>
+	public class SemanticEffectRouter : SemanticEffectRouterBase<SemanticEffectRouter>
+	{
+		public SemanticEffectRouter()
+		{
+		}
+
+		protected override void Update(FrameworkElement view, SemanticEffectRouter effect)
+		{
+			var headingLevel = (AutomationHeadingLevel)((int)SemanticEffect.GetHeadingLevel(Element));
+			AutomationProperties.SetHeadingLevel(view, headingLevel);
+
+			AutomationProperties.SetName(view, SemanticEffect.GetDescription(Element) ?? string.Empty);
+			AutomationProperties.SetHelpText(view, SemanticEffect.GetHint(Element) ?? string.Empty);
+		}
+
+		protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+		{
+			base.OnElementPropertyChanged(args);
+
+			if (args.PropertyName == SemanticEffect.HeadingLevelProperty.PropertyName ||
+				args.PropertyName == SemanticEffect.DescriptionProperty.PropertyName ||
+				args.PropertyName == SemanticEffect.HintProperty.PropertyName)
+			{
+				Update();
+			}
+		}
+	}
+}
