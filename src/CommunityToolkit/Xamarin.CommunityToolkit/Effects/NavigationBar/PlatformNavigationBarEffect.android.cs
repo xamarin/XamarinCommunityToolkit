34f04505a85ddccf4683e80f01ff3419fdd33df1
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2021-05-16 03:07:23 +0300

    Remove using

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/NavigationBar/PlatformNavigationBarEffect.android.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/NavigationBar/PlatformNavigationBarEffect.android.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using Android.App;
 using Android.Views;
 using Xamarin.CommunityToolkit.Android.Effects;
--- src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/StatusBar/PlatformStatusBarEffect.android.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/StatusBar/PlatformStatusBarEffect.android.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using Android.Views;
 using Xamarin.CommunityToolkit.Android.Effects;
 using Xamarin.CommunityToolkit.Effects;
