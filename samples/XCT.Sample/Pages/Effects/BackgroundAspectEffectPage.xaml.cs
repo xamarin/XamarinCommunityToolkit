99b695230bc456d6a059de3e41aa2f041025054e
Axemasta <33064621+Axemasta@users.noreply.github.com>
2021-06-07 15:10:29 +0100

    Properly Implemented Sample
    The sample now shows the effect being updated in realtime

--- samples/XCT.Sample/Pages/Effects/BackgroundAspectEffectPage.xaml.cs
+++ samples/XCT.Sample/Pages/Effects/BackgroundAspectEffectPage.xaml.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-
+﻿
+using System.Diagnostics;
+using System.Linq;
+using Xamarin.CommunityToolkit.Effects;
 using Xamarin.Forms;
 
 namespace Xamarin.CommunityToolkit.Sample.Pages.Effects
 {
-	public partial class BackgroundAspectEffectPage : BasePage
-	{
-		public BackgroundAspectEffectPage() => InitializeComponent();
-	}
+    public partial class BackgroundAspectEffectPage : BasePage
+    {
+        public BackgroundAspectEffectPage() => InitializeComponent();
+
+        void OnOptionChecked(object sender, CheckedChangedEventArgs e)
+        {
+            Aspect? aspect = null;
+
+            if (sender == RadioFill && RadioFill.IsChecked)
+            {
+                aspect = Aspect.Fill;
+            }
+            else if (sender == RadioAspectFill && RadioAspectFill.IsChecked)
+            {
+                aspect = Aspect.AspectFill;
+            }
+            else if (sender == RadioAspectFit && RadioAspectFit.IsChecked)
+            {
+                aspect = Aspect.AspectFit;
+            }
+            else
+            {
+                return;
+            }
+
+            Debug.WriteLine($"Change the current effect to: {aspect}");
+
+            BackgroundAspectEffect.SetAspect(this, aspect.Value);
+        }
+    }
 }
