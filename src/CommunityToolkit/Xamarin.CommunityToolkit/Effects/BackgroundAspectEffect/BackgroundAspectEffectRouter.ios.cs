2d2e5e1ee82f57246508d22c37b49966c2f3e676
Axemasta <33064621+Axemasta@users.noreply.github.com>
2021-06-07 14:21:31 +0100

    Implemented Platform Effect On iOS
    I have done the first rough cut of the effect on iOS. I probably need to add some cleanup for the cancellation source which i will address next

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/BackgroundAspectEffect/BackgroundAspectEffectRouter.ios.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/BackgroundAspectEffect/BackgroundAspectEffectRouter.ios.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using UIKit;
 using Xamarin.CommunityToolkit.Effects;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
@@ -11,14 +15,136 @@ namespace Xamarin.CommunityToolkit.iOS.Effects
 {
 	public class BackgroundAspectEffectRouter : PlatformEffect
 	{
-		protected override void OnAttached()
+		UIImageView? imageView;
+
+		CancellationTokenSource cancellationTokenSource;
+
+		public BackgroundAspectEffectRouter()
 		{
-			Debugger.Break();
+			cancellationTokenSource = new CancellationTokenSource();
 		}
 
-		protected override void OnDetached()
-		{
+		protected override void OnAttached()
+			=> Task.Run(ApplyBackgroundImageAsync, cancellationTokenSource.Token);
 
+		protected override void OnDetached()
+			=> ClearBackgroundImage();
+
+		protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+		{
+			base.OnElementPropertyChanged(args);
+
+			if (!args.PropertyName.Equals(BackgroundAspectEffect.AspectProperty.PropertyName))
+				return;
+
+			cancellationTokenSource.Cancel();
+
+			Task.Run(ApplyBackgroundImageAsync, cancellationTokenSource.Token);
+		}
+
+		async Task ApplyBackgroundImageAsync()
+		{
+			var contentPage = Element as ContentPage;
+
+			if (Element is null || contentPage is null || contentPage.BackgroundImageSource is null)
+				return;
+
+			var image = await GetBackgroundImageFromSource(contentPage.BackgroundImageSource);
+
+			if (image is null)
+				return;
+
+			var fallbackColor = contentPage.BackgroundColor.ToUIColor();
+
+			Device.BeginInvokeOnMainThread(() => ApplyImageToView(image, fallbackColor));
+		}
+
+		void ApplyImageToView(UIImage image, UIColor color)
+		{
+			var effectParam = BackgroundAspectEffect.GetAspect(Element);
+
+			imageView = CreateImageView(image, effectParam);
+
+			Container.BackgroundColor = color;
+
+			Container.InsertSubview(imageView, 0);
+
+			ApplyConstraints(imageView, Container);
+		}
+
+		void ClearBackgroundImage()
+		{
+			if (imageView is null)
+				return;
+
+			cancellationTokenSource.Cancel();
+
+			imageView.RemoveFromSuperview();
+
+			imageView.Dispose();
+		}
+
+		async Task<UIImage> GetBackgroundImageFromSource(ImageSource imageSource)
+		{
+			IImageSourceHandler handler;
+
+			if (imageSource is FileImageSource)
+			{
+				handler = new FileImageSourceHandler();
+			}
+			else if (imageSource is StreamImageSource)
+			{
+				handler = new StreamImagesourceHandler();
+			}
+			else if (imageSource is UriImageSource)
+			{
+				handler = new ImageLoaderSourceHandler();
+			}
+			else
+			{
+				throw new NotSupportedException($"ImageSource is not supported by this effect: {imageSource.GetType().Name}");
+			}
+
+			return await handler.LoadImageAsync(imageSource);
+		}
+
+		UIImageView CreateImageView(UIImage uIImage, Aspect aspect)
+		{
+			var imageView = new UIImageView();
+			imageView.Image = uIImage;
+			imageView.ContentMode = GetContentModeForAspect(aspect);
+			imageView.TranslatesAutoresizingMaskIntoConstraints = false;
+
+			return imageView;
+		}
+
+		void ApplyConstraints(UIView child, UIView parent)
+		{
+			var constraints = new NSLayoutConstraint[]
+			{
+				child.TopAnchor.ConstraintEqualTo(parent.TopAnchor),
+				child.LeadingAnchor.ConstraintEqualTo(parent.LeadingAnchor),
+				child.TrailingAnchor.ConstraintEqualTo(parent.TrailingAnchor),
+				child.BottomAnchor.ConstraintEqualTo(parent.BottomAnchor)
+			};
+
+			NSLayoutConstraint.ActivateConstraints(constraints);
+		}
+
+		UIViewContentMode GetContentModeForAspect(Aspect aspect)
+		{
+			switch (aspect)
+			{
+				default:
+				case Aspect.Fill:
+					return UIViewContentMode.ScaleToFill;
+
+				case Aspect.AspectFill:
+					return UIViewContentMode.ScaleAspectFill;
+
+				case Aspect.AspectFit:
+					return UIViewContentMode.ScaleAspectFit;
+			}
 		}
 	}
 }
