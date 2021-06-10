522a7a22b294305aa09fa028132952dec2aa33b9
Yann Zahringer Ferrando <yann.zahringer@madsense.com>
2021-06-10 08:39:19 +0200

    Effect replaced by event subscription

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/CornerRadius/CornerRadiusEffect.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/CornerRadius/CornerRadiusEffect.shared.cs
@@ -1,19 +1,36 @@
 ﻿using System;
-using System.ComponentModel;
-using System.Linq;
 using Xamarin.Forms;
 using Xamarin.Forms.Shapes;
 
 namespace Xamarin.CommunityToolkit.Effects
 {
-	public class CornerRadiusEffect : NullEffect
+	public class CornerRadiusEffect
 	{
 		public static readonly BindableProperty CornerRadiusProperty = BindableProperty.CreateAttached(
 			nameof(CornerRadius),
 			typeof(CornerRadius),
 			typeof(CornerRadiusEffect),
 			default(CornerRadius),
-			propertyChanged: TryGenerateEffect);
+			propertyChanged: OnCornerRadiusPropertyChanged);
+
+		static void OnCornerRadiusPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+		{
+			if (!(bindable is VisualElement elementView))
+				return;
+
+			elementView.SizeChanged -= ElementViewSizeChanged;
+			elementView.SizeChanged += ElementViewSizeChanged;
+
+			UpdateClip(elementView);
+		}
+
+		static void ElementViewSizeChanged(object? sender, EventArgs e)
+		{
+			if (!(sender is VisualElement elementView))
+				return;
+
+			UpdateClip(elementView);
+		}
 
 		public static CornerRadius GetCornerRadius(BindableObject? bindable)
 			=> (CornerRadius)(bindable?.GetValue(CornerRadiusProperty) ?? throw new ArgumentNullException(nameof(bindable)));
@@ -21,62 +38,10 @@ namespace Xamarin.CommunityToolkit.Effects
 		public static void SetCornerRadius(BindableObject? bindable, CornerRadius value)
 			=> bindable?.SetValue(CornerRadiusProperty, value);
 
-		static void TryGenerateEffect(BindableObject? bindable, object oldValue, object newValue)
+		static void UpdateClip(VisualElement elementView)
 		{
-			if (bindable is not VisualElement elementView)
-				return;
-
-			var cornerRadiusEffects = elementView.Effects.OfType<CornerRadiusEffect>().ToArray();
-
-			if (GetCornerRadius(bindable) == default)
-			{
-				foreach (var cornerRadiusEffect in cornerRadiusEffects)
-					elementView.Effects.Remove(cornerRadiusEffect);
-
-				return;
-			}
-
-			if (cornerRadiusEffects.Length == 0)
-				elementView.Effects.Add(new CornerRadiusEffect());
-		}
-
-		protected override void OnAttached()
-		{
-			if (Element is VisualElement elementView)
-			{
-				elementView.SizeChanged += OnElementSizeChanged;
-				elementView.PropertyChanged += OnElementPropertyChanged;
-				UpdateClip();
-			}
-		}
-
-		protected override void OnDetached()
-		{
-			if (Element is VisualElement elementView)
-			{
-				elementView.SizeChanged -= OnElementSizeChanged;
-				elementView.PropertyChanged -= OnElementPropertyChanged;
-				elementView.Clip = null;
-			}
-		}
-
-		void OnElementSizeChanged(object? sender, EventArgs e) => UpdateClip();
-
-		void OnElementPropertyChanged(object? sender, PropertyChangedEventArgs e)
-		{
-			if (e.PropertyName == CornerRadiusProperty.PropertyName)
-			{
-				UpdateClip();
-			}
-		}
-
-		void UpdateClip()
-		{
-			if (Element is not VisualElement elementView)
-				return;
-
 			var rect = new Rect(0, 0, elementView.Width, elementView.Height);
-			var cornerRadius = GetCornerRadius(rect);
+			var cornerRadius = GetCornerRadius(rect, elementView);
 			if (cornerRadius == default)
 			{
 				elementView.Clip = null;
@@ -93,13 +58,13 @@ namespace Xamarin.CommunityToolkit.Effects
 			roundRectangleGeometry.Rect = rect;
 		}
 
-		CornerRadius GetCornerRadius(Rect rect)
+		static CornerRadius GetCornerRadius(Rect rect, VisualElement elementView)
         {
 	        var maxCornerRadius = Math.Min(rect.Width, rect.Height) / 2;
 	        if (maxCornerRadius <= 0)
 		        return default;
 
-	        var cornerRadius = GetCornerRadius(Element);
+	        var cornerRadius = GetCornerRadius(elementView);
 	        if (cornerRadius.TopLeft > maxCornerRadius ||
 	            cornerRadius.TopRight > maxCornerRadius ||
 	            cornerRadius.BottomLeft > maxCornerRadius ||
