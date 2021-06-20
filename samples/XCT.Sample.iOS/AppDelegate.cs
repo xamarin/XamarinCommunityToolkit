a762932a09e76e998b0115705552563dec66eaf5
Pedro Jesus <pedrojesus.cefet@gmail.com>
2021-06-20 06:34:40 -0300

    Drawing View (#740)
    * DrawingView (#468)
    
    * DrawingView control, samples, documentation
    
    * fix build issues
    
    Co-authored-by: Vladislav Antonyuk <vlad.antonyuk@outlook.com>
    Co-authored-by: Pedro Jesus <pedrojesus.cefet@gmail.com>
    
    * fixed codestyle on Android renderer
    
    * GTK renderer adjustments
    
    * iOS renderer adjustments
    
    * MacOS renderer adjustments
    
    * TIzen rendere adjustments
    
    * UWP renderer adjustments
    
    * WPF tests
    
    * Fixed merge conflict
    
    * Fixed wpf stackoverflow exception
    
    * fixed iOS leaks
    
    * revert toast change
    
    * removed wpf implementations
    
    * root sample page to WelcomePage
    
    * removed DrawingView ctor from MainWindow
    
    * removed drawing instructions from readme
    
    * Update README.md
    
    * Drawing View - Fix nullable, add doc to the public method (#1293)
    
    * Fix nullable, add doc to the public method. add WPF
    
    * Remove WPF renderer from the library.
    
    * Change exception message for small images, fix nullable
    
    * return null instead of exception (#1339)
    
    Co-authored-by: Vladislav Antonyuk <33021114+VladislavAntonyuk@users.noreply.github.com>
    Co-authored-by: Vladislav Antonyuk <vlad.antonyuk@outlook.com>
    Co-authored-by: Brandon Minnick <13558917+brminnick@users.noreply.github.com>
    Co-authored-by: Javier Suárez <javiersuarezruiz@hotmail.com>
    Co-authored-by: Gerald Versluis <gerald.versluis@microsoft.com>

--- samples/XCT.Sample.iOS/AppDelegate.cs
+++ samples/XCT.Sample.iOS/AppDelegate.cs
@@ -6,8 +6,8 @@ namespace Xamarin.CommunityToolkit.Sample.iOS
 	// The UIApplicationDelegate for the application. This class is responsible for launching the
 	// User Interface of the application, as well as listening (and optionally responding) to
 	// application events from iOS.
-	[Register("AppDelegate")]
-	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	[Register(nameof(AppDelegate))]
+	public class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
 	{
 		// This method is invoked when the application has loaded and is ready to run. In this
 		// method you should instantiate the window, load the UI into it and then make the window
@@ -16,7 +16,6 @@ namespace Xamarin.CommunityToolkit.Sample.iOS
 		// You have 17 seconds to return from this method, or iOS will terminate your application.
 		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
 		{
-			global::Xamarin.Forms.Forms.SetFlags("CollectionView_Experimental");
 			global::Xamarin.Forms.Forms.Init();
 			LoadApplication(new App());
 
---
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Views/DrawingView/Service/DrawingViewService.tizen.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using SkiaSharp;
+using SkiaSharp.Views.Tizen;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Tizen;
+
+namespace Xamarin.CommunityToolkit.UI.Views
+{
+	static class DrawingViewService
+	{
+		/// <summary>
+		/// Get image stream from points
+		/// </summary>
+		/// <param name="points">Drawing points</param>
+		/// <param name="imageSize">Image size</param>
+		/// <param name="lineWidth">Line Width</param>
+		/// <param name="strokeColor">Line color</param>
+		/// <param name="backgroundColor">Image background color</param>
+		/// <returns>Image stream</returns>
+		public static Stream GetImageStream(IList<Point> points,
+			Size imageSize,
+			float lineWidth,
+			Color strokeColor,
+			Color backgroundColor)
+		{
+			if (points == null || points.Count < 2)
+			{
+				return Stream.Null;
+			}
+
+			var image = GetImageInternal(points, lineWidth, strokeColor, backgroundColor);
+			if (image == null)
+			{
+				return Stream.Null;
+			}
+
+			var resizedImage = MaxResizeImage(image, (float)imageSize.Width, (float)imageSize.Height);
+			using (resizedImage)
+			{
+				var stream = resizedImage.Encode(SKEncodedImageFormat.Jpeg, 100).AsStream();
+				stream.Position = 0;
+				return stream;
+			}
+		}
+
+		static SKImage? GetImageInternal(IList<Point> points,
+			float lineWidth,
+			Color strokeColor,
+			Color backgroundColor)
+		{
+			var minPointX = points.Min(p => p.X);
+			var minPointY = points.Min(p => p.Y);
+			var drawingWidth = points.Max(p => p.X) - minPointX;
+			var drawingHeight = points.Max(p => p.Y) - minPointY;
+			const int minSize = 1;
+			if (drawingWidth < minSize || drawingHeight < minSize)
+			{
+				return null;
+			}
+
+			var bm = new SKBitmap((int)drawingWidth, (int)drawingHeight);
+			using (var gr = new SKCanvas(bm))
+			{
+				var drawingBackgroundColor = XamarinColorToDrawingColor(backgroundColor);
+				gr.Clear(drawingBackgroundColor);
+				using (var pen = new SKPaint
+				{
+					Color = XamarinColorToDrawingColor(strokeColor),
+					StrokeWidth = lineWidth
+				})
+				{
+					var pointsCount = points.Count;
+					for (var i = 0; i < pointsCount - 1; i++)
+					{
+						var p1 = XamarinPointToDrawingPoint(points.ElementAt(i));
+						var p2 = XamarinPointToDrawingPoint(points.ElementAt(i + 1));
+
+						gr.DrawLine(p1.X - (float)minPointX, p1.Y - (float)minPointY, p2.X - (float)minPointX,
+									p2.Y - (float)minPointY, pen);
+					}
+				}
+			}
+
+			return SKImage.FromBitmap(bm);
+		}
+
+		static SKColor XamarinColorToDrawingColor(Color xamarinColor) => xamarinColor.ToNative().ToSKColor();
+
+		static SKPoint XamarinPointToDrawingPoint(Point xamarinPoint) => new SKPoint((float)xamarinPoint.X, (float)xamarinPoint.Y);
+
+		static SKBitmap MaxResizeImage(SKImage sourceImage, float maxWidth, float maxHeight)
+		{
+			var sourceSize = new Size(sourceImage.Width, sourceImage.Height);
+			var maxResizeFactor = Math.Max(maxWidth / sourceSize.Width, maxHeight / sourceSize.Height);
+			if (maxResizeFactor > 1)
+			{
+				return SKBitmap.FromImage(sourceImage);
+			}
+
+			var width = maxResizeFactor * sourceSize.Width;
+			var height = maxResizeFactor * sourceSize.Height;
+			var bm = new SKBitmap((int)width, (int)height);
+			using (var gr = new SKCanvas(bm))
+			{
+				gr.DrawImage(sourceImage, new SKRect(0, 0, bm.Width, bm.Height));
+			}
+
+			return bm;
+		}
+	}
+}
