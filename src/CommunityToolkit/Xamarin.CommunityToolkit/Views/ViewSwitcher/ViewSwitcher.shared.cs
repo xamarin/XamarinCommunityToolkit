75db8b7184c6e3b70a445c9821d23827f4f8b398
Andrei Misiukevich <andrei.misiukevich@gmail.com>
2021-06-21 02:57:51 +0200

    Added text switcher sample

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Views/ViewSwitcher/ViewSwitcher.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Views/ViewSwitcher/ViewSwitcher.shared.cs
@@ -5,7 +5,7 @@ namespace Xamarin.CommunityToolkit.UI.Views
 	static class ViewSwitcher
 	{
 		internal static readonly BindableProperty TransitionDurationProperty
-			= BindableProperty.Create(nameof(IViewSwitcher.TransitionDuration), typeof(uint), typeof(ViewSwitcher), 250u);
+			= BindableProperty.Create(nameof(IViewSwitcher.TransitionDuration), typeof(uint), typeof(ViewSwitcher), 350u);
 
 		internal static readonly BindableProperty TransitionTypeProperty
 			= BindableProperty.Create(nameof(IViewSwitcher.TransitionType), typeof(TransitionType), typeof(ViewSwitcher), TransitionType.Fade);
