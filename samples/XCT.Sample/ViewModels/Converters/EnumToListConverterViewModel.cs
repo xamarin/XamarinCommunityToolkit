03660e50469fa575b9baf950d22a9fdfaaf3af42
Gustavo Oliveira <gu.ol@hotmail.com>
2021-07-01 13:09:45 +1000

    Added EnumToListConverter

--- samples/XCT.Sample/ViewModels/Converters/EnumToListConverterViewModel.cs
+++ samples/XCT.Sample/ViewModels/Converters/EnumToListConverterViewModel.cs
@@ -1,14 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Xamarin.CommunityToolkit.Behaviors;
 using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms;
 
 namespace Xamarin.CommunityToolkit.Sample.ViewModels.Converters
 {
+	public enum CustomEnum
+	{
+		A,
+		B,
+		C,
+		D,
+		E,
+		F
+	}
+
 	public class EnumToListConverterViewModel : BaseViewModel
 	{
-		List<Type> enumTypes = new List<Type>() { typeof(BadgePosition), typeof(CameraCaptureMode), typeof(CharacterType) };
-		Type? selectedItem = null;
+		List<Type> enumTypes;
+		Type? selectedEnumType;
+		object? selectedItem = null;
+
+		public EnumToListConverterViewModel()
+		{
+			enumTypes = new List<Type>() { typeof(CustomEnum), typeof(BadgePosition), typeof(CameraCaptureMode), typeof(CharacterType) };
+			selectedEnumType = enumTypes.FirstOrDefault();
+		}
 
 		public List<Type> EnumTypes
 		{
@@ -16,7 +35,13 @@ namespace Xamarin.CommunityToolkit.Sample.ViewModels.Converters
 			set => SetProperty(ref enumTypes, value);
 		}
 
-		public Type? SelectedItem
+		public Type? SelectedEnumType
+		{
+			get => selectedEnumType;
+			set => SetProperty(ref selectedEnumType, value);
+		}
+
+		public object? SelectedItem
 		{
 			get => selectedItem;
 			set => SetProperty(ref selectedItem, value);
--- src/CommunityToolkit/Xamarin.CommunityToolkit/Converters/EnumToListConverter.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Converters/EnumToListConverter.shared.cs
@@ -6,24 +6,32 @@ using Xamarin.Forms;
 namespace Xamarin.CommunityToolkit.Converters
 {
 	/// <summary>
-	/// A converter that returns the enumerable sequence of enum as the result.
+	/// A converter that retrieves an array of the values of the constants in a specified enumeration.
 	/// </summary>
 	public class EnumToListConverter : ValueConverterExtension, IValueConverter
 	{
 		/// <summary>
-		/// Returns a string array that contains the substrings in this string that are delimited by <see cref="Separators"/>.
+		/// Retrieves an array of the values of the constants in a specified enumeration.
 		/// </summary>
-		/// <param name="value">The string to split.</param>
+		/// <param name="value">The Enum.</param>
 		/// <param name="targetType">The type of the binding target property. This is not implemented.</param>
-		/// <param name="parameter">The string or strings that delimits the substrings in this string. This overrides the value in <see cref="Separator"/> and <see cref="Separators"/>.</param>
+		/// <param name="parameter">Additional parameter for the converter to handle. This is not implemented.</param>
 		/// <param name="culture">The culture to use in the converter. This is not implemented.</param>
-		/// <returns>An array whose elements contain the substrings in this string that are delimited by <see cref="Separator"/> or, if set, <see cref="Separators"/> or, if set, <paramref name="parameter"/>.</returns>
+		/// <returns>An array that contains the values of the constants in <paramref name="value"/>.</returns>
 		public object? Convert(object? value, Type? targetType, object? parameter, CultureInfo? culture)
 		{
-			if (value is Type type)
-				return Enum.GetValues(type);
-
-			return new string[] { };
+#if !NETSTANDARD1_0 // Use Region or Try Catch???
+#else
+#endif
+			try
+			{
+				if (value is Type type)
+					return Enum.GetValues(type);
+			}
+			finally
+			{
+			}
+			return value;
 		}
 
 		/// <summary>
