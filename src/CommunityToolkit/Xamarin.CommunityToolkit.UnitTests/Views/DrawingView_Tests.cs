a42520671109c5b4550c8b1e5ef37194a79fca96
Shaun Lawrence <shaunrlawrence@gmail.com>
2021-07-05 22:54:04 +0100

    Unit tests for DrawingView

--- src/CommunityToolkit/Xamarin.CommunityToolkit.UnitTests/Views/DrawingView_Tests.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit.UnitTests/Views/DrawingView_Tests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
 using Xamarin.CommunityToolkit.UI.Views;
 using Xamarin.Forms;
 
@@ -18,6 +20,78 @@ namespace Xamarin.CommunityToolkit.UnitTests.Views
 
 			Assert.IsNotEmpty(drawingViewOne.Points);
 			Assert.IsFalse(ReferenceEquals(drawingViewOne.Points, drawingViewTwo.Points));
+			Assert.NotNull(points);
+			Assert.AreEqual(points.Count(), 1);
+
+			drawingView.DrawingCompletedCommand = null;
+		}
+
+		[Test]
+		public void DrawingCompletedCommandShouldNotFireWhenSubscribedAndNoPointsSet()
+		{
+			IEnumerable<Point>? points = null;
+			var drawingView = new DrawingView();
+
+			drawingView.DrawingCompletedCommand = new Command((p) => points = (IEnumerable<Point>)p);
+
+			drawingView.OnDrawingCompleted();
+
+			Assert.Null(points);
+
+			drawingView.DrawingCompletedCommand = null;
+		}
+
+		[Test]
+		public void DrawingCompletedShouldFireWhenSubscribed()
+		{
+			IEnumerable<Point>? points = null;
+			var drawingView = new DrawingView();
+
+			drawingView.DrawingCompleted += (sender, args) => points = args.Points;
+
+			drawingView.Points.Add(Point.Zero);
+			drawingView.OnDrawingCompleted();
+
+			Assert.NotNull(points);
+			Assert.AreEqual(points.Count(), 1);
+
+			drawingView.DrawingCompleted -= (sender, args) => points = args.Points;
+		}
+
+		[Test]
+		public void DrawingCompletedShouldNotFireWhenSubscribedAndNoPointsSet()
+		{
+			IEnumerable<Point>? points = null;
+			var drawingView = new DrawingView();
+
+			drawingView.DrawingCompleted += (sender, args) => points = args.Points;
+
+			drawingView.OnDrawingCompleted();
+
+			Assert.Null(points);
+			drawingView.DrawingCompleted -= (sender, args) => points = args.Points;
+		}
+
+		[Test]
+		public void PointsShouldClearOnceCompleteAndClearOnFinish()
+		{
+			var drawingView = new DrawingView { ClearOnFinish = true };
+
+			drawingView.Points.Add(Point.Zero);
+			drawingView.OnDrawingCompleted();
+
+			Assert.IsEmpty(drawingView.Points);
+		}
+
+		[Test]
+		public void PointsShouldNotClearOnceCompleteAndNotClearOnFinish()
+		{
+			var drawingView = new DrawingView();
+
+			drawingView.Points.Add(Point.Zero);
+			drawingView.OnDrawingCompleted();
+
+			Assert.IsNotEmpty(drawingView.Points);
 		}
 	}
 }
