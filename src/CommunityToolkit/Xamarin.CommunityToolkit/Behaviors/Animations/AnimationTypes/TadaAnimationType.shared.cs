7a7127931ab2a581bab01ee96b4127e9f995fd65
Shaun Lawrence <shaunrlawrence@gmail.com>
2021-07-14 23:36:46 +0100

    Add tada animation (#1468)
    * Base class implementation for pre-built animations
    
    * TADA! Added the first animation
    
    * Max and Min scales for Tada
    
    * Added the RubberBand animation
    
    Co-authored-by: Pedro Jesus <pedrojesus.cefet@gmail.com>

---
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Behaviors/Animations/AnimationTypes/TadaAnimationType.shared.cs
@@ -0,0 +1,66 @@
+﻿using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.Animations;
+using Xamarin.Forms;
+
+namespace Xamarin.CommunityToolkit.Behaviors
+{
+	public class TadaAnimationType : AnimationBase
+	{
+		public static readonly BindableProperty IsRepeatedProperty =
+		   BindableProperty.Create(nameof(IsRepeated), typeof(bool), typeof(TadaAnimationType), default, BindingMode.TwoWay);
+
+		public bool IsRepeated
+		{
+			get => (bool)GetValue(IsRepeatedProperty);
+			set => SetValue(IsRepeatedProperty, value);
+		}
+
+		public static readonly BindableProperty MaximumScaleProperty =
+		   BindableProperty.Create(nameof(MaximumScale), typeof(double), typeof(TadaAnimationType), TadaAnimation.DefaultMaximumScale, BindingMode.TwoWay);
+
+		public double MaximumScale
+		{
+			get => (double)GetValue(MaximumScaleProperty);
+			set => SetValue(MaximumScaleProperty, value);
+		}
+
+		public static readonly BindableProperty MinimumScaleProperty =
+		   BindableProperty.Create(nameof(MinimumScale), typeof(double), typeof(TadaAnimationType), TadaAnimation.DefaultMinimumScale, BindingMode.TwoWay);
+
+		public double MinimumScale
+		{
+			get => (double)GetValue(MinimumScaleProperty);
+			set => SetValue(MinimumScaleProperty, value);
+		}
+
+		public static readonly BindableProperty RotationAngleProperty =
+		   BindableProperty.Create(nameof(RotationAngle), typeof(double), typeof(TadaAnimationType), TadaAnimation.DefaultRotationAngle, BindingMode.TwoWay);
+
+		public double RotationAngle
+		{
+			get => (double)GetValue(RotationAngleProperty);
+			set => SetValue(RotationAngleProperty, value);
+		}
+
+		protected override uint DefaultDuration { get; set; } = TadaAnimation.DefaultLength;
+
+		public override Task Animate(View? view)
+		{
+			if (view != null)
+			{
+				var taskCompletionSource = new TaskCompletionSource<bool>();
+
+				new TadaAnimation(
+					rotationAngle: RotationAngle,
+					length: Duration,
+					shouldRepeat: () => IsRepeated,
+					onFinished: (v, c) => taskCompletionSource.SetResult(c),
+					views: view).Commit();
+
+				return taskCompletionSource.Task;
+			}
+
+			return Task.FromResult(false);
+		}
+	}
+}
