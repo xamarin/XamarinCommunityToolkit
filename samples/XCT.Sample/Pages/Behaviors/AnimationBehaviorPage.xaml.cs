e09b55d9a2c1aa053e2e1dce10fe52d9704ae26d
bijington <shaunrlawrence@gmail.com>
2021-07-14 22:56:27 +0100

    TADA! Added the first animation

--- samples/XCT.Sample/Pages/Behaviors/AnimationBehaviorPage.xaml.cs
+++ samples/XCT.Sample/Pages/Behaviors/AnimationBehaviorPage.xaml.cs
@@ -1,7 +1,20 @@
-﻿namespace Xamarin.CommunityToolkit.Sample.Pages.Behaviors
+﻿using Xamarin.CommunityToolkit.Animations;
+using Xamarin.Forms;
+
+namespace Xamarin.CommunityToolkit.Sample.Pages.Behaviors
 {
 	public partial class AnimationBehaviorPage : BasePage
 	{
 		public AnimationBehaviorPage() => InitializeComponent();
-	}
+
+        private async void TapGestureRecognizer_Tapped(object sender, System.EventArgs e)
+        {
+            var label = (View)sender;
+
+			new TadaAnimation(
+				rotationAngle: 30,
+				length: 1000,
+				views: label).Commit();
+        }
+    }
 }
---
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Behaviors/Animations/AnimationTypes/TadaAnimationType.shared.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.Animations;
+using Xamarin.Forms;
+
+namespace Xamarin.CommunityToolkit.Behaviors
+{
+	public class TadaAnimationType : AnimationBase
+	{
+		public static readonly BindableProperty RotationAngleProperty =
+		   BindableProperty.Create(nameof(RotationAngle), typeof(double), typeof(AnimationBase), TadaAnimation.DefaultRotationAngle, BindingMode.TwoWay);
+
+		public double RotationAngle
+		{
+			get => (double)GetValue(RotationAngleProperty);
+			set => SetValue(RotationAngleProperty, value);
+		}
+
+		// Repeat?
+		// MaximumScale
+		// MinimumScale
+
+		protected override uint DefaultDuration { get; set; } = RubberbandAnimation.DefaultLength;
+
+		public override Task Animate(View? view)
+		{
+			if (view != null)
+			{
+				var taskCompletionSource = new TaskCompletionSource<bool>();
+
+				new TadaAnimation(
+					rotationAngle: RotationAngle,
+					length: Duration,
+					onFinished: (v, c) => taskCompletionSource.SetResult(c),
+					views: view).Commit();
+
+				return taskCompletionSource.Task;
+			}
+
+			// Find the alternative to Task.CompletedTask in netstandard1.0
+			return Task.Delay(1);
+		}
+	}
+}
