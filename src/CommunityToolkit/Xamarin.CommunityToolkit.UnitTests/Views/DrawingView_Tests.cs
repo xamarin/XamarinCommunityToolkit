8d688e4cd25ef7d7dbbbe33629ecd7fad334d026
Vladislav Antonyuk <33021114+VladislavAntonyuk@users.noreply.github.com>
2021-07-16 21:29:11 +0300

    Drawing view multi line (#1475)
    * DrawingView MultiLine
    
    WPF, UWP
    
    * Android
    
    * GTK, Tizen
    
    * iOS, macOS
    
    * DrawingViewService Get Stream from lines
    
    * added missing System using
    
    * Add GetImageStream
    
    * Fix tests
    
    * Add description to switches
    
    * Styling and comments
    
    * DrawingLineCompletedEventArgs
    
    * Add summary, Add tests
    
    Co-authored-by: Pedro Jesus <pedrojesus.cefet@gmail.com>
    Co-authored-by: Gerald Versluis <gerald.versluis@microsoft.com>
    Co-authored-by: Gerald Versluis <gerald@verslu.is>

--- src/CommunityToolkit/Xamarin.CommunityToolkit.UnitTests/Views/DrawingView_Tests.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit.UnitTests/Views/DrawingView_Tests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using NUnit.Framework;
 using Xamarin.CommunityToolkit.UI.Views;
 using Xamarin.Forms;
 
@@ -7,17 +8,92 @@ namespace Xamarin.CommunityToolkit.UnitTests.Views
 	public class DrawingView_Tests
 	{
 		[Test]
-		public void AllInstancesShouldHaveTheirOwnPoints()
+		public void AllInstancesShouldHaveTheirOwnLines()
 		{
 			var drawingViewOne = new DrawingView();
 			var drawingViewTwo = new DrawingView();
 
-			drawingViewOne.Points.Add(Point.Zero);
+			drawingViewOne.Lines.Add(new Line());
 
-			drawingViewTwo.Points.Clear();
+			drawingViewTwo.Lines.Clear();
 
-			Assert.IsNotEmpty(drawingViewOne.Points);
-			Assert.IsFalse(ReferenceEquals(drawingViewOne.Points, drawingViewTwo.Points));
+			Assert.IsNotEmpty(drawingViewOne.Lines);
+			Assert.IsFalse(ReferenceEquals(drawingViewOne.Lines, drawingViewTwo.Lines));
+		}
+
+		[Test]
+		public void AllLineInstancesShouldHaveTheirOwnPoints()
+		{
+			var drawingView = new DrawingView();
+
+			var line1 = new Line();
+			var line2 = new Line();
+			drawingView.Lines.Add(line1);
+			drawingView.Lines.Add(line2);
+
+			line1.Points.Add(Point.Zero);
+
+			line2.Points.Clear();
+
+			Assert.IsNotEmpty(line1.Points);
+			Assert.IsFalse(ReferenceEquals(line1.Points, line2.Points));
+		}
+
+		[Test]
+		public void DrawingLineCompletedCommandShouldFireWhenSet()
+		{
+			Line? line = null;
+			var drawingView = new DrawingView();
+
+			drawingView.DrawingLineCompletedCommand = new Command<Line?>((l) => line = l);
+
+			var newLine = new Line();
+			drawingView.Lines.Add(newLine);
+			drawingView.OnDrawingLineCompleted(newLine);
+
+			Assert.NotNull(line);
+			Assert.AreEqual(drawingView.Lines.Count(), 1);
+		}
+
+		[Test]
+		public void DrawingLineCompletedCommandShouldNotFireWhenLineNotSet()
+		{
+			Line? line = null;
+			var drawingView = new DrawingView();
+
+			drawingView.DrawingLineCompletedCommand = new Command<Line?>((l) => line = l);
+
+			drawingView.OnDrawingLineCompleted(null);
+
+			Assert.Null(line);
+		}
+
+		[Test]
+		public void DrawingLineCompletedShouldFireWhenSubscribed()
+		{
+			Line? line = null;
+			var drawingView = new DrawingView();
+
+			drawingView.DrawingLineCompleted += (sender, args) => line = args.Line;
+			var newLine = new Line();
+			drawingView.Lines.Add(newLine);
+			drawingView.OnDrawingLineCompleted(newLine);
+
+			Assert.NotNull(line);
+			Assert.AreEqual(drawingView.Lines.Count(), 1);
+		}
+
+		[Test]
+		public void DrawingLineCompletedShouldNotFireWhenSubscribedAndLineNotSet()
+		{
+			Line? line = null;
+			var drawingView = new DrawingView();
+
+			drawingView.DrawingLineCompleted += (sender, args) => line = args.Line;
+
+			drawingView.OnDrawingLineCompleted(null);
+
+			Assert.Null(line);
 		}
 	}
 }
