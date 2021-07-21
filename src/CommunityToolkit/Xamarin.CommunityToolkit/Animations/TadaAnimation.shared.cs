edd5e659993f250f6a1b330c4ae680f61c7b6585
bijington <shaunrlawrence@gmail.com>
2021-07-21 22:55:09 +0100

    Code tidy up

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Animations/TadaAnimation.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Animations/TadaAnimation.shared.cs
@@ -5,7 +5,7 @@ namespace Xamarin.CommunityToolkit.Animations
 {
     public class TadaAnimation : AnimationBase
     {
-        internal const uint DefaultLength = 500;
+        internal const uint DefaultLength = 1000;
         internal const double DefaultMaximumScale = 1.1;
         internal const double DefaultMinimumScale = 0.9;
         internal const double DefaultRotationAngle = 3.0;
