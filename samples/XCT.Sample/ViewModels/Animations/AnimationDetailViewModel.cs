7e631ce614b3f3c20e9a05b12396bffd6cd59255
bijington <shaunrlawrence@gmail.com>
2021-07-21 21:49:29 +0100

    Initial attempt at an Animations sample section

---
+++ samples/XCT.Sample/ViewModels/Animations/AnimationDetailViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.CommunityToolkit.Animations;
+using Xamarin.Forms;
+
+namespace Xamarin.CommunityToolkit.Sample.ViewModels.Animations
+{
+	public class AnimationDetailViewModel : BaseViewModel
+	{
+		public string Name { get; }
+		public Func<View, Action<double, bool>, AnimationBase> CreateAnimation { get; }
+
+		public AnimationDetailViewModel(string name, Func<View, Action<double, bool>, AnimationBase> createAnimation)
+		{
+			Name = name;
+			CreateAnimation = createAnimation;
+		}
+	}
+}
---
+++ samples/XCT.Sample/ViewModels/Animations/AnimationViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using Xamarin.CommunityToolkit.Animations;
+using Xamarin.Forms;
+
+namespace Xamarin.CommunityToolkit.Sample.ViewModels.Animations
+{
+	public class AnimationViewModel : BaseViewModel
+	{
+		AnimationBase? currentAnimation;
+		AnimationDetailViewModel? selectedAnimation;
+
+		public ObservableCollection<AnimationDetailViewModel> Animations { get; }
+
+		public AnimationDetailViewModel? SelectedAnimation
+		{
+			get => selectedAnimation;
+			set => SetProperty(ref selectedAnimation, value);
+		}
+
+		public Command StartAnimationCommand { get; }
+		public Command StopAnimationCommand { get; }
+
+		public AnimationViewModel()
+		{
+			Animations = new ObservableCollection<AnimationDetailViewModel>
+			{
+				new AnimationDetailViewModel("Tada", (view, onFinished) => new TadaAnimation(onFinished: onFinished, views: view)),
+				new AnimationDetailViewModel("RubberBand", (view, onFinished) => new RubberBandAnimation(onFinished: onFinished, views: view))
+			};
+
+			SelectedAnimation = Animations.First();
+			StartAnimationCommand = new Command<View>(OnStart, (view) => !(SelectedAnimation is null) && currentAnimation?.IsRunning != true);
+			StopAnimationCommand = new Command(OnStop, () => currentAnimation?.IsRunning == true);
+		}
+
+		void OnStart(View view)
+		{
+			if (currentAnimation != null)
+			{
+				currentAnimation.Abort();
+			}
+
+			currentAnimation = SelectedAnimation!.CreateAnimation(view, (d, b) =>
+			{
+				StartAnimationCommand.ChangeCanExecute();
+				StopAnimationCommand.ChangeCanExecute();
+			});
+
+			currentAnimation.Commit();
+
+			StopAnimationCommand.ChangeCanExecute();
+		}
+
+		void OnStop()
+		{
+			if (currentAnimation != null)
+			{
+				currentAnimation.Abort();
+			}
+		}
+	}
+}
+
