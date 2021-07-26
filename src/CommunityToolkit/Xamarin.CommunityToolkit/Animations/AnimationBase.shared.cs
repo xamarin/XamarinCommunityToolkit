c1d363e3c04bfb36e7b4ad5278ebc4a9ab003bff
Pedro Jesus <pedrojesus.cefet@gmail.com>
2021-07-26 19:40:11 -0300

    changed the null and empty verification to be more performant

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Animations/AnimationBase.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Animations/AnimationBase.shared.cs
@@ -4,63 +4,63 @@ using Xamarin.Forms;
 
 namespace Xamarin.CommunityToolkit.Animations
 {
-    /// <summary>
+	/// <summary>
 	/// Base class representation of a pre-built animation.
 	/// </summary>
-    public abstract class AnimationBase
-    {
-        readonly Easing? easing;
-        readonly uint length = 250;
-        readonly Action<double, bool>? onFinished;
-        readonly IAnimatable? owner;
-        readonly uint rate = 16;
-        readonly Func<bool>? shouldRepeat;
-        readonly string name;
-        protected readonly View[] views;
+	public abstract class AnimationBase
+	{
+		readonly Easing? easing;
+		readonly uint length = 250;
+		readonly Action<double, bool>? onFinished;
+		readonly IAnimatable? owner;
+		readonly uint rate = 16;
+		readonly Func<bool>? shouldRepeat;
+		readonly string name;
+		protected readonly View[] views;
 
-        protected AnimationBase(
-            string name,
-            IAnimatable? owner = null,
-            uint rate = 16,
-            uint length = 250,
-            Easing? easing = null,
-            Action<double, bool>? onFinished = null,
-            Func<bool>? shouldRepeat = null,
-            params View[] views)
-        {
-            if (views is null || !views.Any())
+		protected AnimationBase(
+			string name,
+			IAnimatable? owner = null,
+			uint rate = 16,
+			uint length = 250,
+			Easing? easing = null,
+			Action<double, bool>? onFinished = null,
+			Func<bool>? shouldRepeat = null,
+			params View[] views)
+		{
+			if (views == null || views.Length == 0)
 			{
-                throw new ArgumentException("Views cannot be null or empty.", nameof(views));
+				throw new ArgumentException("Views cannot be null or empty.", nameof(views));
 			}
 
-            this.name = name + Guid.NewGuid().ToString();
-            this.length = length;
-            this.views = views;
-            this.easing = easing;
-            this.onFinished = onFinished;
-            this.owner = owner;
-            this.rate = rate;
-            this.shouldRepeat = shouldRepeat;
-        }
+			this.name = name + Guid.NewGuid().ToString();
+			this.length = length;
+			this.views = views;
+			this.easing = easing;
+			this.onFinished = onFinished;
+			this.owner = owner;
+			this.rate = rate;
+			this.shouldRepeat = shouldRepeat;
+		}
 
-        /// <summary>
+		/// <summary>
 		/// Stops the animation.
 		/// </summary>
 		/// <returns>True if successful, false otherwise.</returns>
-        public bool Abort() => GetOwner().AbortAnimation(name);
+		public bool Abort() => GetOwner().AbortAnimation(name);
 
-        /// <summary>
+		/// <summary>
 		/// Runs the animation.
 		/// </summary>
-        public void Commit() => Create().Commit(GetOwner(), name, rate, length, easing, onFinished, shouldRepeat);
+		public void Commit() => Create().Commit(GetOwner(), name, rate, length, easing, onFinished, shouldRepeat);
 
-        /// <summary>
+		/// <summary>
 		/// Gets a value indicating whether the animation is running.
 		/// </summary>
-        public bool IsRunning => GetOwner().AnimationIsRunning(name);
+		public bool IsRunning => GetOwner().AnimationIsRunning(name);
 
-        protected abstract Animation Create();
+		protected abstract Animation Create();
 
-        IAnimatable GetOwner() => owner ?? views.First();
-    }
+		IAnimatable GetOwner() => owner ?? views.First();
+	}
 }
