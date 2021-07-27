8ab874d4ca3ad85b0ee5dfee9c1726daba64a767
Leonard Harris <leojharris@hotmail.com>
2021-07-27 23:19:46 +1200

    Added UtcDateTimeToLocalStringConverter with page to test.

---
+++ samples/XCT.Sample/ViewModels/Converters/UtcDateTimeToLocalStringConverterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Xamarin.CommunityToolkit.Sample.ViewModels.Converters
+{
+	public class UtcDateTimeToLocalStringConverterViewModel : BaseViewModel
+	{
+		DateTime date = DateTime.UtcNow;
+
+		public DateTime UtcDate
+		{
+			get => date;
+			set => SetProperty(ref date, value);
+		}
+
+		DateTimeOffset utcDateTimeOffset = DateTimeOffset.UtcNow;
+
+		public DateTimeOffset UtcDateTimeOffset
+		{
+			get => utcDateTimeOffset;
+			set => SetProperty(ref utcDateTimeOffset, value);
+		}
+	}
+}
