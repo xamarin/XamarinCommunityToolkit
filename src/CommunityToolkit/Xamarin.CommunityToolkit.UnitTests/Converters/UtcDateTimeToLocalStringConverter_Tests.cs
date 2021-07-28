9511a67d4f2734714e921de75464d519465b2e8e
Leonard Harris <leojharris@hotmail.com>
2021-07-28 21:23:50 +1200

    Refactored Tests

--- src/CommunityToolkit/Xamarin.CommunityToolkit.UnitTests/Converters/UtcDateTimeToLocalStringConverter_Tests.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit.UnitTests/Converters/UtcDateTimeToLocalStringConverter_Tests.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 using NUnit.Framework;
 using Xamarin.CommunityToolkit.Converters;
 
@@ -9,36 +8,23 @@ namespace Xamarin.CommunityToolkit.UnitTests.Converters
 {
 	public class UtcDateTimeToLocalStringConverter_Tests
 	{
-		public const string TestValue = nameof(TestValue);
-
-		public static IEnumerable<object?[]> GetData() => new List<object?[]>
+		public static IEnumerable<object?[]> GetPassingData() => new List<object?[]>
 		{
-			new object[] { DateTime.UtcNow.Ticks, DateTime.Now.ToLocalTime() },
-			new object[] { new List<string>() { "TestValue" }, false },
-			new object?[] { null, true },
-			new object[] { Enumerable.Range(1, 3), false },
+			new object[] { DateTime.UtcNow, DateTime.Now.ToLocalTime().ToString("G") },
+			new object[] { DateTimeOffset.UtcNow, DateTime.Now.ToLocalTime().ToString("G") }
 		};
 
-		[TestCaseSource(nameof(GetData))]
-		public void IsEqual(object value, object comparedValue)
+		[TestCaseSource(nameof(GetPassingData))]
+		public void UtcDateTimeToLocalStringConvert_Validation(DateTimeOffset value, string expectedResult)
 		{
-			var utcDateTimeToLocalStringConverter = new UtcDateTimeToLocalStringConverter();
+			var utcDateTimeToLocalStringConverter = new UtcDateTimeToLocalStringConverter()
+			{
+				DateTimeFormat = "G"
+			};
 
-			var result = utcDateTimeToLocalStringConverter.Convert(value, typeof(UtcDateTimeToLocalStringConverter_Tests), comparedValue, CultureInfo.CurrentCulture);
+			var result = utcDateTimeToLocalStringConverter.Convert(value, typeof(UtcDateTimeToLocalStringConverter_Tests), null, CultureInfo.CurrentCulture);
 
-			Assert.IsInstanceOf<bool>(result);
-			Assert.IsTrue((bool)result);
-		}
-
-		[TestCaseSource(nameof(GetData))]
-		public void IsNotEqual(object value, object comparedValue)
-		{
-			var utcDateTimeToLocalStringConverter = new UtcDateTimeToLocalStringConverter();
-
-			var result = utcDateTimeToLocalStringConverter.Convert(value, typeof(UtcDateTimeToLocalStringConverter_Tests), comparedValue, CultureInfo.CurrentCulture);
-
-			Assert.IsInstanceOf<bool>(result);
-			Assert.False((bool)result);
+			Assert.AreEqual(expectedResult, result);
 		}
 	}
 }
