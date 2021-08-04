ca23c6ff8c8af2cd7d0b872181286e14d8b1dd04
Damian Antonowicz <kontakt@damianantonowicz.pl>
2021-08-04 17:42:37 +0200

    DayOfWeekToAbbreviatedNameConverter is now checking if value is DayOfWeek
    Co-authored-by: Pedro Jesus <pedrojesus.cefet@gmail.com>

--- samples/XCT.Sample/Pages/Views/Calendar/DayOfWeekToAbbreviatedNameConverter.cs
+++ samples/XCT.Sample/Pages/Views/Calendar/DayOfWeekToAbbreviatedNameConverter.cs
@@ -8,7 +8,7 @@ namespace Xamarin.CommunityToolkit.Sample.Pages.Views
 	{
 		public object Convert(object? value, Type? targetType, object? parameter, CultureInfo? culture)
 		{
-			if (value == null)
+			if (value is not DayOfWeek)
 				throw new ArgumentNullException(nameof(value));
 
 			var dayOfWeek = (DayOfWeek)value;
