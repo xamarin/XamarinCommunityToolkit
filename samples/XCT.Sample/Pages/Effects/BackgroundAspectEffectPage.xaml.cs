214fa2a2402ed53286d55532bf1195168ebaceee
Alex Duffell <a.duffell@senso.cloud>
2021-08-06 17:05:31 +0100

    Work In Progress On Android Effect Renderer
    Started adding the android code, got it setting the background to a solid color

--- samples/XCT.Sample/Pages/Effects/BackgroundAspectEffectPage.xaml.cs
+++ samples/XCT.Sample/Pages/Effects/BackgroundAspectEffectPage.xaml.cs
@@ -1,39 +1,43 @@
-﻿
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Diagnostics;
 using Xamarin.CommunityToolkit.Effects;
 using Xamarin.Forms;
 
 namespace Xamarin.CommunityToolkit.Sample.Pages.Effects
 {
-    public partial class BackgroundAspectEffectPage : BasePage
+	public partial class BackgroundAspectEffectPage : BasePage
     {
         public BackgroundAspectEffectPage() => InitializeComponent();
 
         void OnOptionChecked(object sender, CheckedChangedEventArgs e)
         {
-            Aspect? aspect = null;
+            var aspect = GetSelectedAspect(sender);
 
-            if (sender == RadioFill && RadioFill.IsChecked)
-            {
-                aspect = Aspect.Fill;
-            }
-            else if (sender == RadioAspectFill && RadioAspectFill.IsChecked)
-            {
-                aspect = Aspect.AspectFill;
-            }
-            else if (sender == RadioAspectFit && RadioAspectFit.IsChecked)
-            {
-                aspect = Aspect.AspectFit;
-            }
-            else
-            {
+            if (aspect == null)
                 return;
-            }
 
             Debug.WriteLine($"Change the current effect to: {aspect}");
 
             BackgroundAspectEffect.SetAspect(this, aspect.Value);
+        }
+
+        Aspect? GetSelectedAspect(object sender)
+		{
+            if (sender == RadioFill && RadioFill.IsChecked)
+            {
+                return Aspect.Fill;
+            }
+            else if (sender == RadioAspectFill && RadioAspectFill.IsChecked)
+            {
+                return Aspect.AspectFill;
+            }
+            else if (sender == RadioAspectFit && RadioAspectFit.IsChecked)
+            {
+                return Aspect.AspectFit;
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
--- src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/BackgroundAspectEffect/BackgroundAspectEffectRouter.android.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/BackgroundAspectEffect/BackgroundAspectEffectRouter.android.cs
@@ -1,9 +1,19 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Threading.Tasks;
+using Android.Content;
+using Android.Views;
+using Android.Views.Accessibility;
+using AndroidX.Core.Content;
+using Android.Graphics;
 using Xamarin.CommunityToolkit.Effects;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 using Effects = Xamarin.CommunityToolkit.Android.Effects;
+
+using AOS = Android.OS;
+
+using AColor = Android.Graphics.Color;
 
 [assembly: ExportEffect(typeof(Effects.BackgroundAspectEffectRouter), nameof(BackgroundAspectEffectRouter))]
 
@@ -11,14 +21,93 @@ namespace Xamarin.CommunityToolkit.Android.Effects
 {
 	public class BackgroundAspectEffectRouter : PlatformEffect
 	{
-		protected override void OnAttached()
+		//readonly Context context;
+
+		public BackgroundAspectEffectRouter()
 		{
-			Debugger.Break();
+		}
+
+		// TODO: Pass context in so we can get image from handler
+		//public BackgroundAspectEffectRouter(Context context)
+		//{
+		//	this.context = context;
+		//}
+
+		protected override async void OnAttached()
+		{
+			return;
+			//Debugger.Break();
+
+			var contentPage = Element as ContentPage;
+
+			if (Element == null || contentPage == null || contentPage.BackgroundImageSource == null)
+				return;
+
+			//var image = await GetBackgroundImageFromSource(contentPage.BackgroundImageSource);
+
+			//if (image == null)
+			//	return;
+
+			var fallbackColor = contentPage.BackgroundColor.ToAndroid();
+
+			ApplyImageToView(null!, fallbackColor, contentPage);
 		}
 
 		protected override void OnDetached()
 		{
 
+		}
+
+		void ApplyImageToView(Bitmap image, AColor color, ContentPage contentPage)
+		{
+			var effectParam = BackgroundAspectEffect.GetAspect(Element);
+
+			var hotPink = AColor.HotPink;
+
+			if (Container == null)
+				return;
+
+			// Working with colors
+
+			var backgroundDrawable = Container.Background!;
+
+			if (AOS.Build.VERSION.SdkInt >= AOS.BuildVersionCodes.Q)
+			{
+				var colorFilter = new BlendModeColorFilter(hotPink, BlendMode.SrcAtop!);
+
+				backgroundDrawable.SetColorFilter(colorFilter);
+			}
+			else
+			{
+				var mode = PorterDuff.Mode.SrcAtop!;
+
+				backgroundDrawable.SetColorFilter(hotPink, mode);
+			}
+		}
+
+		async Task<Bitmap> GetBackgroundImageFromSource(ImageSource imageSource)
+		{
+			IImageSourceHandler handler;
+
+			if (imageSource is FileImageSource)
+			{
+				handler = new FileImageSourceHandler();
+			}
+			else if (imageSource is StreamImageSource)
+			{
+				handler = new StreamImagesourceHandler();
+			}
+			else if (imageSource is UriImageSource)
+			{
+				handler = new ImageLoaderSourceHandler();
+			}
+			else
+			{
+				throw new NotSupportedException($"ImageSource is not supported by this effect: {imageSource.GetType().Name}");
+			}
+
+			return await handler.LoadImageAsync(imageSource, null)
+				.ConfigureAwait(false);
 		}
 	}
 }
