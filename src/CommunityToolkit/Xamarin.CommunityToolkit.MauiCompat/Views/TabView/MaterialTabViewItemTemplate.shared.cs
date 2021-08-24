c1585fc3eb5b8e7ddd503bd898441fc3831af9b4
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-08-23 19:07:27 -0700

    Add Android Resources

--- src/CommunityToolkit/Xamarin.CommunityToolkit.MauiCompat/Views/TabView/MaterialTabViewItemTemplate.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit.MauiCompat/Views/TabView/MaterialTabViewItemTemplate.shared.cs
@@ -25,8 +25,8 @@ namespace Xamarin.CommunityToolkit.UI.Views
 			HorizontalOptions = LayoutOptions.FillAndExpand;
 			VerticalOptions = LayoutOptions.FillAndExpand;
 
-			RowDefinitions.Add(new RowDefinition { Height = Microsoft.Maui.Compatibility.GridLength.Star });
-			RowDefinitions.Add(new RowDefinition { Height = Microsoft.Maui.Compatibility.GridLength.Auto });
+			RowDefinitions.Add(new RowDefinition { Height = Microsoft.Maui.GridLength.Star });
+			RowDefinitions.Add(new RowDefinition { Height = Microsoft.Maui.GridLength.Auto });
 
 			icon = new Image
 			{
