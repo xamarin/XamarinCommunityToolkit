329523b0a61437bfb916aa906eeac5188c5ffceb
Pedro Jesus <pedrojesus.cefet@gmail.com>
2021-08-28 20:10:31 -0300

    created solution

---
+++ samples/XCT.MauiCompat.Sample/XCT.MauiCompat.Sample/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+using Microsoft.Maui;
+
+namespace XCT.MauiCompat.Sample
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate<Startup>
+    {
+    }
+}
