0108c7ae20d12a7b72b5ab87d16f4ab91aa6e52e
Shaun Lawrence <shaunrlawrence@gmail.com>
2021-11-30 21:24:58 +0000

    Initial snackbar samples rework

---
+++ samples/XCT.Sample/ViewModels/Views/SnackBarViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.CommunityToolkit.Extensions;
+using Xamarin.CommunityToolkit.ObjectModel;
+using Xamarin.Forms;
+
+namespace Xamarin.CommunityToolkit.Sample.ViewModels.Views
+{
+	public class SnackBarViewModel : BaseViewModel
+	{
+		bool isToasting;
+
+		public bool IsToasting
+		{
+			get => isToasting;
+			set => SetProperty(ref isToasting, value);
+		}
+
+		public SnackBarViewModel()
+		{
+			ToasterTappedCommand = new AsyncCommand<VisualElement>(OnToasterTapped, allowsMultipleExecutions: false);
+		}
+
+		public ICommand ToasterTappedCommand { get; }
+
+		async Task OnToasterTapped(VisualElement? element)
+		{
+			IsToasting = true;
+
+			await Task.Delay(TimeSpan.FromSeconds(2));
+
+			IsToasting = false;
+
+			await element!.DisplayToastAsync("Your toast is ready");
+		}
+	}
+}
