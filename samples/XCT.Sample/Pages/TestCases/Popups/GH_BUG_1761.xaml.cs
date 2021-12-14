850e57c7946e4f365aaf77e028a82c554ca3f087
Dmytro Bondarenko <bondarenkod@windowslive.com>
2021-12-14 22:07:00 +0200

    added ui not-a-test for #1761

---
+++ samples/XCT.Sample/Pages/TestCases/Popups/GH_BUG_1761.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.CommunityToolkit.Extensions;
+using Xamarin.CommunityToolkit.Sample.Pages.Views.Popups;
+using Xamarin.CommunityToolkit.Sample.ViewModels;
+using Xamarin.Forms;
+
+namespace Xamarin.CommunityToolkit.Sample.Pages.TestCases.Popups
+{
+	public partial class GH_BUG_1761
+	{
+		public GH_BUG_1761() => InitializeComponent();
+
+		private async void ButtonAB_Clicked(object sender, EventArgs e)
+		{
+			var popupA = new ReturnResultPopup();
+			var result = await Navigation.ShowPopupAsync(popupA);
+			var popupB = new ButtonPopup();
+			Navigation.ShowPopup(popupB);
+		}
+	}
+
+
+	public class GH_BUG_1761_ViewModel : BaseViewModel
+	{
+		public GH_BUG_1761_ViewModel()
+		{
+
+		}
+
+		INavigation Navigation => Application.Current.MainPage.Navigation;
+	}
+}
