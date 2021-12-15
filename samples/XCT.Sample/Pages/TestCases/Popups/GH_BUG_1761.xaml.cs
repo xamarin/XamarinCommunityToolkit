e5c88aeddabaddf4576fb27f301ec0c9ac770e9e
Dmytro Bondarenko <bondarenkod@windowslive.com>
2021-12-15 21:17:54 +0200

    #1761 test improved

--- samples/XCT.Sample/Pages/TestCases/Popups/GH_BUG_1761.xaml.cs
+++ samples/XCT.Sample/Pages/TestCases/Popups/GH_BUG_1761.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows.Input;
 using Xamarin.CommunityToolkit.Extensions;
 using Xamarin.CommunityToolkit.Sample.Pages.Views.Popups;
 using Xamarin.CommunityToolkit.Sample.ViewModels;
+using Xamarin.CommunityToolkit.UI.Views;
 using Xamarin.Forms;
 
 namespace Xamarin.CommunityToolkit.Sample.Pages.TestCases.Popups
@@ -14,10 +15,44 @@ namespace Xamarin.CommunityToolkit.Sample.Pages.TestCases.Popups
 
 		private async void ButtonAB_Clicked(object sender, EventArgs e)
 		{
-			var popupA = new ReturnResultPopup();
+			var popupA = new PopupThatReturnRandomString();
 			var result = await Navigation.ShowPopupAsync(popupA);
-			var popupB = new ButtonPopup();
+			var popupB = new PopupThatDisplayString(result);
 			Navigation.ShowPopup(popupB);
+		}
+
+
+		public class PopupThatReturnRandomString : Popup<string?>
+		{
+			protected override string GetLightDismissResult() => "Light Dismiss";
+
+			void Button_Clicked(object? sender, System.EventArgs e) => Dismiss(Value);
+
+			private string Value = string.Empty;
+			public PopupThatReturnRandomString()
+			{
+				Value = Guid.NewGuid().ToString("N").Substring(0, 4).ToUpperInvariant();
+				Size = new Size(200, 200);
+				var sl = new StackLayout();
+				var label = new Label() { Text = $"Generated value: {Value}" };
+				var btn = new Button() { Text = $"DONE" };
+				btn.Pressed += Button_Clicked;
+				sl.Children.Add(label);
+				sl.Children.Add(btn);
+				Content = sl;
+			}
+		}
+
+		public class PopupThatDisplayString : Popup
+		{
+			public PopupThatDisplayString(string? value)
+			{
+				Size = new Size(200, 200);
+				var sl = new StackLayout();
+				var btn = new Label() { Text = $"Received value: {value ?? "value is null"}" };
+				sl.Children.Add(btn);
+				Content = sl;
+			}
 		}
 	}
 
