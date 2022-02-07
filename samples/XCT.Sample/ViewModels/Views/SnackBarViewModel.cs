9df6aa14b8c920fab542781703ba32d7dfbb5159
Shaun Lawrence <shaunrlawrence@gmail.com>
2022-02-07 21:57:05 +0000

    Expose advanced Toast API

--- samples/XCT.Sample/ViewModels/Views/SnackBarViewModel.cs
+++ samples/XCT.Sample/ViewModels/Views/SnackBarViewModel.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.CommunityToolkit.Extensions;
 using Xamarin.CommunityToolkit.ObjectModel;
+using Xamarin.CommunityToolkit.UI.Views.Options;
 using Xamarin.Forms;
 
 namespace Xamarin.CommunityToolkit.Sample.ViewModels.Views
@@ -26,13 +27,34 @@ namespace Xamarin.CommunityToolkit.Sample.ViewModels.Views
 
 		async Task OnToasterTapped(VisualElement? element)
 		{
+			if (element is null)
+			{
+				return;
+			}
+
 			IsToasting = true;
 
 			await Task.Delay(TimeSpan.FromSeconds(2));
 
 			IsToasting = false;
 
-			await element!.DisplayToastAsync("Your toast is ready");
+			var messageOptions = new MessageOptions
+			{
+				Foreground = Color.LightGray,
+				Font = Font.SystemFontOfSize(16),
+				Message = "Your toast is ready",
+				Padding = new Thickness(10, 10, 10, 10),
+			};
+			var options = new ToastOptions
+			{
+				MessageOptions = messageOptions,
+				Duration = TimeSpan.FromMilliseconds(3000),
+				BackgroundColor = Color.Black,
+				CornerRadius = new Thickness(10, 10, 10, 10),
+				IsRtl = false
+			};
+
+			await element.DisplayToastAsync(options);
 		}
 	}
 }
