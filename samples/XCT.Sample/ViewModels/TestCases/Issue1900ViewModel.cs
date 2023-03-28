e7c0c80d79d363e949443970e7c65bcfd6717c57
Federico Nembrini <federico.nembrini@gea-solution.com>
2023-03-28 21:35:56 +0200

    Fix badge view default text

---
+++ samples/XCT.Sample/ViewModels/TestCases/Issue1900ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.CommunityToolkit.ObjectModel;
+
+namespace Xamarin.CommunityToolkit.Sample.ViewModels.TestCases
+{
+	public class Issue1900ViewModel : BaseViewModel
+	{
+		int counter;
+
+		public int Counter
+		{
+			get => counter;
+			set => SetProperty(ref counter, value);
+		}
+
+		public ICommand ClickCommand => new AsyncCommand(Click);
+
+		public Issue1900ViewModel()
+		{
+		}
+
+		Task Click()
+		{
+			Counter++;
+			return Task.CompletedTask;
+		}
+	}
+}
