12dce06f3ce5857bfc62e0c6f3094fc29dd988c6
FedericoNembrini <25205086+FedericoNembrini@users.noreply.github.com>
2023-08-07 10:12:41 +0200

    Fix tab view item font family (ios and uwp)

---
+++ samples/XCT.Sample/Pages/TestCases/Issue1978.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace Xamarin.CommunityToolkit.Sample.Pages.TestCases
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Issue1978Page : BasePage
+	{
+		public Issue1978Page()
+		{
+			InitializeComponent();
+		}
+	}
+}
